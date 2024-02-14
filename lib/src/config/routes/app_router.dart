f1e7a763bfa5ca3d5b519e98a57a3e23fd473ed7
Kehinde <ajolayemi82@gmail.com>
2024-02-14 18:14:05 +0100

    Added logic to support bottom navigation bar

--- lib/src/config/routes/app_router.dart
+++ lib/src/config/routes/app_router.dart
@@ -4,8 +4,21 @@ import 'package:go_router/go_router.dart';
 import 'package:irrigazione_iot/src/config/routes/go_router_refresh_stream.dart';
 import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
 import 'package:irrigazione_iot/src/features/authentication/presentation/sign_in/sign_in_screen.dart';
+import 'package:irrigazione_iot/src/features/home/presentation/home_nested_navigator.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/empty_placeholder_widget.dart';
+
+// private navigator keys
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _dashboardShellNavigatorKey = GlobalKey<NavigatorState>(
+  debugLabel: 'Dashboard',
+);
+final _settingsShellNavigatorKey = GlobalKey<NavigatorState>(
+  debugLabel: 'Settings',
+);
+final _moreShellNavigatorKey = GlobalKey<NavigatorState>(
+  debugLabel: 'More',
+);
 
 /// All supported routes in the app
 /// By using an enum, we can avoid using strings for route names
@@ -13,16 +26,14 @@ import 'package:irrigazione_iot/src/widgets/empty_placeholder_widget.dart';
 /// ```dart
 /// context.goNamed(AppRoute.sampleRoute.name)
 /// ```
-enum AppRoute {
-  home,
-  signIn,
-}
+enum AppRoute { home, signIn, settings, more }
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
+    navigatorKey: _rootNavigatorKey,
 
     // * redirect logic based on the authentication state
     redirect: (context, state) {
@@ -42,20 +53,68 @@ final goRouterProvider = Provider<GoRouter>((ref) {
     ),
     routes: [
       GoRoute(
-        path: '/',
-        name: AppRoute.home.name,
-        builder: (context, state) => Scaffold(
-          body: EmptyPlaceholderWidget(
-            message: context.loc.homePageTitle,
-          ),
-        ), // TODO: replace with your home page
-      ),
-      GoRoute(
           path: '/sign-in',
           name: AppRoute.signIn.name,
           builder: (context, state) =>
               const SignInScreen() // TODO: replace with your sign in page
           ),
+      // Stateful navigation based on:
+      // https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return HomeNestedNavigation(navigationShell: navigationShell);
+        },
+        branches: [
+          // The dashboard branch which also serves as the app's home screen
+          StatefulShellBranch(
+            navigatorKey: _dashboardShellNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/',
+                name: AppRoute.home.name,
+                pageBuilder: (context, state) => NoTransitionPage(
+                  child: EmptyPlaceholderWidget(
+                    message: context.loc.homePageTitle,
+                  ),
+                ), // TODO: replace with your home page
+              ),
+            ],
+          ),
+
+          // Settings branch
+          StatefulShellBranch(
+            navigatorKey: _settingsShellNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/settings',
+                name: AppRoute.settings.name,
+                pageBuilder: (context, state) => NoTransitionPage(
+                  child: EmptyPlaceholderWidget(
+                    message: context.loc.settingsPageTitle,
+                  ),
+                ), // TODO: replace with settings page
+              ),
+            ],
+          ),
+
+          // More branch
+          // Settings branch
+          StatefulShellBranch(
+            navigatorKey: _moreShellNavigatorKey,
+            routes: [
+              GoRoute(
+                path: '/more',
+                name: AppRoute.more.name,
+                pageBuilder: (context, state) => NoTransitionPage(
+                  child: EmptyPlaceholderWidget(
+                    message: context.loc.morePageTitle,
+                  ),
+                ), // TODO: replace with more page
+              ),
+            ],
+          ),
+        ],
+      ),
     ],
   );
 });
