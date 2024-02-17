a8eee8e0d69aaf42d8cac1ad283fb014de00dba3
Kehinde <ajolayemi82@gmail.com>
2024-02-17 20:35:13 +0100

    Updated app_user's class removing companyId

--- lib/src/features/authentication/domain/app_user.dart
+++ lib/src/features/authentication/domain/app_user.dart
@@ -7,47 +7,40 @@ class AppUser {
   final UserID uid;
   final String email;
   final String name;
-  final String surname;
-  final int companyId; // TODO replace this with a CompanyID type
+  final String surname; 
   final AppUserRoles role;
 
   const AppUser({
     required this.uid,
     required this.email,
     required this.name,
-    required this.companyId,
     required this.role,
     required this.surname,
   });
 
-
-
   @override
   bool operator ==(covariant AppUser other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.uid == uid &&
-      other.email == email &&
-      other.name == name &&
-      other.surname == surname &&
-      other.companyId == companyId &&
-      other.role == role;
+
+    return other.uid == uid &&
+        other.email == email &&
+        other.name == name &&
+        other.surname == surname &&
+        other.role == role;
   }
 
   @override
   int get hashCode {
     return uid.hashCode ^
-      email.hashCode ^
-      name.hashCode ^
-      surname.hashCode ^
-      companyId.hashCode ^
-      role.hashCode;
+        email.hashCode ^
+        name.hashCode ^
+        surname.hashCode ^
+        role.hashCode;
   }
 
   @override
   String toString() {
-    return 'AppUser(uid: $uid, email: $email, name: $name, surname: $surname, companyId: $companyId, role: $role)';
+    return 'AppUser(uid: $uid, email: $email, name: $name, surname: $surname,  role: $role)';
   }
 
   AppUser copyWith({
@@ -55,7 +48,6 @@ class AppUser {
     String? email,
     String? name,
     String? surname,
-    int? companyId,
     AppUserRoles? role,
   }) {
     return AppUser(
@@ -63,8 +55,30 @@ class AppUser {
       email: email ?? this.email,
       name: name ?? this.name,
       surname: surname ?? this.surname,
-      companyId: companyId ?? this.companyId,
       role: role ?? this.role,
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'uid': uid,
+      'email': email,
+      'name': name,
+      'surname': surname,
+      'role': role,
+    };
+  }
+
+  factory AppUser.fromMap(Map<String, dynamic> map) {
+    return AppUser(
+      uid: map['uid'] as String,
+      email: map['email'] as String,
+      name: map['name'] as String,
+      surname: map['surname'] as String,
+      role: AppUserRoles.values.firstWhere(
+        (role) => role.toString() == map['role'] as String,
+      ),
+    );
+  }
+
 }
