fa75de94ca4c1825964bded9ddf7a5cb13b065d0
Kehinde <ajolayemi82@gmail.com>
2024-02-19 12:06:54 +0100

    added provider container

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,18 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
+  // Move elsewhere later
+  final providerContainer = ProviderContainer(
+    overrides: [
+      settingsControllerProvider.overrideWithValue(settingsController),
+    ],
+  );
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
   runApp(
-     ProviderScope(
-      overrides: [
-        settingsControllerProvider.overrideWithValue(settingsController),
-      ],
+    UncontrolledProviderScope(
+      container: providerContainer,
       child: const MyApp(),
     ),
   );
