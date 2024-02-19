8b1c3c8ad2d0af37b3cf5b7ad9abd875ae784fcd
Kehinde <ajolayemi82@gmail.com>
2024-02-19 12:10:28 +0100

    did some refactoring here

--- lib/src/features/user_companies/presentation/user_company_list/user_companies_list_screen.dart
+++ lib/src/features/user_companies/presentation/user_company_list/user_companies_list_screen.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:irrigazione_iot/src/config/routes/app_router.dart';
-import 'package:irrigazione_iot/src/features/user_companies/data/company_repository.dart';
 import 'package:irrigazione_iot/src/features/user_companies/data/user_companies_repository.dart';
 import 'package:irrigazione_iot/src/features/user_companies/domain/company.dart';
 import 'package:irrigazione_iot/src/features/user_companies/presentation/user_company_list/company_logo.dart';
+import 'package:irrigazione_iot/src/features/user_companies/presentation/user_company_list/user_companies_controller.dart';
+import 'package:irrigazione_iot/src/utils/async_value_ui.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/async_value_widget.dart';
 
@@ -21,43 +22,52 @@ class _UserCompaniesListScreenState
     extends ConsumerState<UserCompaniesListScreen> {
   @override
   Widget build(BuildContext context) {
+    ref.listen(
+      userCompaniesControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+    final state = ref.watch(userCompaniesControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text(context.loc.chooseCompany),
       ),
-      body: SafeArea(
-        child: Consumer(
-          builder: (context, ref, child) {
-            final userCompanies = ref.watch(userCompaniesStreamProvider);
-            return AsyncValueWidget<List<Company>>(
-              value: userCompanies,
-              data: (userCompanies) => ListView.builder(
-                itemBuilder: (context, index) {
-                  final company = userCompanies[index];
-                  return InkWell(
-                    onTap: () {
-                      ref.read(tappedCompanyProvider.notifier).state = company;
-                      context.goNamed(AppRoute.home.name);
-                    },
-                    child: ListTile(
-                      leading: CompanyLogo(
-                        imageUrl: company.imageUrl,
-                      ),
-                      title: Text(
-                        company.name,
-                      ),
-                      trailing: const Icon(
-                        Icons.arrow_forward_ios,
-                      ),
+      body: state.isLoading
+          ? const CircularProgressIndicator.adaptive()
+          : SafeArea(
+              child: Consumer(
+                builder: (context, ref, child) {
+                  final userCompanies = ref.watch(userCompaniesStreamProvider);
+                  return AsyncValueWidget<List<Company>>(
+                    value: userCompanies,
+                    data: (userCompanies) => ListView.builder(
+                      itemBuilder: (context, index) {
+                        final company = userCompanies[index];
+                        return InkWell(
+                          onTap: () {
+                            ref
+                                .read(userCompaniesControllerProvider.notifier)
+                                .updateTappedCompany(company);
+                            context.goNamed(AppRoute.home.name);
+                          },
+                          child: ListTile(
+                            leading: CompanyLogo(
+                              imageUrl: company.imageUrl,
+                            ),
+                            title: Text(
+                              company.name,
+                            ),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: userCompanies.length,
                     ),
                   );
                 },
-                itemCount: userCompanies.length,
               ),
-            );
-          },
-        ),
-      ),
+            ),
     );
   }
 }
