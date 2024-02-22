1f14c62e23efd0ecff0c45115d71131a7a539dce
jolayemi <ajolayemi82@gmail.com>
2024-02-22 19:10:18 +0100

    did first implementation of fakePumpRepository

---
+++ lib/src/features/pumps/data/fake_pump_repository.dart
@@ -0,0 +1,49 @@
+import 'package:irrigazione_iot/src/config/mock/fake_pumps.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/pump_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/domain/pump.dart';
+import 'package:irrigazione_iot/src/utils/in_memory_store.dart';
+
+class FakePumpRepository implements PumpRepository {
+
+
+  final _fakePumps = InMemoryStore<List<Pump>>(kFakePumps);
+
+  static Pump? _getPump(List<Pump> pumps, String pumpId) {
+    try {
+      return pumps.firstWhere((pump) => pump.id == pumpId);
+    } catch (e) {
+      return null;
+    }
+  }
+  static List<Pump> _getCompanyPumps(List<Pump> pumps, String companyId) {
+    return pumps.where((pump) => pump.companyId == companyId).toList();
+  }
+
+  @override
+  Future<List<Pump>> getCompanyPumps(String companyId) {
+    return Future.value(_getCompanyPumps(_fakePumps.value, companyId));
+  }
+
+  @override
+  Future<bool> getPumpStatus(String pumpId) {
+    // TODO: implement getPumpStatus
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> togglePumpStatus(String pumpId, bool status) {
+    // TODO: implement togglePumpStatus
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<Pump>> watchCompanyPumps(String companyId) {
+    return _fakePumps.stream.map((pumps) => _getCompanyPumps(pumps, companyId));
+  }
+
+  @override
+  Stream<bool> watchPumpStatus(String pumpId) {
+    // TODO: implement watchPumpStatus
+    throw UnimplementedError();
+  }
+}
