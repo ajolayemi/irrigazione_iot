e33f234178f6229dc6e1d6b25d15d868b7570cff
jolayemi <ajolayemi82@gmail.com>
2024-02-25 13:27:26 +0100

    defined new providers and added new abstract methods

--- lib/src/features/pumps/data/pump_status_repository.dart
+++ lib/src/features/pumps/data/pump_status_repository.dart
@@ -1,4 +1,6 @@
 import 'package:irrigazione_iot/src/features/pumps/data/fake_pump_status_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/pump_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/domain/pump.dart';
 import 'package:irrigazione_iot/src/features/pumps/domain/pump_status.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -8,8 +10,9 @@ abstract class PumpStatusRepository {
   Stream<PumpStatus> watchPumpStatus(String pumpId);
   Future<PumpStatus> getPumpStatus(String pumpId);
   Future<void> togglePumpStatus(String pumpId, String status);
+  Stream<DateTime?> watchLastDispensation(Pump pump);
+  Future<DateTime?> getLastDispensation(Pump pump);
 }
-
 
 @Riverpod(keepAlive: true)
 PumpStatusRepository pumpStatusRepository(PumpStatusRepositoryRef ref) {
@@ -30,7 +33,28 @@ Future<PumpStatus> pumpStatusFuture(PumpStatusFutureRef ref, String pumpId) {
 }
 
 @riverpod
-Future<void> pumpStatusToggle(PumpStatusToggleRef ref, String pumpId, String status) {
+Future<void> pumpStatusToggle(
+    PumpStatusToggleRef ref, String pumpId, String status) {
   final pumpStatusRepository = ref.watch(pumpStatusRepositoryProvider);
   return pumpStatusRepository.togglePumpStatus(pumpId, status);
+}
+
+@riverpod
+Stream<DateTime?> lastDispensationStream(
+    LastDispensationStreamRef ref, String pumpId) {
+  final availablePumps = ref.watch(companyPumpsStreamProvider).value;
+  if (availablePumps == null) return const Stream.empty();
+  final pump = availablePumps.firstWhere((pump) => pump.id == pumpId);
+  final pumpStatusRepository = ref.watch(pumpStatusRepositoryProvider);
+  return pumpStatusRepository.watchLastDispensation(pump);
+}
+
+@riverpod
+Future<DateTime?> lastDispensationFuture(
+    LastDispensationFutureRef ref, String pumpId) {
+  final availablePumps = ref.watch(companyPumpsStreamProvider).value;
+  if (availablePumps == null) return Future.value(null);
+  final pump = availablePumps.firstWhere((pump) => pump.id == pumpId);
+  final pumpStatusRepository = ref.watch(pumpStatusRepositoryProvider);
+  return pumpStatusRepository.getLastDispensation(pump);
 }
