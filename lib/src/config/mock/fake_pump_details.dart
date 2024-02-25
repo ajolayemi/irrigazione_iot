c1780d4ebe5fd35e5732b4f53d19b5a903bb660f
jolayemi <ajolayemi82@gmail.com>
2024-02-25 13:57:31 +0100

    refactored fake pump details list

--- lib/src/config/mock/fake_pump_details.dart
+++ lib/src/config/mock/fake_pump_details.dart
@@ -4,16 +4,19 @@ import 'package:irrigazione_iot/src/config/mock/fake_pumps.dart';
 import 'package:irrigazione_iot/src/features/pumps/domain/pump_details.dart';
 
 List<PumpDetails> _generateFakePumpDetails() {
-  return List.generate(
-    kFakePumps.length,
-    (index) => PumpDetails(
-      pump: kFakePumps[index],
-      totalLitresDispensed: Random().nextInt(5000),
-      lastDispensation: DateTime.now().subtract(
-        Duration(days: Random().nextInt(30)),
-      ),
-    ),
-  );
+  return List.generate(kFakePumps.length, (index) {
+    final pump = kFakePumps[index];
+    return PumpDetails(
+      capacityInVolume: pump.capacityInVolume,
+      commandForOff: pump.commandForOff,
+      commandForOn: pump.commandForOn,
+      companyId: pump.companyId,
+      consumeRateInKw: pump.consumeRateInKw,
+      id: pump.id,
+      name: pump.name,
+      totalLitresDispensed: Random().nextInt(5000), 
+    );
+  });
 }
 
 final kFakePumpDetails = _generateFakePumpDetails();
