1bac26a2d22a1f2772ae9309050c6cc6913be753
jolayemi <ajolayemi82@gmail.com>
2024-03-01 11:36:23 +0100

    implemented parent class methods

--- lib/src/features/pumps/data/fake_pump_repository.dart
+++ lib/src/features/pumps/data/fake_pump_repository.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:irrigazione_iot/src/config/mock/fake_pumps.dart';
 import 'package:irrigazione_iot/src/features/pumps/data/pump_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/domain/pump.dart';
+import 'package:irrigazione_iot/src/features/user_companies/domain/company.dart';
 import 'package:irrigazione_iot/src/utils/delay.dart';
 import 'package:irrigazione_iot/src/utils/in_memory_store.dart';
 
@@ -45,15 +46,57 @@ class FakePumpRepository implements PumpRepository {
   }
 
   @override
-  Future<Pump?> createPump(Pump pump) async {
+  Future<Pump?> createPump(Pump pump, CompanyID companyId) async {
+    // No validation is done here, the validation is done in the form
     await delay(true);
-    debugPrint("creating pump");
+    final finalPump = pump.copyWith(
+        id: '${_fakePumps.value.length + 1}', companyId: companyId);
+    final currentPumps = [..._fakePumps.value];
+    debugPrint('currentPumps length: ${currentPumps.length}');
+    currentPumps.add(finalPump);
+    debugPrint('currentPumps length after: ${currentPumps.length}');
+    _fakePumps.value = currentPumps;
+
+    return Future.value(pump);
+  }
+
+  @override
+  Future<Pump?> updatePump(Pump pump, CompanyID companyId) async {
+    // No validation is done here, the validation is done in the form
+    await delay(true);
+    final currentPumps = _fakePumps.value;
+    final pumpIndex = currentPumps.indexWhere((p) => p.id == pump.id);
+    if (pumpIndex > 0) {
+      currentPumps[pumpIndex] = pump;
+      _fakePumps.value = currentPumps;
+      return Future.value(pump);
+    }
     return Future.value(null);
   }
 
   @override
-  Future<Pump?> updatePump(Pump pump) async {
-    await delay(true);
-    return Future.value(null);
+  Stream<List<String?>> watchCompanyUsedPumpNames(CompanyID companyId) {
+    return _fakePumps.stream.map(
+      (pumps) =>
+          _getCompanyPumps(pumps, companyId).map((pump) => pump.name).toList(),
+    );
+  }
+
+  @override
+  Stream<List<String?>> watchCompanyUsedPumpOffCommands(CompanyID companyId) {
+    return _fakePumps.stream.map(
+      (pumps) => _getCompanyPumps(pumps, companyId)
+          .map((pump) => pump.commandForOff)
+          .toList(),
+    );
+  }
+
+  @override
+  Stream<List<String?>> watchCompanyUsedPumpOnCommands(CompanyID companyId) {
+    return _fakePumps.stream.map(
+      (pumps) => _getCompanyPumps(pumps, companyId)
+          .map((pump) => pump.commandForOn)
+          .toList(),
+    );
   }
 }
