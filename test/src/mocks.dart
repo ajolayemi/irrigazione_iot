5f3d1777c27a96f8c6bfc10ed3fcd43e0f77e57b
jolayemi <ajolayemi82@gmail.com>
2024-03-03 18:03:37 +0100

    wrote test for user companies controller

--- test/src/mocks.dart
+++ test/src/mocks.dart
@@ -1,7 +1,9 @@
 import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
+import 'package:irrigazione_iot/src/features/user_companies/data/selected_company_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthRepository extends Mock implements AuthRepository {}
+class MockSelectedCompanyRepository extends Mock implements SelectedCompanyRepository {}
 
 class Listener<T> extends Mock {
   void call(T? previous, T next);
