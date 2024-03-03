aac001fff2c56e88aaa90244c0978dcf7814f262
jolayemi <ajolayemi82@gmail.com>
2024-03-03 14:58:56 +0100

    wrote test for fake company repository

--- lib/src/features/user_companies/data/fake_company_repository.dart
+++ lib/src/features/user_companies/data/fake_company_repository.dart
@@ -1,12 +1,17 @@
 import 'package:irrigazione_iot/src/config/mock/fake_companies_list.dart';
 import 'package:irrigazione_iot/src/features/user_companies/data/company_repository.dart';
 import 'package:irrigazione_iot/src/features/user_companies/domain/company.dart';
+import 'package:irrigazione_iot/src/utils/delay.dart';
 import 'package:irrigazione_iot/src/utils/in_memory_store.dart';
 
 class FakeCompanyRepository implements CompanyRepository {
+  FakeCompanyRepository({this.addDelay = true});
+
+  final bool addDelay;
   final _companies = InMemoryStore<List<Company>>(kFakeCompanies);
   @override
-  Future<Company?> fetchCompany(CompanyID companyId) {
+  Future<Company?> fetchCompany(CompanyID companyId) async {
+    await delay(addDelay);
     return Future.value(_getCompany(_companies.value, companyId));
   }
 
