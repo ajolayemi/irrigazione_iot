a60dd4898fb0bc1421bad96cada4648b3a213953
jolayemi <ajolayemi82@gmail.com>
2024-03-04 15:16:17 +0100

    wrote test for PumpStatusSwitchController

--- test/src/mocks.dart
+++ test/src/mocks.dart
@@ -1,6 +1,7 @@
 import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/application/add_update_pump_service.dart';
 import 'package:irrigazione_iot/src/features/pumps/data/fake_pump_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/fake_pump_status_repository.dart';
 import 'package:irrigazione_iot/src/features/user_companies/data/selected_company_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -12,6 +13,8 @@ class MockSelectedCompanyRepository extends Mock
 class MockPumpRepository extends Mock implements FakePumpRepository {}
 
 class MockAddUpdatePumpService extends Mock implements AddUpdatePumpService {}
+
+class MockPumpStatusRepository extends Mock implements FakePumpStatusRepository{}
 
 class Listener<T> extends Mock {
   void call(T? previous, T next);
