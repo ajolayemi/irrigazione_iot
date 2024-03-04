756085ec80ba8f9c7cf2f730079fde49add71ea2
jolayemi <ajolayemi82@gmail.com>
2024-03-04 19:23:33 +0100

    wrote controller to handle pump deletion

---
+++ lib/src/features/pumps/presentation/pump_status/pump_status_tile_wid_controller.dart
@@ -0,0 +1,20 @@
+import 'package:irrigazione_iot/src/features/pumps/data/pump_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/domain/pump.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'pump_status_tile_wid_controller.g.dart';
+
+@riverpod
+class PumpStatusTileWidgetController extends _$PumpStatusTileWidgetController {
+  @override
+  FutureOr<void> build() {
+    // nothing to do
+  }
+
+  Future<bool> confirmDismiss(PumpID pumpId) async {
+    final pumpRepository = ref.read(pumpRepositoryProvider);
+    state = const AsyncLoading<void>();
+    state = await AsyncValue.guard(() => pumpRepository.deletePump(pumpId));
+    return !state.hasError;
+  }
+}
---
+++ lib/src/features/pumps/presentation/pump_status/pump_status_tile_wid_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pump_status_tile_wid_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pumpStatusTileWidgetControllerHash() =>
+    r'44de5ec1f879970e143aa1538dc4d84c3c3c0cb9';
+
+/// See also [PumpStatusTileWidgetController].
+@ProviderFor(PumpStatusTileWidgetController)
+final pumpStatusTileWidgetControllerProvider = AutoDisposeAsyncNotifierProvider<
+    PumpStatusTileWidgetController, void>.internal(
+  PumpStatusTileWidgetController.new,
+  name: r'pumpStatusTileWidgetControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$pumpStatusTileWidgetControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PumpStatusTileWidgetController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
