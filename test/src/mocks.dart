c1f7709c3b173abb469f2b9595d2971b3adcad16
jolayemi <ajolayemi82@gmail.com>
2024-03-04 09:14:50 +0100

    wrote test for AddUpdatePumpService

--- test/src/mocks.dart
+++ test/src/mocks.dart
@@ -1,10 +1,11 @@
 import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/fake_pump_repository.dart';
 import 'package:irrigazione_iot/src/features/user_companies/data/selected_company_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthRepository extends Mock implements AuthRepository {}
 class MockSelectedCompanyRepository extends Mock implements SelectedCompanyRepository {}
-
+class MockPumpRepository extends Mock implements FakePumpRepository {}
 class Listener<T> extends Mock {
   void call(T? previous, T next);
 }
