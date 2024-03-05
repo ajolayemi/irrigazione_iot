208eb239a95f85b1db9ec7e48468ac16a52f4c20
jolayemi <ajolayemi82@gmail.com>
2024-03-05 20:05:20 +0100

    wrote controller to control sector switch

---
+++ lib/src/features/sectors/presentation/sector_switch_controller.dart
@@ -0,0 +1,34 @@
+import 'package:irrigazione_iot/src/features/sectors/data/sector_status_repository.dart';
+import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
+import 'package:irrigazione_iot/src/utils/custom_controller_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'sector_switch_controller.g.dart';
+
+@riverpod
+class SectorSwitchController extends _$SectorSwitchController {
+  final initState = const CustomControllerState(loadingStates: {});
+  @override
+  FutureOr<CustomControllerState> build() {
+    state = AsyncData<CustomControllerState>(initState);
+    return initState;
+  }
+
+  void setLoading(SectorID sectorId, bool isLoading) {
+    state = AsyncData(state.value!.setLoading(sectorId, isLoading));
+  }
+
+  Future<void> toggleStatus(Sector sector, bool status) async {
+    setLoading(sector.id, true);
+    state = const AsyncLoading<CustomControllerState>().copyWithPrevious(state);
+    final statusCommand = sector.getMqttStatusCommand(status);
+    final sectorStatusRepository = ref.read(sectorStatusRepositoryProvider);
+    final value = await AsyncValue.guard(
+        () => sectorStatusRepository.toggleSectorStatus(sector, statusCommand));
+    if (value.hasError) {
+      state = AsyncError(value.error!, StackTrace.current);
+    } else {
+      state = AsyncData<CustomControllerState>(initState);
+    }
+  }
+}
---
+++ lib/src/features/sectors/presentation/sector_switch_controller.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sector_switch_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sectorSwitchControllerHash() =>
+    r'fcd76e24128a535c48a7aa69ebc030be8219df24';
+
+/// See also [SectorSwitchController].
+@ProviderFor(SectorSwitchController)
+final sectorSwitchControllerProvider = AutoDisposeAsyncNotifierProvider<
+    SectorSwitchController, CustomControllerState>.internal(
+  SectorSwitchController.new,
+  name: r'sectorSwitchControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sectorSwitchControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SectorSwitchController
+    = AutoDisposeAsyncNotifier<CustomControllerState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
