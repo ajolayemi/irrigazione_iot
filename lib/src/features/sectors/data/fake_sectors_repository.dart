a59780cfc3ba60f143a0eaa7f4b4adf0199d6427
jolayemi <ajolayemi82@gmail.com>
2024-03-06 10:47:01 +0100

    implemented new methods from abstract class

--- lib/src/features/sectors/data/fake_sectors_repository.dart
+++ lib/src/features/sectors/data/fake_sectors_repository.dart
@@ -1,7 +1,9 @@
+import 'package:collection/collection.dart';
 import 'package:irrigazione_iot/src/config/mock/fake_sectors.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
 import 'package:irrigazione_iot/src/features/user_companies/domain/company.dart';
+import 'package:irrigazione_iot/src/utils/delay.dart';
 import 'package:irrigazione_iot/src/utils/in_memory_store.dart';
 
 class FakeSectorsRepository extends SectorsRepository {
@@ -40,7 +42,22 @@ class FakeSectorsRepository extends SectorsRepository {
         .map((sectors) => _getSectors(sectors, companyId));
   }
 
-  List<Sector?> _getSectors(List<Sector?> sectors, CompanyID companyId) {
+  static List<Sector?> _getSectors(List<Sector?> sectors, CompanyID companyId) {
     return sectors.where((sector) => sector?.companyId == companyId).toList();
+  }
+
+  static Sector? _getSector(List<Sector?> sectors, SectorID sectorID) {
+    return sectors.firstWhereOrNull((sector) => sector?.id == sectorID);
+  }
+
+  @override
+  Stream<Sector?> watchSector(SectorID sectorID) {
+    return _sectorsState.stream.map((sectors) => _getSector(sectors, sectorID));
+  }
+
+  @override
+  Future<Sector?> fetchSector(SectorID sectorID) async {
+    await delay(addDelay);
+    return Future.value(_getSector(_sectorsState.value, sectorID));
   }
 }
