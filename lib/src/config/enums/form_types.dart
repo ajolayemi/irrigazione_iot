698c4fd1887ea97e3d96215e90c8d1462592f966
jolayemi <ajolayemi82@gmail.com>
2024-03-06 19:25:01 +0100

    added extension on GenericFormTypes enum

--- lib/src/config/enums/form_types.dart
+++ lib/src/config/enums/form_types.dart
@@ -2,3 +2,9 @@ enum GenericFormTypes {
   add,
   update,
 }
+
+
+extension GenericFormTypesX on GenericFormTypes {
+  bool isUpdating() => this == GenericFormTypes.update;
+  bool isAdding() => this == GenericFormTypes.add;
+}
