0449d881ac463ab34df4845276b795224a7ec37b
jolayemi <ajolayemi82@gmail.com>
2024-03-06 10:51:15 +0100

    built first part of sector details screen

---
+++ lib/src/features/sectors/presentation/sector_details/sector_details.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/config/enums/roles.dart';
+import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
+import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
+import 'package:irrigazione_iot/src/features/sectors/presentation/sector_details/survey_details_screen_content.dart';
+import 'package:irrigazione_iot/src/features/sectors/presentation/sector_list/sectors_list_tile_skeleton.dart';
+import 'package:irrigazione_iot/src/features/user_companies/data/user_companies_repository.dart';
+import 'package:irrigazione_iot/src/widgets/app_bar_icon_buttons.dart';
+import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
+import 'package:irrigazione_iot/src/widgets/async_value_widget.dart';
+import 'package:irrigazione_iot/src/widgets/empty_placeholder_widget.dart';
+
+class SectorDetailsScreen extends ConsumerWidget {
+  const SectorDetailsScreen({
+    super.key,
+    required this.sectorID,
+  });
+
+  final SectorID sectorID;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final canEdit = ref.watch(companyUserRoleProvider).valueOrNull?.canEdit;
+    final sectorData = ref.watch(sectorStreamProvider(sectorID));
+    return SafeArea(
+      child: Scaffold(
+        body: AsyncValueSliverWidget(
+          value: sectorData,
+          data: (sector) {
+            if (sector == null) {
+              return const EmptyPlaceholderWidget(message: 'aaaa');
+            } // todo replace widget with the right one
+
+            return CustomScrollView(
+              slivers: [
+                AppSliverBar(
+                  title: sector.name,
+                  actions: [
+                    AppBarIconButton(
+                      onPressed: () {},
+                      icon: Icons.edit,
+                      isVisibile: canEdit,
+                    )
+                  ],
+                ),
+                SurveyDetailsScreenContents(
+                  sector: sector,
+                )
+              ],
+            );
+          },
+          loading: () => const SectorsListTileSkeletonNonSliver(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/sectors/presentation/sector_details/survey_details_screen_content.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/sectors/data/sector_status_repository.dart';
+import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
+import 'package:irrigazione_iot/src/features/sectors/domain/sector_status.dart';
+import 'package:irrigazione_iot/src/features/sectors/presentation/sector_switch.dart';
+import 'package:irrigazione_iot/src/utils/date_formatter.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/details_tile_widget.dart';
+import 'package:irrigazione_iot/src/widgets/responsive_details_card.dart';
+
+class SurveyDetailsScreenContents extends ConsumerWidget {
+  const SurveyDetailsScreenContents(
+      {super.key, required this.sector, this.sectorStatus});
+
+  final Sector sector;
+  final SectorStatus? sectorStatus;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SliverList(
+      delegate: SliverChildListDelegate.fixed(
+        [
+          ResponsiveDetailsCard(
+            child: Consumer(
+              builder: (context, ref, child) {
+                final status =
+                    ref.watch(sectorStatusStreamProvider(sector)).valueOrNull ??
+                        false;
+                return DetailTileWidget(
+                  title: context.loc.statusListTileTitle,
+                  subtitle: status
+                      ? context.loc.onStatusValue
+                      : context.loc.offStatusValue,
+                  trailing: SectorSwitch(
+                    sector: sector,
+                  ),
+                );
+              },
+            ),
+          ),
+          ResponsiveDetailsCard(child: Consumer(builder: (context, ref, child) {
+            final lastIrrigated = ref
+                .watch(sectorLastIrrigatedStreamProvider(sector))
+                .valueOrNull;
+            final dateFormatter = ref.watch(dateFormatWithTimeProvider);
+            return DetailTileWidget(
+              title: context.loc.sectorLastIrrigationForTile,
+              subtitle: lastIrrigated == null
+                  ? context.loc.sectorLastIrrigationEmpty
+                  : dateFormatter.format(lastIrrigated),
+            );
+          })),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorSpecie,
+              subtitle: sector.availableSpecie,
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorVariety,
+              subtitle: sector.specieVariety,
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorNumberOfPlants,
+              subtitle: sector.numOfPlants.toString(),
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorUnitConsumptionPerHour,
+              subtitle: sector.waterConsumptionPerHourByPlant.toString(),
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorTotalConsumption,
+              subtitle: sector.totalWaterConsumption.toString(),
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorIrrigationSystem,
+              subtitle: sector.irrigationSystemType.uiName,
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorIrrigationSource,
+              subtitle: sector.irrigationSource.uiName,
+            ),
+          ),
+          ResponsiveDetailsCard(
+            child: DetailTileWidget(
+              title: context.loc.sectorNotes,
+              subtitle: sector.notes,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
