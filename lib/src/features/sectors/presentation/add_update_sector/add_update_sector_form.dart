c71380a52c4cf3b85ea1ede84f81c9de800b5c9d
jolayemi <ajolayemi82@gmail.com>
2024-03-06 19:28:42 +0100

    defined widget to display form contents to add or update sectors

--- lib/src/features/sectors/presentation/add_update_sector/add_update_sector_form.dart
+++ lib/src/features/sectors/presentation/add_update_sector/add_update_sector_form.dart
@@ -1,8 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:irrigazione_iot/src/config/enums/form_types.dart';
+import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
+import 'package:irrigazione_iot/src/features/sectors/presentation/add_update_sector/add_update_sector_form_contents.dart';
+
 class AddUpdateSectorForm extends StatelessWidget {
-  const AddUpdateSectorForm({super.key});
+  const AddUpdateSectorForm({
+    super.key,
+    required this.formType,
+    this.sectorId,
+  });
+
+  final GenericFormTypes formType;
+  final SectorID? sectorId;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: AddUpdateSectorFormContents(
+        formType: formType,
+        sectorId: sectorId,
+      )
+    );
   }
 }
