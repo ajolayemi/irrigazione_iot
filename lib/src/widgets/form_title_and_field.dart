a79c0c605bea1276a4184412758855e196135c65
jolayemi <ajolayemi82@gmail.com>
2024-03-06 19:27:03 +0100

    refactored form title and field widget to accept new parameters

--- lib/src/widgets/form_title_and_field.dart
+++ lib/src/widgets/form_title_and_field.dart
@@ -4,22 +4,27 @@ import 'package:irrigazione_iot/src/constants/app_sizes.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class FormTitleAndField extends StatelessWidget {
-  const FormTitleAndField(
-      {super.key,
-      required this.fieldKey,
-      required this.fieldTitle,
-      required this.fieldController,
-      this.fieldHintText,
-      this.autovalidateMode,
-      this.autoCorrect,
-      this.keyboardType,
-      this.keyboardAppearance,
-      this.onEditingComplete,
-      this.inputFormatters,
-      this.validator,
-      this.textInputAction,
-      this.obscureText,
-      this.enabled});
+  const FormTitleAndField({
+    super.key,
+    required this.fieldKey,
+    required this.fieldTitle,
+    required this.fieldController,
+    this.fieldHintText,
+    this.autovalidateMode,
+    this.autoCorrect,
+    this.keyboardType,
+    this.keyboardAppearance,
+    this.onEditingComplete,
+    this.inputFormatters,
+    this.validator,
+    this.textInputAction,
+    this.obscureText,
+    this.enabled,
+    this.maxLines,
+    this.suffixIcon,
+    this.onTap,
+    this.canRequestFocus
+  });
 
   final Key fieldKey;
   final String fieldTitle;
@@ -32,9 +37,13 @@ class FormTitleAndField extends StatelessWidget {
   final VoidCallback? onEditingComplete;
   final List<TextInputFormatter>? inputFormatters;
   final String? Function(String?)? validator;
+  final VoidCallback? onTap;
   final TextInputAction? textInputAction;
+  final int? maxLines;
   final bool? obscureText;
   final bool? enabled;
+  final Widget? suffixIcon;
+  final bool? canRequestFocus;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,10 +57,14 @@ class FormTitleAndField extends StatelessWidget {
         TextFormField(
           key: fieldKey,
           controller: fieldController,
+          onTap: onTap,
+          canRequestFocus: canRequestFocus ?? true,
+          //maxLines: maxLines,
           decoration: InputDecoration(
             hintText: fieldHintText,
             errorMaxLines: 3,
             enabled: enabled ?? true,
+            suffixIcon: suffixIcon,
           ),
           autovalidateMode:
               autovalidateMode ?? AutovalidateMode.onUserInteraction,
