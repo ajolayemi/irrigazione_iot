b2c4c211199610b0f71381d999f74ec9009aecab
jolayemi <ajolayemi82@gmail.com>
2024-03-06 10:47:36 +0100

    - moved widgets elsewhere - referenced new widgets - renamed old ones

--- lib/src/features/pumps/presentation/pump_details/pump_details_sliver_list.dart
+++ lib/src/features/pumps/presentation/pump_details/pump_details_sliver_list.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:irrigazione_iot/src/constants/app_sizes.dart';
-import 'package:irrigazione_iot/src/constants/breakpoints.dart';
 import 'package:irrigazione_iot/src/features/pumps/data/pump_details_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/domain/pump.dart';
 import 'package:irrigazione_iot/src/features/pumps/presentation/pump_status/pump_status_tile_wid.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
-import 'package:irrigazione_iot/src/widgets/responsive_center.dart';
+import 'package:irrigazione_iot/src/widgets/details_tile_widget.dart';
+import 'package:irrigazione_iot/src/widgets/responsive_details_card.dart';
 
 class PumpDetailsSliverList extends ConsumerWidget {
   const PumpDetailsSliverList({super.key, required this.pump});
@@ -17,20 +16,20 @@ class PumpDetailsSliverList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return SliverList(
       delegate: SliverChildListDelegate.fixed([
-        PumpDetailsCard(
+        ResponsiveDetailsCard(
           child: PumpStatusTileWidget(
             pump: pump,
-            title: context.loc.pumpStatusListTileTitle,
+            title: context.loc.statusListTileTitle,
           ),
         ),
-        PumpDetailsCard(
-          child: PumpDetailTileWidget(
+        ResponsiveDetailsCard(
+          child: DetailTileWidget(
             title: 'Capacity in volume',
             subtitle: '${pump.capacityInVolume.toString()} m³',
           ),
         ),
-        PumpDetailsCard(
-          child: PumpDetailTileWidget(
+        ResponsiveDetailsCard(
+          child: DetailTileWidget(
             title: 'Consume rate in kW',
             subtitle: '${pump.consumeRateInKw.toString()} kW',
           ),
@@ -39,8 +38,8 @@ class PumpDetailsSliverList extends ConsumerWidget {
         Consumer(
           builder: (context, ref, child) {
             final litresDispensed = ref.watch(pumpTotalDispensedLitresProvider(pump.id)).value;
-            return PumpDetailsCard(
-              child: PumpDetailTileWidget(
+            return ResponsiveDetailsCard(
+              child: DetailTileWidget(
                 title: 'Total litres dispensed',
                 subtitle: '${litresDispensed.toString()} L',
               ),
@@ -53,53 +52,6 @@ class PumpDetailsSliverList extends ConsumerWidget {
   }
 }
 
-// * Tile to display pump details
-class PumpDetailTileWidget extends StatelessWidget {
-  const PumpDetailTileWidget({
-    super.key,
-    this.title,
-    this.subtitle,
-    this.trailing,
-  });
 
-  final String? title;
-  final String? subtitle;
-  final Widget? trailing;
 
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      subtitleTextStyle:
-          context.textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
-      titleTextStyle: context.textTheme.bodyLarge?.copyWith(
-        color: Colors.transparent.withOpacity(0.8),
-      ),
-      title: Text(title ?? ''),
-      subtitle: Text(subtitle ?? ''),
-      trailing: trailing,
-    );
-  }
-}
 
-// * Card to display pump details
-class PumpDetailsCard extends StatelessWidget {
-  const PumpDetailsCard({
-    super.key,
-    required this.child,
-  });
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return ResponsiveCenter(
-      maxContentWidth: Breakpoint.tablet,
-      padding: const EdgeInsets.all(Sizes.p8),
-      child: Card(
-        elevation: 2,
-        surfaceTintColor: Colors.transparent.withOpacity(0.2),
-        child: child,
-      ),
-    );
-  }
-}
