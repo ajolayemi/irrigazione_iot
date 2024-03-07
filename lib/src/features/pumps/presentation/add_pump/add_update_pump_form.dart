ea05b4eee869c00cd513dcc421d96978be7d0ac3
jolayemi <ajolayemi82@gmail.com>
2024-03-07 18:08:20 +0100

    refactored AddUpdatePumpForm

--- lib/src/features/pumps/presentation/add_pump/add_update_pump_form.dart
+++ lib/src/features/pumps/presentation/add_pump/add_update_pump_form.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/config/enums/form_types.dart';
 import 'package:irrigazione_iot/src/features/pumps/domain/pump.dart';
+import 'package:irrigazione_iot/src/features/pumps/presentation/add_pump/add_update_pump_controller.dart';
 import 'package:irrigazione_iot/src/features/pumps/presentation/add_pump/add_update_pump_form_content.dart';
+import 'package:irrigazione_iot/src/utils/async_value_ui.dart';
 
-class AddUpdatePumpForm extends StatelessWidget {
+class AddUpdatePumpForm extends ConsumerWidget {
   const AddUpdatePumpForm({
     super.key,
     required this.formType,
@@ -14,11 +17,26 @@ class AddUpdatePumpForm extends StatelessWidget {
   final PumpID? pumpId;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: AddUpdatePumpContents(
-        formType: formType,
-        pumpId: pumpId,
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(
+      addUpdatePumpControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+    final isLoading = ref.watch(addUpdatePumpControllerProvider).isLoading;
+    return PopScope(
+      canPop: !isLoading,
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          debugPrint('User exited the form');
+        } else {
+          debugPrint('User tried to exit the form');
+        }
+      },
+      child: Scaffold(
+        body: AddUpdatePumpContents(
+          formType: formType,
+          pumpId: pumpId,
+        ),
       ),
     );
   }
