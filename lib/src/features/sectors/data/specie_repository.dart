6639509d360b1b25d9d72387ca2258936b17c873
jolayemi <ajolayemi82@gmail.com>
2024-03-07 11:05:07 +0100

    added logic to keep specie list alive for 60 seconds

--- lib/src/features/sectors/data/specie_repository.dart
+++ lib/src/features/sectors/data/specie_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:irrigazione_iot/src/features/sectors/data/fake_specie_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -14,6 +16,10 @@ SpecieRepository specieRepository(SpecieRepositoryRef ref) {
 
 @riverpod
 Future<List<String>> specieNamesFuture(SpecieNamesFutureRef ref) {
+  final link = ref.keepAlive();
+  // * keep the previous value in memory for 60 seconds
+  final timer = Timer(const Duration(seconds: 60), link.close);
+  ref.onDispose(() => timer.cancel());
   final specieRepository = ref.watch(specieRepositoryProvider);
   return specieRepository.getSpecieNames();
 }
