752534f09fd3f8add3377d1a34718a56e77dd34a
jolayemi <ajolayemi82@gmail.com>
2024-03-08 12:31:01 +0100

    defined controller to dismiss widgets

---
+++ lib/src/features/sectors/presentation/sector_list/dismiss_sector_controller.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
+import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+
+part 'dismiss_sector_controller.g.dart';
+
+@riverpod
+class DismissSectorController extends _$DismissSectorController {
+  @override
+  FutureOr<void> build() {
+    // nothing to do
+  }
+
+  Future<bool> confirmDismiss(SectorID sectorId) async {
+    final sectorRepository = ref.read(sectorsRepositoryProvider);
+    state = const AsyncLoading<void>();
+    final res = await AsyncValue.guard(() => sectorRepository.deleteSector(sectorId));
+    if (res.hasError) {
+      state = AsyncError(res.error!, StackTrace.current);
+      return false;
+    }
+    state = const AsyncData<void>(null);
+    return true;
+  }
+}
---
+++ lib/src/features/sectors/presentation/sector_list/dismiss_sector_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dismiss_sector_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dismissSectorControllerHash() =>
+    r'38dcf96cba8c476c4981a2e7dd3abfaf8cbb30d8';
+
+/// See also [DismissSectorController].
+@ProviderFor(DismissSectorController)
+final dismissSectorControllerProvider =
+    AutoDisposeAsyncNotifierProvider<DismissSectorController, void>.internal(
+  DismissSectorController.new,
+  name: r'dismissSectorControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dismissSectorControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DismissSectorController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
