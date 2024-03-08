8a980987c6d664c978edeed0bdaf5166adeb7762
jolayemi <ajolayemi82@gmail.com>
2024-03-08 11:50:13 +0100

    refactored how list of available sectors are read

--- lib/src/features/sectors/data/sector_repository.dart
+++ lib/src/features/sectors/data/sector_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/fake_sectors_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/domain/sector.dart';
 import 'package:irrigazione_iot/src/features/user_companies/data/selected_company_repository.dart';
@@ -42,16 +43,20 @@ SectorsRepository sectorsRepository(SectorsRepositoryRef ref) {
 }
 
 @riverpod
-Stream<List<Sector?>> sectorListStream(
-    SectorListStreamRef ref, CompanyID companyId) {
+Stream<List<Sector?>> sectorListStream(SectorListStreamRef ref) {
   final sectorsRepository = ref.read(sectorsRepositoryProvider);
+  final companyId = ref.watch(currentTappedCompanyProvider).valueOrNull?.id;
+  debugPrint('company id: $companyId');
+  if (companyId == null) return const Stream.empty();
   return sectorsRepository.watchSectors(companyId);
 }
 
 @riverpod
 Future<List<Sector?>> sectorListFuture(
-    SectorListFutureRef ref, CompanyID companyId) {
+    SectorListFutureRef ref) {
   final sectorsRepository = ref.read(sectorsRepositoryProvider);
+  final companyId = ref.watch(currentTappedCompanyProvider).valueOrNull?.id;
+  if (companyId == null) return Future.value([]);
   return sectorsRepository.fetchSectors(companyId);
 }
 
