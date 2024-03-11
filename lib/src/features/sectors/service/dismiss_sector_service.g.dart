cc0a3761c2b2388af060c9f159a1a16e0ae9bf9e
jolayemi <ajolayemi82@gmail.com>
2024-03-11 10:11:26 +0100

    defined service class to handle sector dismissal

---
+++ lib/src/features/sectors/service/dismiss_sector_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dismiss_sector_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dismissSectorServiceHash() =>
+    r'328e0f549000ab41b1383a724c0f4d1328317c55';
+
+/// See also [dismissSectorService].
+@ProviderFor(dismissSectorService)
+final dismissSectorServiceProvider = Provider<DismissSectorService>.internal(
+  dismissSectorService,
+  name: r'dismissSectorServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dismissSectorServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DismissSectorServiceRef = ProviderRef<DismissSectorService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
