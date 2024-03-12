324d6991560797d3dbd2b1d9857e754429f1cd8d
jolayemi <ajolayemi82@gmail.com>
2024-03-12 18:45:02 +0100

    did a complete refactor of pump switch

--- lib/src/features/pumps/screen/pump_status/pump_status_switch.dart
+++ lib/src/features/pumps/screen/pump_status/pump_status_switch.dart
@@ -1,68 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/pump_status_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
-import 'package:irrigazione_iot/src/features/pumps/model/pump_status.dart';
 import 'package:irrigazione_iot/src/features/pumps/screen/pump_status/pump_status_switch_controller.dart';
+import 'package:irrigazione_iot/src/utils/custom_controller_state.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
 
-class PumpStatusSwitchWidget extends ConsumerWidget {
-  const PumpStatusSwitchWidget({
+class PumpSwitch extends ConsumerWidget {
+  const PumpSwitch({
     super.key,
     required this.pump,
-    this.pumpStatus,
   });
 
   final Pump pump;
-  final PumpStatus? pumpStatus;
 
   // * Keys for testing using find.byKey
   static Key pumpStatusSwitchKey(Pump pump) =>
       Key('pumpStatusSwitchKey_${pump.id}');
 
+  Future<void> _toggleSwitch(
+    BuildContext context,
+    WidgetRef ref,
+    bool status,
+  ) async {
+    final loc = context.loc;
+    final update = await showAlertDialog(
+      context: context,
+      content: status
+          ? loc.onStatusUpdateAlertDialogContent(pump.name)
+          : loc.offStatusUpdateAlertDialogContent(pump.name),
+      title: loc.genericAlertDialogTitle,
+      cancelActionText: loc.alertDialogCancel,
+      defaultActionText: status
+          ? loc.onStatusDialogConfirmButtonTitle
+          : loc.offStatusDialogConfirmButtonTitle,
+    );
+
+    if (update == null || update == false) return;
+
+    ref
+        .read(pumpStatusSwitchControllerProvider.notifier)
+        .toggleStatus(pump, status);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final thisPumpStatusIsLoading = ref
+        .watch(pumpStatusSwitchControllerProvider)
+        .stateWithIdIsLoading(pump.id);
 
-    // todo replace this with extension method
-    final loading = ref.watch(pumpStatusSwitchControllerProvider.select(
-        (state) =>
-            (state.value?.stateWithIdIsLoading(pump.id) ?? false) &&
-            !state.hasError));
+    final globalLoadingState =
+        ref.watch(pumpStatusSwitchControllerProvider).isGlobalLoading;
+    final isSwitchedOn =
+        ref.watch(pumpStatusStreamProvider(pump)).valueOrNull ?? false;
 
-    final aPumpIsCurrentlyLoading = ref.watch(
-        pumpStatusSwitchControllerProvider.select(
-            (state) => (state.value?.isLoading ?? false) && !state.hasError));
-    final valueForSwitch = pumpStatus?.translateStatusToBoolean(
-      pump,
-      pumpStatus?.status ?? '',
-    );
-    return loading
+    return thisPumpStatusIsLoading
         ? const CircularProgressIndicator.adaptive()
         : Switch.adaptive(
             key: pumpStatusSwitchKey(pump),
-            value: valueForSwitch ?? false,
-            onChanged: aPumpIsCurrentlyLoading
+            value: isSwitchedOn,
+            onChanged: globalLoadingState
                 ? null
-                : (value) async {
-                    final update = await showAlertDialog(
-                      context: context,
-                      content: value
-                          ? context.loc
-                              .onStatusUpdateAlertDialogContent(pump.name)
-                          : context.loc
-                              .offStatusUpdateAlertDialogContent(pump.name),
-                      title: context.loc.genericAlertDialogTitle,
-                      cancelActionText: context.loc.alertDialogCancel,
-                      defaultActionText: value
-                          ? context.loc.onStatusDialogConfirmButtonTitle
-                          : context.loc.offStatusDialogConfirmButtonTitle,
-                    );
-                    if (update == true) {
-                      ref
-                          .read(pumpStatusSwitchControllerProvider.notifier)
-                          .toggleStatus(pump, value);
-                    }
-                  },
+                : (value) => _toggleSwitch(context, ref, value),
           );
   }
 }
