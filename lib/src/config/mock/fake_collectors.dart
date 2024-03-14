f3b9066dc1a4e355b139097fdf184ca1ff6b2952
jolayemi <ajolayemi82@gmail.com>
2024-03-14 08:46:46 +0100

    defined a list of fake collectors

---
+++ lib/src/config/mock/fake_collectors.dart
@@ -0,0 +1,118 @@
+import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
+
+const kFakeCollectors = [
+  Collector(
+    id: '1',
+    name: 'S1 (cisterne)',
+    companyId: '1',
+    filterName: 'filtro s1',
+  ),
+  Collector(
+    id: '2',
+    name: 'S2 (pozzo)',
+    companyId: '1',
+    filterName: 'filtro s2',
+  ),
+  Collector(
+    id: '3',
+    name: "O1 Mx2a",
+    companyId: '1',
+    filterName: 'filtro O1',
+  ),
+  Collector(
+    id: '4',
+    name: "O4 (uliveto)",
+    companyId: '2',
+    filterName: 'filtro O4',
+  ),
+  Collector(
+    id: '5',
+    name: 'S3 (reservoir)',
+    companyId: '1',
+    filterName: 'filter S3',
+  ),
+  Collector(
+    id: '6',
+    name: 'S4 (well)',
+    companyId: '1',
+    filterName: 'filter S4',
+  ),
+  Collector(
+    id: '7',
+    name: 'O2 Mx2b',
+    companyId: '1',
+    filterName: 'filter O2',
+  ),
+  Collector(
+    id: '8',
+    name: 'O5 (olive grove)',
+    companyId: '2',
+    filterName: 'filter O5',
+  ),
+  Collector(
+    id: '9',
+    name: 'O6 (vineyard)',
+    companyId: '2',
+    filterName: 'filter O6',
+  ),
+  Collector(
+    id: '10',
+    name: 'O7 Mx2c',
+    companyId: '2',
+    filterName: 'filter O7',
+  ),
+  Collector(
+    id: '11',
+    name: 'S5 (tank)',
+    companyId: '3',
+    filterName: 'filter S5',
+  ),
+  Collector(
+    id: '12',
+    name: 'S6 (borehole)',
+    companyId: '3',
+    filterName: 'filter S6',
+  ),
+  Collector(
+    id: '13',
+    name: 'O3 Mx2d',
+    companyId: '3',
+    filterName: 'filter O3',
+  ),
+  Collector(
+    id: '14',
+    name: 'O8 (orchard)',
+    companyId: '4',
+    filterName: 'filter O8',
+  ),
+  Collector(
+    id: '15',
+    name: 'O9 (field)',
+    companyId: '4',
+    filterName: 'filter O9',
+  ),
+  Collector(
+    id: '16',
+    name: 'O10 Mx2e',
+    companyId: '4',
+    filterName: 'filter O10',
+  ),
+  Collector(
+    id: '17',
+    name: 'S7 (cistern)',
+    companyId: '5',
+    filterName: 'filter S7',
+  ),
+  Collector(
+    id: '18',
+    name: 'S8 (well)',
+    companyId: '5',
+    filterName: 'filter S8',
+  ),
+  Collector(
+    id: '19',
+    name: 'O11 Mx2f',
+    companyId: '5',
+    filterName: 'filter O11',
+  ),
+];
