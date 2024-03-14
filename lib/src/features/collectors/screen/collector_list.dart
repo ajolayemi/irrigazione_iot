b0e1aebf49fb07fb8a843d5b29e0fc1f31c6c5c9
jolayemi <ajolayemi82@gmail.com>
2024-03-14 08:45:44 +0100

    defined an empty CollectorListScreen

---
+++ lib/src/features/collectors/screen/collector_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CollectorListScreen extends ConsumerWidget {
+  const CollectorListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
