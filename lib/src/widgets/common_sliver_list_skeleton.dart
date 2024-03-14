ad52d27571015a2df5610c00dbd02352c967d6c4
jolayemi <ajolayemi82@gmail.com>
2024-03-14 13:03:07 +0100

    defined a sliver version of CommonSkeletonTile

---
+++ lib/src/widgets/common_sliver_list_skeleton.dart
@@ -0,0 +1,35 @@
+// todo: this should replace all other skeletons in the app
+import 'package:flutter/material.dart';
+import 'package:irrigazione_iot/src/widgets/common_skeleton_tile.dart';
+
+/// A sliver version of the [CommonSkeletonTile]
+class CommonSliverListSkeleton extends StatelessWidget {
+  const CommonSliverListSkeleton({
+    super.key,
+    this.hasLeading = true,
+    this.hasTrailing = true,
+    this.hasSubtitle = true,
+    this.hasTitle = true,
+  });
+
+  final bool hasLeading;
+  final bool hasTrailing;
+  final bool hasSubtitle;
+  final bool hasTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      (context, index) {
+        return  CommonSkeletonTile(
+          hasLeading: hasLeading,
+          hasSubtitle: hasSubtitle,
+          hasTitle: hasTitle,
+          hasTrailing: hasTrailing,
+        );
+      },
+      childCount: 10,
+    ));
+  }
+}
