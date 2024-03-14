c99ac9f4c729c6dee48c3a887f8b6f18992c72d3
jolayemi <ajolayemi82@gmail.com>
2024-03-14 13:04:24 +0100

    built first version of CollectorListScreen

--- lib/src/features/collectors/screen/collector_list_screen.dart
+++ lib/src/features/collectors/screen/collector_list_screen.dart
@@ -1,11 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:irrigazione_iot/src/config/enums/roles.dart';
+import 'package:irrigazione_iot/src/config/routes/app_router.dart';
+import 'package:irrigazione_iot/src/features/collectors/data/collector_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/widgets/empty_collector_widget.dart';
+import 'package:irrigazione_iot/src/features/user_companies/data/user_companies_repository.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
+import 'package:irrigazione_iot/src/widgets/app_bar_icon_buttons.dart';
+import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
+import 'package:irrigazione_iot/src/widgets/async_value_widget.dart';
+import 'package:irrigazione_iot/src/widgets/common_sliver_list_skeleton.dart';
 
 class CollectorListScreen extends ConsumerWidget {
   const CollectorListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final loc = context.loc;
+    final canEdit = ref.watch(companyUserRoleProvider).valueOrNull?.canEdit;
+    final collectors = ref.watch(collectorListStreamProvider);
+    return SafeArea(
+        child: Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          AppSliverBar(
+            title: loc.collectorPageTitle,
+            actions: [
+              AppBarIconButton(
+                onPressed: () => showNotImplementedAlertDialog(
+                  context: context,
+                ),
+                // onPressed: () => context.pushNamed(
+                //   AppRoute.addCollector.name,
+                // ),
+                icon: Icons.add,
+              )
+            ],
+          ),
+          AsyncValueSliverWidget(
+            value: collectors,
+            data: (collectors) {
+              if (collectors.isEmpty) {
+                return const EmptyCollectorWidget();
+              }
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    // collector shouldn't be null if we reach here
+                    final collector = collectors[index]!;
+                    return Text('coming soon...');
+                  },
+                  childCount: collectors.length,
+                ),
+              );
+            },
+            loading: () => const CommonSliverListSkeleton(
+              hasLeading: false,
+              hasSubtitle: false,
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
