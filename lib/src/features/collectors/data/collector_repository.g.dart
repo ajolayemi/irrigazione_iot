617a50b1f1909b8be671d136f700b67cedbf6fe6
jolayemi <ajolayemi82@gmail.com>
2024-03-14 08:44:55 +0100

    defined CollectorRepository

---
+++ lib/src/features/collectors/data/collector_repository.g.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'collector_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$collectorRepositoryHash() =>
+    r'cbb506c96f6fd3348ee9b2b9b0d595b5dba8629a';
+
+/// See also [collectorRepository].
+@ProviderFor(collectorRepository)
+final collectorRepositoryProvider = Provider<CollectorRepository>.internal(
+  collectorRepository,
+  name: r'collectorRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$collectorRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CollectorRepositoryRef = ProviderRef<CollectorRepository>;
+String _$collectorListStreamHash() =>
+    r'b8168ccacd21e04f94a1d95f25f9d2fcdab541f9';
+
+/// See also [collectorListStream].
+@ProviderFor(collectorListStream)
+final collectorListStreamProvider =
+    AutoDisposeStreamProvider<List<Collector?>>.internal(
+  collectorListStream,
+  name: r'collectorListStreamProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$collectorListStreamHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CollectorListStreamRef = AutoDisposeStreamProviderRef<List<Collector?>>;
+String _$collectorListFutureHash() =>
+    r'51bfdb5832092d71554050a6e82b2eb33123d4ce';
+
+/// See also [collectorListFuture].
+@ProviderFor(collectorListFuture)
+final collectorListFutureProvider =
+    AutoDisposeFutureProvider<List<Collector?>>.internal(
+  collectorListFuture,
+  name: r'collectorListFutureProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$collectorListFutureHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CollectorListFutureRef = AutoDisposeFutureProviderRef<List<Collector?>>;
+String _$companyUsedCollectorNamesStreamHash() =>
+    r'290aa66241ae2c8879468df0f90c36fe09ec813a';
+
+/// See also [companyUsedCollectorNamesStream].
+@ProviderFor(companyUsedCollectorNamesStream)
+final companyUsedCollectorNamesStreamProvider =
+    AutoDisposeStreamProvider<List<String?>>.internal(
+  companyUsedCollectorNamesStream,
+  name: r'companyUsedCollectorNamesStreamProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$companyUsedCollectorNamesStreamHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CompanyUsedCollectorNamesStreamRef
+    = AutoDisposeStreamProviderRef<List<String?>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
