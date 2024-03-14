71ba5f8afd9c654a95f50f767390d02de2899037
jolayemi <ajolayemi82@gmail.com>
2024-03-14 12:31:59 +0100

    refactored how fake collector sector list is built

--- lib/src/config/mock/fake_collector_sectors.dart
+++ lib/src/config/mock/fake_collector_sectors.dart
@@ -4,22 +4,18 @@ import 'package:irrigazione_iot/src/config/mock/fake_collectors.dart';
 import 'package:irrigazione_iot/src/config/mock/fake_sectors.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector_sector.dart';
 
-final kFakeCollectorSectors = kFakeSectors.expand((sector) {
-  final companyId = sector.companyId;
-  final companyCollectors = kFakeCollectors
-      .where((collector) => collector.companyId == companyId)
-      .toList();
-
+List<CollectorSector> generateFakeCollectorSectors() {
+  final collectorSectors = <CollectorSector>[];
   final random = Random();
-  final sectorCount = random.nextInt(3) + 2;
+  for (final collector in kFakeCollectors) {
+    final sectors = kFakeSectors
+        .where((sector) => sector.companyId == collector.companyId)
+        .toList();
+    final sector = sectors[random.nextInt(sectors.length)];
+    collectorSectors
+        .add(CollectorSector(collectorId: collector.id, sectorId: sector.id));
+  }
+  return collectorSectors;
+}
 
-  final randomSectors = List<CollectorSector>.generate(sectorCount, (index) {
-    final randomSector = kFakeSectors[random.nextInt(kFakeSectors.length)];
-    return CollectorSector(
-      collectorId: companyCollectors[index].id,
-      sectorId: randomSector.id,
-    );
-  });
-
-  return randomSectors;
-}).toList();
+final  kFakeCollectorSectors = generateFakeCollectorSectors();
