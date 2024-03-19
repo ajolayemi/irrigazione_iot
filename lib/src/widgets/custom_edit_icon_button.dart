66c0aa1af5a173e2606c2d78869445d81d352845
jolayemi <ajolayemi82@gmail.com>
2024-03-19 14:46:46 +0100

    created a common edit icon button

---
+++ lib/src/widgets/custom_edit_icon_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/config/enums/roles.dart';
+import 'package:irrigazione_iot/src/features/user_companies/data/user_companies_repository.dart';
+import 'package:irrigazione_iot/src/widgets/app_bar_icon_buttons.dart';
+
+class CustomEditIconButton extends ConsumerWidget {
+  const CustomEditIconButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final canEdit = ref.watch(companyUserRoleProvider).valueOrNull?.canEdit;
+    return AppBarIconButton(
+      onPressed: onPressed,
+      icon: Icons.edit,
+      isVisibile: canEdit,
+    );
+  }
+}
