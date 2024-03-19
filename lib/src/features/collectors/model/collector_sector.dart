86efc6bc8397f0a6edcde9760f800f4f23af467e
jolayemi <ajolayemi82@gmail.com>
2024-03-19 11:18:22 +0100

    added new class parameter

--- lib/src/features/collectors/model/collector_sector.dart
+++ lib/src/features/collectors/model/collector_sector.dart
@@ -4,41 +4,52 @@ import 'package:equatable/equatable.dart';
 
 import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector.dart';
+import 'package:irrigazione_iot/src/features/user_companies/model/company.dart';
 
 /// A representation of a [Sector] in a [Collector]
 class CollectorSector extends Equatable {
   const CollectorSector({
     required this.collectorId,
     required this.sectorId,
+    required this.companyId,
   });
 
   const CollectorSector.empty()
       : collectorId = '',
-        sectorId = '';
+        sectorId = '',
+        companyId = '';
   final CollectorID collectorId;
   final SectorID sectorId;
+  final CompanyID companyId;
 
   @override
-  List<Object> get props => [collectorId, sectorId];
+  List<Object> get props => [collectorId, sectorId, companyId];
 
   CollectorSector copyWith({
     CollectorID? collectorId,
     SectorID? sectorId,
+    CompanyID? companyId,
   }) {
     return CollectorSector(
       collectorId: collectorId ?? this.collectorId,
       sectorId: sectorId ?? this.sectorId,
+      companyId: companyId ?? this.companyId,
     );
   }
 
   Map<String, dynamic> toJson() {
-    return <String, dynamic>{'collectorId': collectorId, 'sectorId': sectorId};
+    return <String, dynamic>{
+      'collectorId': collectorId,
+      'sectorId': sectorId,
+      'companyId': companyId
+    };
   }
 
   factory CollectorSector.fromJson(Map<String, dynamic> map) {
     return CollectorSector(
       collectorId: map['collectorId'] as CollectorID,
       sectorId: map['sectorId'] as SectorID,
+      companyId: map['companyId'] as CompanyID,
     );
   }
 }
