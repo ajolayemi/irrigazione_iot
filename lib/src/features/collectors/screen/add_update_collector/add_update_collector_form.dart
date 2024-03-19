d352abdc7c8bf22a5503c7c8419277dd59a8b201
jolayemi <ajolayemi82@gmail.com>
2024-03-19 11:23:41 +0100

    built a functioning form to add or update collectors

--- lib/src/features/collectors/screen/add_update_collector/add_update_collector_form.dart
+++ lib/src/features/collectors/screen/add_update_collector/add_update_collector_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/config/enums/form_types.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
+import 'package:irrigazione_iot/src/features/collectors/screen/add_update_collector/add_update_collector_form_contents.dart';
 
 class AddUpdateCollectorForm extends ConsumerWidget {
   const AddUpdateCollectorForm({
@@ -14,8 +15,23 @@ class AddUpdateCollectorForm extends ConsumerWidget {
   final CollectorID? collectorId;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(),
+    const isLoading = false; // todo replace this with proper state
+    return PopScope(
+      canPop: !isLoading,
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          debugPrint('User exited COLLECTOR form');
+        } else {
+          debugPrint('User tried to exit COLLECTOR form');
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+            child: AddUpdateCollectorFormContents(
+          collectorId: collectorId,
+          formType: formType,
+        )),
+      ),
     );
   }
 }
