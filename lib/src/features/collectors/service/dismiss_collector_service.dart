d4f38fbc9f11c742285f051ef0dfe832fae6427d
jolayemi <ajolayemi82@gmail.com>
2024-03-20 15:37:28 +0100

    wrote service logic to dismiss collectors

--- lib/src/features/collectors/service/dismiss_collector_service.dart
+++ lib/src/features/collectors/service/dismiss_collector_service.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
-import 'package:irrigazione_iot/src/utils/delay.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:irrigazione_iot/src/features/collectors/data/collector_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/data/collector_sector_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
 
 part 'dismiss_collector_service.g.dart';
 
@@ -14,8 +17,32 @@ class DismissCollectorService {
   /// Handles the deletion of all data related to a collector
   /// i.e the standard collector data and its connected sectors
   Future<void> dismissCollector(CollectorID collectorId) async {
-    await delay(true, 9000);
-    print('coming soonnnn');
+    final collectorRepo = _ref.read(collectorRepositoryProvider);
+    final collectorSectorRepo = _ref.read(collectorSectorRepositoryProvider);
+    final collectorWasDeleted =
+        await collectorRepo.deleteCollector(collectorId);
+    // if the collector deletion was completed successfully
+    if (collectorWasDeleted) {
+      debugPrint('Collector deleted successfully');
+      // get a list of all the sectors connected to the collector
+      final collectorSectors =
+          await collectorSectorRepo.getCollectorSectorsById(
+        collectorId: collectorId,
+      );
+
+      if (collectorSectors.isEmpty) {
+        debugPrint('No sectors connected to the collector');
+        return;
+      }
+
+      // delete all the sectors connected to the collector
+      for (final collectorSector in collectorSectors) {
+        debugPrint('Deleting collector sector: ${collectorSector?.sectorId}');
+        await collectorSectorRepo.deleteCollectorSector(
+          collectorSector: collectorSector!,
+        );
+      }
+    }
   }
 }
 
