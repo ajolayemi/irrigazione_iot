8c1ce3fcd0c67a6d48ef0b799c3d60de4aaf37aa
jolayemi <ajolayemi82@gmail.com>
2024-03-20 15:26:24 +0100

    wrapped collector expansion tile with Dismissible

--- lib/src/features/collectors/screen/collector_expansion_list_tile.dart
+++ lib/src/features/collectors/screen/collector_expansion_list_tile.dart
@@ -6,9 +6,13 @@ import 'package:irrigazione_iot/src/constants/app_sizes.dart';
 import 'package:irrigazione_iot/src/constants/breakpoints.dart';
 import 'package:irrigazione_iot/src/features/collectors/data/collector_sector_repository.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
+import 'package:irrigazione_iot/src/features/collectors/screen/collector_list/dismiss_collector_controller.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector.dart';
 import 'package:irrigazione_iot/src/features/sectors/screen/sector_list/sector_list_tile.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
+import 'package:irrigazione_iot/src/widgets/custom_dismissible.dart';
 import 'package:irrigazione_iot/src/widgets/responsive_center.dart';
 
 class CollectorExpansionListTile extends ConsumerStatefulWidget {
@@ -19,10 +23,6 @@ class CollectorExpansionListTile extends ConsumerStatefulWidget {
 
   final Collector collector;
 
-  // Key for testing using find.byKey
-  static Key collectorExpansionListTileKey(Collector collector) =>
-      Key('collectorExpansionListTileKey_${collector.id}');
-
   @override
   ConsumerState<CollectorExpansionListTile> createState() =>
       _CollectorExpansionListTileState();
@@ -31,41 +31,77 @@ class CollectorExpansionListTile extends ConsumerStatefulWidget {
 class _CollectorExpansionListTileState
     extends ConsumerState<CollectorExpansionListTile> {
   bool _isExpanded = false;
-  
+
+  // Key for testing using find.byKey
+  static Key collectorExpansionListTileKey(Collector collector) =>
+      Key('collectorExpansionListTileKey_${collector.id}');
+
+  Future<bool> _dismissCollector(BuildContext context, WidgetRef ref) async {
+    final loc = context.loc;
+    final shouldDismiss = await showAlertDialog(
+          context: context,
+          title: loc.genericAlertDialogTitle,
+          content: loc.deleteConfirmationDialogTitle(
+            loc.nCollectorsWithArticulatedPreposition(1),
+          ),
+          defaultActionText: loc.alertDialogDelete,
+          cancelActionText: loc.alertDialogCancel,
+        ) ??
+        false;
+    if (!shouldDismiss) return false;
+
+    return ref
+        .read(dismissCollectorControllerProvider.notifier)
+        .confirmDismiss(widget.collector.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     final collectorSectors = ref
         .watch(collectorSectorsStreamProvider(widget.collector.id))
         .valueOrNull;
-    return ResponsiveCenter(
-      padding: _isExpanded
-          ? const EdgeInsets.only(left: Sizes.p8, right: Sizes.p8)
-          : const EdgeInsets.all(0),
-      maxContentWidth: Breakpoint.tablet,
-      child: ExpansionTile(
-        expandedCrossAxisAlignment: CrossAxisAlignment.end,
-        onExpansionChanged: (value) => setState(() => _isExpanded = value),
-        leading: _isExpanded
-            ? null
-            : IconButton(
-                icon: const Icon(Icons.info_outline),
-                onPressed: () => context.pushNamed(
-                  AppRoute.collectorDetails.name,
-                  pathParameters: {
-                    'collectorId': widget.collector.id,
-                  },
-                ),
-              ),
-        title: Text(widget.collector.name),
-        children: collectorSectors == null || collectorSectors.isEmpty
-            ? []
-            : collectorSectors
-                .map(
-                  (collectorSector) => CollectorExpansionTileChildItem(
-                    sectorID: collectorSector!.sectorId,
+
+    final isDeleting = ref.watch(dismissCollectorControllerProvider).isLoading;
+    return IgnorePointer(
+      ignoring: isDeleting,
+      child: CustomDismissibleWidget(
+        dismissibleKey: collectorExpansionListTileKey(widget.collector),
+        isDeleting: isDeleting,
+        confirmDismiss: (_) async => await _dismissCollector(
+          context,
+          ref,
+        ),
+        child: ResponsiveCenter(
+          padding: _isExpanded
+              ? const EdgeInsets.only(left: Sizes.p8, right: Sizes.p8)
+              : const EdgeInsets.all(0),
+          maxContentWidth: Breakpoint.tablet,
+          child: ExpansionTile(
+            expandedCrossAxisAlignment: CrossAxisAlignment.end,
+            onExpansionChanged: (value) => setState(() => _isExpanded = value),
+            leading: _isExpanded
+                ? null
+                : IconButton(
+                    icon: const Icon(Icons.info_outline),
+                    onPressed: () => context.pushNamed(
+                      AppRoute.collectorDetails.name,
+                      pathParameters: {
+                        'collectorId': widget.collector.id,
+                      },
+                    ),
                   ),
-                )
-                .toList(),
+            title: Text(widget.collector.name),
+            children: collectorSectors == null || collectorSectors.isEmpty
+                ? []
+                : collectorSectors
+                    .map(
+                      (collectorSector) => CollectorExpansionTileChildItem(
+                        sectorID: collectorSector!.sectorId,
+                      ),
+                    )
+                    .toList(),
+          ),
+        ),
       ),
     );
   }
