20f72ef799bd01aad2053d59995526261b247d73
jolayemi <ajolayemi82@gmail.com>
2024-03-20 14:09:22 +0100

    completed logic to create new collector

--- lib/src/features/collectors/service/add_update_collector_service.dart
+++ lib/src/features/collectors/service/add_update_collector_service.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/data/collector_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/data/collector_sector_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
+import 'package:irrigazione_iot/src/features/collectors/model/collector_sector.dart';
+import 'package:irrigazione_iot/src/features/user_companies/data/selected_company_repository.dart';
 
 part 'add_update_collector_service.g.dart';
 
@@ -11,9 +18,52 @@ class AddUpdateCollectorService {
   final Ref _ref;
 
   Future<void> createCollector(Collector collector) async {
-    print(
-      'Creating collector coming soon...',
-    ); // todo implement createCollector
+    final user = _ref.read(authRepositoryProvider).currentUser;
+    if (user == null) {
+      debugPrint('Exiting createCollector, user is null');
+      return;
+    }
+
+    final selectedCompanyRepo = _ref.read(selectedCompanyRepositoryProvider);
+    final companyId = selectedCompanyRepo.loadSelectedCompanyId(user.uid);
+
+    if (companyId == null) {
+      debugPrint('Exiting createCollector, companyId is null');
+      return;
+    }
+
+    final collectorRepo = _ref.read(collectorRepositoryProvider);
+    final collectorSectorsRepo = _ref.read(collectorSectorRepositoryProvider);
+    final sectorsToConnectToCollector = _ref.read(selectedSectorsIdProvider);
+
+    // create collector
+    final createdCollector = await collectorRepo.addCollector(
+      collector,
+      companyId,
+    );
+
+    if (createdCollector == null) {
+      debugPrint('Exiting createCollector, createdCollector is null');
+      return;
+    }
+
+    if (sectorsToConnectToCollector.isNotEmpty) {
+      // loop over each sector and connect it to the created collector
+      for (final sectorId in sectorsToConnectToCollector) {
+        final collectorSector = CollectorSector(
+            collectorId: createdCollector.id,
+            sectorId: sectorId!,
+            companyId: companyId);
+        debugPrint(
+            'Creating collector sector: ${collectorSector.toJson()} for collector: ${createdCollector.name}');
+        final createdCollectorSector =
+            await collectorSectorsRepo.addCollectorSector(
+          collectorSector: collectorSector,
+        );
+        debugPrint(
+            'Created collectorSector: ${createdCollectorSector?.toJson()}');
+      }
+    }
   }
 
   Future<void> updateCollector(Collector collector) async {
