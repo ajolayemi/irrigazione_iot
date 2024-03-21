d53d7f03b30eb25dc16a21287a5a7899e69804bb
jolayemi <ajolayemi82@gmail.com>
2024-03-21 15:03:56 +0100

    activated button to log user out

--- lib/src/features/more/screen/more_options_screen.dart
+++ lib/src/features/more/screen/more_options_screen.dart
@@ -1,55 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/config/enums/button_types.dart';
+import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
 import 'package:irrigazione_iot/src/features/more/widgets/more_page_item_list_tile.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
+import 'package:irrigazione_iot/src/widgets/app_cta_button.dart';
 import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
 import 'package:irrigazione_iot/src/widgets/common_responsive_divider.dart';
 
-class MoreOptionsScreen extends StatelessWidget {
+class MoreOptionsScreen extends ConsumerWidget {
   const MoreOptionsScreen({super.key});
 
   void _showNotImplemented(BuildContext context) {
     showNotImplementedAlertDialog(context: context);
   }
 
+  Future<bool> _showSignOutDialog(BuildContext context) async {
+    final loc = context.loc;
+    return await showAlertDialog(
+        context: context,
+        title: loc.logOutAlertDialogTitle,
+        content: loc.logOutAlertDialogContent,
+        cancelActionText: loc.alertDialogCancel,
+        defaultActionText: loc.logOutAlertDialogConfirm) ?? false;
+  }
+
+  void _signOut(BuildContext context, WidgetRef ref) async {
+    if (await _showSignOutDialog(context)) {
+      ref.read(authRepositoryProvider).signOut();
+    }
+    
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final loc = context.loc;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: CustomScrollView(
-                slivers: [
-                  AppSliverBar(title: loc.morePageTitle),
-                  SliverList(
-                    delegate: SliverChildListDelegate.fixed(
-                      [
-                        MorePageItemListTile(
-                          title: loc.iotBoardsMenuTitle,
-                          onTap: () => _showNotImplemented(context),
-                          leadingIcon: Icons.device_hub,
-                        ),
-                        const CommonResponsiveDivider(),
-                        MorePageItemListTile(
-                            title: loc.profilePageTitle,
-                            onTap: () => _showNotImplemented(context),
-                            leadingIcon: Icons.person),
-                        MorePageItemListTile(
-                          title: loc.settingsMenuTitle,
-                          onTap: () => _showNotImplemented(context),
-                          leadingIcon: Icons.settings,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+          child: CustomScrollView(
+        slivers: [
+          AppSliverBar(title: loc.morePageTitle),
+          SliverList(
+            delegate: SliverChildListDelegate.fixed(
+              [
+                MorePageItemListTile(
+                  title: loc.iotBoardsMenuTitle,
+                  onTap: () => _showNotImplemented(context),
+                  leadingIcon: Icons.device_hub,
+                ),
+                const CommonResponsiveDivider(),
+                MorePageItemListTile(
+                    title: loc.profilePageTitle,
+                    onTap: () => _showNotImplemented(context),
+                    leadingIcon: Icons.person),
+                MorePageItemListTile(
+                  title: loc.settingsMenuTitle,
+                  onTap: () => _showNotImplemented(context),
+                  leadingIcon: Icons.settings,
+                )
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SliverCTAButton(
+                  text: loc.logOutButtonTitle,
+                  buttonType: ButtonType.secondary,
+                  onPressed: () => _signOut(context, ref)),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
