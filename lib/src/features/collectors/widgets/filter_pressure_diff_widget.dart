77e6461516056b24e70dfea2d40388dadd71e8ab
jolayemi <ajolayemi82@gmail.com>
2024-03-21 12:26:05 +0100

    added helper widgets for collectors

---
+++ lib/src/features/collectors/widgets/filter_pressure_diff_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/collectors/data/collector_pressure_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+
+class CollectorFilterPressureDifference extends ConsumerWidget {
+  const CollectorFilterPressureDifference({
+    super.key,
+    required this.collectorId,
+  });
+
+  final CollectorID collectorId;
+
+  // Determine battery color based on level
+  Color _getPressureColor(double pressureDifference) {
+    if (pressureDifference >= 0.7) {
+      return Colors.red;
+    } else if (pressureDifference >= 0.5) {
+      return Colors.orange;
+    } else {
+      return Colors.green;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pressures = ref
+        .watch(collectorPressureStreamProvider(collectorId: collectorId))
+        .valueOrNull;
+    double pressureDifference =
+        pressures == null ? 0.0 : pressures.pressureDifference;
+    return Row(
+      children: [
+        Text(
+          '${context.loc.filterPressureDifference} ',
+          textScaler: const TextScaler.linear(1),
+        ),
+        Text(
+          pressureDifference.toStringAsFixed(2),
+          textScaler: const TextScaler.linear(1),
+          style: TextStyle(
+            color: _getPressureColor(pressureDifference),
+          ),
+        ),
+      ],
+    );
+  }
+}
