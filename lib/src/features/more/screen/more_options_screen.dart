dd0d5120584080bf7e00d5d2b61a50553d8ec7f8
jolayemi <ajolayemi82@gmail.com>
2024-03-21 12:54:17 +0100

    defined a skeleton more options screen

---
+++ lib/src/features/more/screen/more_options_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoreOptionsScreen extends StatelessWidget {
+  const MoreOptionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
