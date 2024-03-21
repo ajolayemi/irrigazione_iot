fde8139c9b3904b8fd35e00d6c9c5f7f0382d364
jolayemi <ajolayemi82@gmail.com>
2024-03-21 18:14:41 +0100

    built board list screen scaffold

--- lib/src/features/board-centraline/screen/boards_list/boards_list_screen.dart
+++ lib/src/features/board-centraline/screen/boards_list/boards_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
+import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
+import 'package:irrigazione_iot/src/widgets/common_add_icon_button.dart';
 
 /// Displays a list of boards, i.e centraline in italian.
 class BoardsListScreen extends ConsumerWidget {
@@ -8,6 +11,23 @@ class BoardsListScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final loc = context.loc;
+    return Scaffold(
+      body: SafeArea(
+          child: CustomScrollView(
+        slivers: [
+          AppSliverBar(
+            title: loc.iotBoardsMenuTitle,
+            actions: [
+              CommonAddIconButton(
+                onPressed: () => showNotImplementedAlertDialog(
+                  context: context,
+                ),
+              ),
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
