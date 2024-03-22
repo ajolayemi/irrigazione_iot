f3a3d62a7b153d95c89d72a2393919a62a9bab53
jolayemi <ajolayemi82@gmail.com>
2024-03-22 09:49:28 +0100

    added async sliver widget to display board details

--- lib/src/features/board-centraline/screen/board_details/board_details_screen.dart
+++ lib/src/features/board-centraline/screen/board_details/board_details_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/features/board-centraline/data/board_repository.dart';
 import 'package:irrigazione_iot/src/features/board-centraline/models/board.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/screen/board_details/board_details_screen_contents.dart';
 import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
 import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
+import 'package:irrigazione_iot/src/widgets/async_value_widget.dart';
+import 'package:irrigazione_iot/src/widgets/common_sliver_list_skeleton.dart';
 import 'package:irrigazione_iot/src/widgets/custom_edit_icon_button.dart';
 
 class BoardDetailsScreen extends ConsumerWidget {
@@ -30,6 +33,18 @@ class BoardDetailsScreen extends ConsumerWidget {
               )
             ],
           ),
+          AsyncValueSliverWidget(
+            value: board,
+            data: (board) {
+              // it's safe to assume that board won't be null
+              // since reaching this page means we have a board
+              return BoardDetailsScreenContents(board: board!);
+            },
+            loading: () => const CommonSliverListSkeleton(
+              hasLeading: false,
+              hasTrailing: false,
+            ),
+          )
         ],
       )),
     );
