2b3012ca73965f79abe519886ba63b37fc44beaf
jolayemi <ajolayemi82@gmail.com>
2024-03-22 08:27:51 +0100

    referenced the provider to get board linked to collector

--- lib/src/features/collectors/widgets/collector_tile_subtitle.dart
+++ lib/src/features/collectors/widgets/collector_tile_subtitle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/constants/app_sizes.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/data/board_repository.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
 import 'package:irrigazione_iot/src/features/collectors/widgets/battery_level_indicator.dart';
 import 'package:irrigazione_iot/src/features/collectors/widgets/filter_pressure_diff_widget.dart';
@@ -20,6 +21,15 @@ class CollectorTileSubtitle extends ConsumerWidget {
       children: [
         Consumer(
           builder: (context, ref, child) {
+            /// Get the board (centralina) that is linked to this collector
+            final board = ref
+                .watch(collectorBoardStreamProvider(collectorID: collectorId))
+                .valueOrNull;
+
+            /// If the board is null, return an empty widget
+            if (board == null) {
+              return const SliverToBoxAdapter();
+            }
             return BatteryIndicator(
               collectorId: collectorId,
             );
