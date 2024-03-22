ed44d85fd3d0edea8497ac11f87c46b3907e6a6c
jolayemi <ajolayemi82@gmail.com>
2024-03-22 09:49:43 +0100

    skeleton widget that will hold all board details

---
+++ lib/src/features/board-centraline/screen/board_details/board_details_screen_contents.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/models/board.dart';
+
+class BoardDetailsScreenContents extends ConsumerWidget {
+  const BoardDetailsScreenContents({
+    super.key,
+    required this.board,
+  });
+
+  final Board board;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SliverToBoxAdapter();
+  }
+}
