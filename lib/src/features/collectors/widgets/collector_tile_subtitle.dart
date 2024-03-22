8c7e22478aa7b66c46855a2e382902c2bb378f2f
jolayemi <ajolayemi82@gmail.com>
2024-03-22 08:25:13 +0100

    wrapped collector battery indicator with consumer, changed wid to ConsumerWidget

--- lib/src/features/collectors/widgets/collector_tile_subtitle.dart
+++ lib/src/features/collectors/widgets/collector_tile_subtitle.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/constants/app_sizes.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
 import 'package:irrigazione_iot/src/features/collectors/widgets/battery_level_indicator.dart';
 import 'package:irrigazione_iot/src/features/collectors/widgets/filter_pressure_diff_widget.dart';
 
 /// A row widget that displays the battery level and the filter pressure difference
-class CollectorTileSubtitle extends StatelessWidget {
+class CollectorTileSubtitle extends ConsumerWidget {
   const CollectorTileSubtitle({
     super.key,
     required this.collectorId,
@@ -14,11 +15,15 @@ class CollectorTileSubtitle extends StatelessWidget {
   final CollectorID collectorId;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       children: [
-        BatteryIndicator(
-          collectorId: collectorId,
+        Consumer(
+          builder: (context, ref, child) {
+            return BatteryIndicator(
+              collectorId: collectorId,
+            );
+          },
         ),
         gapW8,
         CollectorFilterPressureDifference(
