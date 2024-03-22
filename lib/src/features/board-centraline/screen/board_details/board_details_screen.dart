5e234eeee3bfbac88001b3d3317574979c50904f
jolayemi <ajolayemi82@gmail.com>
2024-03-22 09:33:16 +0100

    built first skeleton of page to display board details

--- lib/src/features/board-centraline/screen/board_details/board_details_screen.dart
+++ lib/src/features/board-centraline/screen/board_details/board_details_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/data/board_repository.dart';
 import 'package:irrigazione_iot/src/features/board-centraline/models/board.dart';
+import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
+import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
+import 'package:irrigazione_iot/src/widgets/custom_edit_icon_button.dart';
 
 class BoardDetailsScreen extends ConsumerWidget {
   const BoardDetailsScreen({
@@ -12,6 +16,22 @@ class BoardDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final board = ref.watch(boardStreamProvider(boardID: boardID)).valueOrNull;
+    return Scaffold(
+      body: SafeArea(
+          child: CustomScrollView(
+        slivers: [
+          AppSliverBar(
+            title: board?.name ?? '',
+            actions: [
+              CustomEditIconButton(
+                onPressed: () =>
+                    showNotImplementedAlertDialog(context: context),
+              )
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
