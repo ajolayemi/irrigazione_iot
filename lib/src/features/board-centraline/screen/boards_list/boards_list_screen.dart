ddffc4fe2b9a10599d862fd4718f8f4c695eef10
jolayemi <ajolayemi82@gmail.com>
2024-03-22 08:57:03 +0100

    added async sliver widget to boards screen

--- lib/src/features/board-centraline/screen/boards_list/boards_list_screen.dart
+++ lib/src/features/board-centraline/screen/boards_list/boards_list_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/data/board_repository.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/widgets/empty_board_widget.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
 import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
+import 'package:irrigazione_iot/src/widgets/async_value_widget.dart';
 import 'package:irrigazione_iot/src/widgets/common_add_icon_button.dart';
+import 'package:irrigazione_iot/src/widgets/common_sliver_list_skeleton.dart';
 
 /// Displays a list of boards, i.e centraline in italian.
 class BoardsListScreen extends ConsumerWidget {
@@ -12,6 +16,7 @@ class BoardsListScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final loc = context.loc;
+    final boards = ref.watch(boardListStreamProvider);
     return Scaffold(
       body: SafeArea(
           child: CustomScrollView(
@@ -25,7 +30,33 @@ class BoardsListScreen extends ConsumerWidget {
                 ),
               ),
             ],
-          )
+          ),
+          AsyncValueSliverWidget(
+              value: boards,
+              data: (boards) {
+                if (boards.isEmpty) {
+                  return const EmptyBoardWidget();
+                }
+
+                return SliverList(delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    // It's safe to assume that board is not null
+                    final board = boards[index]!;
+                    return ListTile(
+                      title: Text(board.name),
+                      onTap: () => showNotImplementedAlertDialog(
+                        context: context,
+                      ),
+                    );
+                  },
+                  childCount: boards.length,
+                ));
+              },
+              loading: () => const CommonSliverListSkeleton(
+                hasLeading: false,
+                hasTrailing: false,
+              )
+           ),
         ],
       )),
     );
