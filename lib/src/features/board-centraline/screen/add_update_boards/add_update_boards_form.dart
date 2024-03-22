a8bd203e3c9d967027d0f36977880ef3c6c0b473
jolayemi <ajolayemi82@gmail.com>
2024-03-22 10:06:53 +0100

    replaced placeholder widget with the correct one

--- lib/src/features/board-centraline/screen/add_update_boards/add_update_boards_form.dart
+++ lib/src/features/board-centraline/screen/add_update_boards/add_update_boards_form.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/config/enums/form_types.dart';
 import 'package:irrigazione_iot/src/features/board-centraline/models/board.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/screen/add_update_boards/add_update_board_form_content.dart';
 
-class AddUpdateBoardsForm extends ConsumerStatefulWidget {
+class AddUpdateBoardsForm extends ConsumerWidget {
   const AddUpdateBoardsForm({
     super.key,
     required this.formType,
@@ -12,15 +13,30 @@ class AddUpdateBoardsForm extends ConsumerStatefulWidget {
 
   final BoardID? boardID;
   final GenericFormTypes formType;
-
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _AddUpdateBoardsFormState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO add listener on state
 
-class _AddUpdateBoardsFormState extends ConsumerState<AddUpdateBoardsForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    // TODO replace with normal state
+    const isLoading = false;
+
+    return PopScope(
+      canPop: !isLoading,
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          debugPrint('User exited Board form');
+        } else {
+          debugPrint('User tried to exit Board form');
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: AddUpdateBoardFormContent(
+            formType: formType,
+            boardID: boardID,
+          ),
+        ),
+      ),
+    );
   }
 }
