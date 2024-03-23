52713dcaf4d965c6ad54d4186c5775ef5280ef6d
jolayemi <ajolayemi82@gmail.com>
2024-03-23 14:06:11 +0100

    added extension on build context to show dismiss dialog

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
 
 extension BuildContextExtensions on BuildContext {
   ThemeData get theme => Theme.of(this);
@@ -12,7 +13,23 @@ extension BuildContextExtensions on BuildContext {
 
   AppLocalizations get loc => AppLocalizations.of(this);
 
-  void popNavigator<T extends Object?>([T? result]) => Navigator.of(this).pop(result);
+  void popNavigator<T extends Object?>([T? result]) =>
+      Navigator.of(this).pop(result);
+
+  /// An alert dialog to display when user is trying to delete an item
+  /// such as board, collector, pump and so on.
+  // TODO: this should replace all places where such dialog is being shown at the moment
+  Future<bool> showDismissalDialog(String where) async {
+    final loc = this.loc;
+    return await showAlertDialog(
+          context: this,
+          title: loc.genericAlertDialogTitle,
+          defaultActionText: loc.alertDialogDelete,
+          cancelActionText: loc.alertDialogCancel,
+          content: loc.deleteConfirmationDialogTitle(where),
+        ) ??
+        false;
+  }
 }
 
 extension StringExtensions on String {
