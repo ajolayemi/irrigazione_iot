1ccad5a0c0a2a2f911ace458ddd35488e48f4d0a
jolayemi <ajolayemi82@gmail.com>
2024-03-23 13:17:24 +0100

    defined a skeleton AddUpdateBoardController

---
+++ lib/src/features/board-centraline/screen/add_update_boards/add_update_board_controller.dart
@@ -0,0 +1,12 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+
+part 'add_update_board_controller.g.dart';
+
+@riverpod
+class AddUpdateBoardController extends _$AddUpdateBoardController {
+  @override
+  FutureOr<void> build() {
+
+  }
+}
---
+++ lib/src/features/board-centraline/screen/add_update_boards/add_update_board_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'add_update_board_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$addUpdateBoardControllerHash() =>
+    r'647e29f307ae934afd66b5c6362044ccff3d1113';
+
+/// See also [AddUpdateBoardController].
+@ProviderFor(AddUpdateBoardController)
+final addUpdateBoardControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AddUpdateBoardController, void>.internal(
+  AddUpdateBoardController.new,
+  name: r'addUpdateBoardControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addUpdateBoardControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AddUpdateBoardController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
