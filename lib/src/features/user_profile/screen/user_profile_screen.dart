5e56a193a809809f094ee64ad3d31b30425511cc
jolayemi <ajolayemi82@gmail.com>
2024-03-23 14:52:02 +0100

    defined a skeleton user_profile screen

---
+++ lib/src/features/user_profile/screen/user_profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserProfileScreen extends ConsumerWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
