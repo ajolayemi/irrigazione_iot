b4fb785b84756a0445225c2049526008215efed4
jolayemi <ajolayemi82@gmail.com>
2024-03-23 15:00:29 +0100

    defined scaffold to hold user profile contents

--- lib/src/features/user_profile/screen/user_profile_screen.dart
+++ lib/src/features/user_profile/screen/user_profile_screen.dart
@@ -1,11 +1,24 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
 
 class UserProfileScreen extends ConsumerWidget {
   const UserProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final loc = context.loc;
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            AppSliverBar(
+              title: loc.profilePageTitle,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
