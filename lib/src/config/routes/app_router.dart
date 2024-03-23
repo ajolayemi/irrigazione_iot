3dc8881bc9832dc01dec23e966f7b5267ff44516
jolayemi <ajolayemi82@gmail.com>
2024-03-23 14:52:52 +0100

    removed unused import statement

--- lib/src/config/routes/app_router.dart
+++ lib/src/config/routes/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:irrigazione_iot/src/config/enums/form_types.dart';
 import 'package:irrigazione_iot/src/config/routes/go_router_refresh_stream.dart';
