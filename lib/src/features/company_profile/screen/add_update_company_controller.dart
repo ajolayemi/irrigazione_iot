795920c49578240d0682b4bdb1a25638e950f16c
jolayemi <ajolayemi82@gmail.com>
2024-03-24 13:30:43 +0100

    defined an empty controller for adding or updating companies

---
+++ lib/src/features/company_profile/screen/add_update_company_controller.dart
@@ -0,0 +1,9 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'add_update_company_controller.g.dart';
+
+@riverpod
+class AddUpdateCompanyController extends _$AddUpdateCompanyController {
+  @override
+  FutureOr<void> build() {}
+}
---
+++ lib/src/features/company_profile/screen/add_update_company_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'add_update_company_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$addUpdateCompanyControllerHash() =>
+    r'25e9122ca8c7b3248bb8e44c42a32dc80e30169d';
+
+/// See also [AddUpdateCompanyController].
+@ProviderFor(AddUpdateCompanyController)
+final addUpdateCompanyControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AddUpdateCompanyController, void>.internal(
+  AddUpdateCompanyController.new,
+  name: r'addUpdateCompanyControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addUpdateCompanyControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AddUpdateCompanyController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
