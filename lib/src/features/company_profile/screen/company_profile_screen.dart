4d4885d830f68e8cc6e8eb5906e7375839086aaf
jolayemi <ajolayemi82@gmail.com>
2024-03-24 12:42:59 +0100

    refactored CompanyProfileScreen

--- lib/src/features/company_profile/screen/company_profile_screen.dart
+++ lib/src/features/company_profile/screen/company_profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/company_profile/screen/company_profile_screen_contents.dart';
 import 'package:irrigazione_iot/src/features/user_companies/model/company.dart';
 
-class CompanyProfileScreen extends ConsumerWidget {
+class CompanyProfileScreen extends StatelessWidget {
   const CompanyProfileScreen({
     super.key,
     required this.companyID,
@@ -11,7 +11,13 @@ class CompanyProfileScreen extends ConsumerWidget {
   final CompanyID companyID;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CompanyProfileScreenContents(
+          companyID: companyID,
+        ),
+      ),
+    );
   }
 }
