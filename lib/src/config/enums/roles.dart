938f79e08c8d2678e308b488b6975bf16e9b4e3e
jolayemi <ajolayemi82@gmail.com>
2024-03-24 11:54:34 +0100

    added a new extension method on CompanyUserRoles

--- lib/src/config/enums/roles.dart
+++ lib/src/config/enums/roles.dart
@@ -9,4 +9,12 @@ extension CompanyUserRolesExtension on CompanyUserRoles {
   bool get canEdit {
     return this == CompanyUserRoles.admin || this == CompanyUserRoles.owner;
   }
+
+  // Check if the user can add new user for a specified company
+  // This can be done by admin, superuser, and owner
+  bool get canAddNewUser {
+    return this == CompanyUserRoles.admin ||
+        this == CompanyUserRoles.owner ||
+        this == CompanyUserRoles.superuser;
+  }
 }
