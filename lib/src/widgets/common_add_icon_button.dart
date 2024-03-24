f464bb3f598229da0d1024bf2e42d35e37f73cc3
jolayemi <ajolayemi82@gmail.com>
2024-03-24 12:08:34 +0100

    added a new parameter

--- lib/src/widgets/common_add_icon_button.dart
+++ lib/src/widgets/common_add_icon_button.dart
@@ -8,16 +8,21 @@ class CommonAddIconButton extends ConsumerWidget {
   const CommonAddIconButton({
     super.key,
     required this.onPressed,
+    this.alternateIsVisible,
   });
 
   final VoidCallback onPressed;
+
+  /// If the button should be visible or not
+  /// if not provided, the generic canEdit check on user role is used
+  final bool? alternateIsVisible;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final canEdit = ref.watch(companyUserRoleProvider).valueOrNull?.canEdit;
     return AppBarIconButton(
       onPressed: onPressed,
       icon: Icons.add,
-      isVisibile: canEdit,
+      isVisibile: alternateIsVisible ?? canEdit,
     );
   }
 }
