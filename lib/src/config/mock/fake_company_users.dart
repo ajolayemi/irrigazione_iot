5a8ad0f9e5ec62016ff21a1d1a5ff2cf96fc7dcc
jolayemi <ajolayemi82@gmail.com>
2024-03-25 13:44:31 +0100

    added full name arg

--- lib/src/config/mock/fake_company_users.dart
+++ lib/src/config/mock/fake_company_users.dart
@@ -1,10 +1,12 @@
 import 'package:irrigazione_iot/src/config/enums/roles.dart';
 import 'package:irrigazione_iot/src/config/mock/fake_users_list.dart';
+import 'package:irrigazione_iot/src/features/authentication/model/app_user.dart';
 import 'package:irrigazione_iot/src/features/company_users/model/company_user.dart';
 
 final kFakeCompanyUsers = <CompanyUser>[
   CompanyUser(
     email: kFakeUsers[0].email,
+    fullName: kFakeUsers[0].fullName,
     companyId: '1',
     role: CompanyUserRoles.admin,
     createdAt: DateTime.now(),
@@ -12,6 +14,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[0].email,
+    fullName: kFakeUsers[0].fullName,
     companyId: '2',
     role: CompanyUserRoles.user,
     createdAt: DateTime.now(),
@@ -19,6 +22,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[2].email,
+    fullName: kFakeUsers[2].fullName,
     companyId: '3',
     role: CompanyUserRoles.superuser,
     createdAt: DateTime.now(),
@@ -26,6 +30,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[2].email,
+    fullName: kFakeUsers[2].fullName,
     companyId: '4',
     role: CompanyUserRoles.admin,
     createdAt: DateTime.now(),
@@ -33,6 +38,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[3].email,
+    fullName: kFakeUsers[3].fullName,
     companyId: '5',
     role: CompanyUserRoles.user,
     createdAt: DateTime.now(),
@@ -40,6 +46,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[3].email,
+    fullName: kFakeUsers[3].fullName,
     companyId: '6',
     role: CompanyUserRoles.owner,
     createdAt: DateTime.now(),
@@ -47,6 +54,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[5].email,
+    fullName: kFakeUsers[5].fullName,
     companyId: '4',
     role: CompanyUserRoles.admin,
     createdAt: DateTime.now(),
@@ -54,6 +62,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[5].email,
+    fullName: kFakeUsers[5].fullName,
     companyId: '3',
     role: CompanyUserRoles.user,
     createdAt: DateTime.now(),
@@ -61,6 +70,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[7].email,
+    fullName: kFakeUsers[7].fullName,
     companyId: '2',
     role: CompanyUserRoles.user,
     createdAt: DateTime.now(),
@@ -68,6 +78,7 @@ final kFakeCompanyUsers = <CompanyUser>[
   ),
   CompanyUser(
     email: kFakeUsers[7].email,
+    fullName: kFakeUsers[7].fullName,
     companyId: '1',
     role: CompanyUserRoles.admin,
     createdAt: DateTime.now(),
