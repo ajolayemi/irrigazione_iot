24c7b9b634b742c063f771853cfd6f3ff79ebfb9
jolayemi <ajolayemi82@gmail.com>
2024-03-25 12:13:33 +0100

    changed widget to StatelessWidget

--- lib/src/features/company_users/screen/company_users_list/company_users_list_screen.dart
+++ lib/src/features/company_users/screen/company_users_list/company_users_list_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
 
-class CompanyUsersListScreen extends ConsumerWidget {
+class CompanyUsersListScreen extends StatelessWidget {
   const CompanyUsersListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final loc = context.loc;
     return Scaffold(
       body: SafeArea(
