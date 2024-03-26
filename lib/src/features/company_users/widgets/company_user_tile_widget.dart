674eebc067a72eb3df19cff682f23866c55ed392
jolayemi <ajolayemi82@gmail.com>
2024-03-26 16:02:29 +0100

    wrapped company user tile with consumer

--- lib/src/features/company_users/widgets/company_user_tile_widget.dart
+++ lib/src/features/company_users/widgets/company_user_tile_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:irrigazione_iot/src/config/routes/app_router.dart';
 import 'package:irrigazione_iot/src/constants/app_sizes.dart';
@@ -26,23 +27,26 @@ class CompanyUserTileWidget extends StatelessWidget {
           left: Sizes.p8,
         ),
         child: InkWell(
-          onTap: () => context.pushNamed(
-            AppRoute.companyUserDetails.name,
-            pathParameters: {
-              'companyUserId': user.id.toString(),
-            },
-          ),
-          child: ListTile(
-            title: Text(user.fullName),
-            subtitle: Text(
-              user.email,
-              style: textTheme.bodyMedium?.copyWith(
-                color: Colors.grey[600],
-              ),
-            ),
-            trailing: const Icon(Icons.arrow_forward_ios),
-          ),
-        ),
+            onTap: () => context.pushNamed(
+                  AppRoute.companyUserDetails.name,
+                  pathParameters: {
+                    'companyUserId': user.id.toString(),
+                  },
+                ),
+            child: Consumer(
+              builder: (context, ref, child) {
+                return ListTile(
+                  title: Text(user.fullName),
+                  subtitle: Text(
+                    user.email,
+                    style: textTheme.bodyMedium?.copyWith(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                );
+              },
+            )),
       ),
     );
   }
