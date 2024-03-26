914191ef3a92ef054c86ac0ace275053a107be44
jolayemi <ajolayemi82@gmail.com>
2024-03-26 16:17:53 +0100

    refactored companyUsersDetailsScreen contents

--- lib/src/features/company_users/screen/company_user_details/company_user_details_screen.dart
+++ lib/src/features/company_users/screen/company_user_details/company_user_details_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:irrigazione_iot/src/config/routes/app_router.dart';
+import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
 import 'package:irrigazione_iot/src/features/company_users/data/company_users_repository.dart';
 import 'package:irrigazione_iot/src/features/company_users/screen/company_user_details/company_user_details_screen_contents.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
 import 'package:irrigazione_iot/src/widgets/async_value_widget.dart';
 import 'package:irrigazione_iot/src/widgets/common_edit_icon_button.dart';
@@ -21,12 +23,17 @@ class CompanyUserDetailsScreen extends ConsumerWidget {
     final companyUser = ref.watch(
       companyUserStreamProvider(companyUserId: companyUserId),
     );
+    final loggedInUser = ref.watch(authRepositoryProvider).currentUser;
+    final isMe = loggedInUser != null &&
+        loggedInUser.email == companyUser.valueOrNull?.email;
+    final loc = context.loc;
+
     return Scaffold(
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
             AppSliverBar(
-              title: companyUser.valueOrNull?.fullName ?? '',
+              title: isMe ? loc.me : companyUser.valueOrNull?.fullName ?? '',
               actions: [
                 CommonEditIconButton(
                   onPressed: () => context.pushNamed(
@@ -35,6 +42,8 @@ class CompanyUserDetailsScreen extends ConsumerWidget {
                       'companyUserId': companyUserId,
                     },
                   ),
+                  alternateIsVisible:
+                      !isMe, // Hide the edit button if the user is viewing their own profile
                 ),
               ],
             ),
