19db9ff93eca2fb0af15c9daa25b2710a4ed56b2
jolayemi <ajolayemi82@gmail.com>
2024-03-28 18:36:40 +0100

    removed state error listener

--- lib/src/features/authentication/screen/sign_in/sign_in_screen.dart
+++ lib/src/features/authentication/screen/sign_in/sign_in_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/features/authentication/screen/sign_in/sign_in_controller.dart';
 import 'package:irrigazione_iot/src/features/authentication/screen/sign_in/sign_in_screen_contents.dart';
+import 'package:irrigazione_iot/src/utils/async_value_ui.dart';
 
 class SignInScreen extends ConsumerWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Listen to controller state for when error occurs
+    ref.listen(
+      signInControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
     return const Scaffold(
       body: SignInScreenContents(),
     );
