c237667f31eee3770a01bfd33241f2db3c95ffde
jolayemi <ajolayemi82@gmail.com>
2024-03-28 17:24:44 +0100

    added documentation and injected state management

--- lib/src/features/authentication/screen/sign_in/providers_sign_in_button.dart
+++ lib/src/features/authentication/screen/sign_in/providers_sign_in_button.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:irrigazione_iot/src/constants/app_sizes.dart';
 import 'package:irrigazione_iot/src/constants/breakpoints.dart';
+import 'package:irrigazione_iot/src/features/authentication/screen/sign_in/sign_in_controller.dart';
+import 'package:irrigazione_iot/src/utils/custom_controller_state.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/widgets/responsive_center.dart';
 
-class SliverAuthProviderSignInButton extends StatelessWidget {
+/// A sliver button that signs in with an authentication provider
+/// such as Google, Facebook, or Apple.
+class SliverAuthProviderSignInButton extends ConsumerWidget {
   const SliverAuthProviderSignInButton({
     super.key,
     required this.text,
-    this.onPressed,
-    required this.isLoading,
     required this.providerIcon,
+    required this.buttonStateKey,
+    this.onPressed,
   });
 
   final String text;
+  final String buttonStateKey;
   final VoidCallback? onPressed;
   final Widget providerIcon;
-  final bool isLoading;
 
   @override
-  Widget build(BuildContext context) {
-    final content = isLoading
+  Widget build(BuildContext context, WidgetRef ref) {
+    final thisButtonIsLoading = ref
+        .watch(signInControllerProvider)
+        .stateWithIdIsLoading(buttonStateKey);
+    final globalLoadingState =
+        ref.watch(signInControllerProvider).isGlobalLoading;
+    final content = thisButtonIsLoading
         ? const CircularProgressIndicator.adaptive()
         : Row(
             mainAxisSize: MainAxisSize.min,
@@ -35,15 +45,18 @@ class SliverAuthProviderSignInButton extends StatelessWidget {
                   ))
             ],
           );
-    return ResponsiveCenter(
-      maxContentWidth: Breakpoint.tablet,
-      padding: const EdgeInsets.only(
-        right: Sizes.p16,
-        left: Sizes.p16,
-      ),
-      child: OutlinedButton(
-        onPressed: onPressed,
-        child: content,
+    return IgnorePointer(
+      ignoring: globalLoadingState,
+      child: ResponsiveCenter(
+        maxContentWidth: Breakpoint.tablet,
+        padding: const EdgeInsets.only(
+          right: Sizes.p16,
+          left: Sizes.p16,
+        ),
+        child: OutlinedButton(
+          onPressed: onPressed,
+          child: content,
+        ),
       ),
     );
   }
