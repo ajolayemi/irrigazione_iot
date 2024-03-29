64b60f2d712e647a22fee0579b738c8d66d1c958
jolayemi <ajolayemi82@gmail.com>
2024-03-29 11:33:12 +0100

    defined an empty sign up controller

---
+++ lib/src/features/authentication/screen/sign_up/sign_up_controller.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'sign_up_controller.g.dart';
+
+@riverpod
+class SignUpController extends _$SignUpController {
+  @override
+  FutureOr<void> build() {
+    
+  }
+}
---
+++ lib/src/features/authentication/screen/sign_up/sign_up_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sign_up_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signUpControllerHash() => r'13752076d85843a3a08b7ac1643885d2789559e9';
+
+/// See also [SignUpController].
+@ProviderFor(SignUpController)
+final signUpControllerProvider =
+    AutoDisposeAsyncNotifierProvider<SignUpController, void>.internal(
+  SignUpController.new,
+  name: r'signUpControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$signUpControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SignUpController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
