964f21bf9586ba0f331e91c707a5fbdb0d066110
jolayemi <ajolayemi82@gmail.com>
2024-03-29 12:31:27 +0100

    defined widget for when user already has an account

---
+++ lib/src/features/authentication/widgets/already_have_an_account.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:irrigazione_iot/src/config/routes/routes_enums.dart';
+import 'package:irrigazione_iot/src/constants/app_sizes.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/custom_text_button.dart';
+
+class AlreadyHaveAnAccount extends StatelessWidget {
+  const AlreadyHaveAnAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final loc = context.loc;
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(loc.alreadyHaveAnAccountText),
+        gapW4,
+        CustomTextButton(
+          onPressed: () => context.goNamed(
+            AppRoute.signIn.name,
+          ),
+          text: loc.signInButtonTitle,
+        )
+      ],
+    );
+  }
+}
