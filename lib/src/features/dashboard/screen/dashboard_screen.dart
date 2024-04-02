66110ccc9fdd7940d017dafa0b6f5d6d9a20fca8
jolayemi <ajolayemi82@gmail.com>
2024-04-02 10:37:09 +0200

    updated icon buttons on home screen

--- lib/src/features/dashboard/screen/dashboard_screen.dart
+++ lib/src/features/dashboard/screen/dashboard_screen.dart
@@ -4,10 +4,29 @@ import 'package:go_router/go_router.dart';
 import 'package:irrigazione_iot/src/config/routes/routes_enums.dart';
 import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/widgets/alert_dialogs.dart';
 import 'package:irrigazione_iot/src/widgets/app_sliver_bar.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
+
+  // TODO avoid repeating this function as the same function is in more_options_screen_contents.dart
+  Future<bool> _showSignOutDialog(BuildContext context) async {
+    final loc = context.loc;
+    return await showAlertDialog(
+            context: context,
+            title: loc.logOutAlertDialogTitle,
+            content: loc.logOutAlertDialogContent,
+            cancelActionText: loc.alertDialogCancel,
+            defaultActionText: loc.logOutAlertDialogConfirm) ??
+        false;
+  }
+
+  void _signOut(BuildContext context, WidgetRef ref) async {
+    if (await _showSignOutDialog(context)) {
+      ref.read(authRepositoryProvider).signOut();
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -17,18 +36,26 @@ class DashboardScreen extends ConsumerWidget {
     // todo: dashboard screen should keep track of statuses
     // todo: for example, track if Sector pressure is okay, if collector battery is low, etc
     // todo: show "filtro" statuses as well
- 
+
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
           AppSliverBar(
             title: context.loc.welcome(user?.name ?? ''),
             actions: [
+              // Icon button to select a new company
               IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.person_3,
-                ),
+                icon: const Icon(Icons.business),
+                onPressed: () {
+                  context.pushNamed(
+                    AppRoute.companiesListGrid.name,
+                  );
+                },
+              ),
+              // Icon button to logout
+              IconButton(
+                icon: const Icon(Icons.logout),
+                onPressed: () => _signOut(context, ref),
               ),
             ],
           ),
