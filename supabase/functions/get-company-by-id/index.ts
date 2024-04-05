a6c2ee0c3dd5bb2e39d7f054d652e67767acd32c
jolayemi <ajolayemi82@gmail.com>
2024-04-05 19:32:21 +0200

    refactored get-company-by-id function

--- supabase/functions/get-company-by-id/index.ts
+++ supabase/functions/get-company-by-id/index.ts
@@ -1,31 +1,10 @@
-import {createClient} from "https://esm.sh/@supabase/supabase-js@2.42.0";
+import {createSupabaseClient} from "../_utils/supabaseClient.ts";
 
 console.log(`Function "get-companies-by-id" up and running!`);
 
 Deno.serve(async (req) => {
-  const authHeaders = req.headers;
-
   try {
-    const supabaseUrl = Deno.env.get("SUPABASE_URL");
-    // console.log(`Supabase url from deno env: ${supabaseUrl}`);
-
-    const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
-    // console.log(`Supabase anon key from deno env: ${supabaseAnonKey}`);
-
-    // Create a Supabase client with the Auth context of the logged in user
-    const supabaseClient = createClient(
-      supabaseUrl ?? "",
-      supabaseAnonKey ?? "",
-      // Create a global Auth context of the user that called the function
-      // This allows the RLS policies to be applied correctly
-      {
-        global: {
-          headers: {
-            Authorization: authHeaders.get("Authorization")!,
-          },
-        },
-      }
-    );
+    const supabaseClient = createSupabaseClient(req);
 
     // Get the id provided in the request body
     const {id} = await req.json();
@@ -34,10 +13,12 @@ Deno.serve(async (req) => {
     const {data: company, error} = await supabaseClient
       .from("companies")
       .select("*")
-      .eq("id", id as number).maybeSingle();
+      .eq("id", id as number)
+      .maybeSingle();
     if (error) throw error;
     return new Response(JSON.stringify(company), {
       headers: {"Content-Type": "application/json"},
+      status: 200,
     });
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
