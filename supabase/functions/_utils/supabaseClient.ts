9f1956af322f3c6bee95d0acffe1d1fa0b188688
jolayemi <ajolayemi82@gmail.com>
2024-04-05 19:37:16 +0200

    updated function name

--- supabase/functions/_utils/supabaseClient.ts
+++ supabase/functions/_utils/supabaseClient.ts
@@ -6,7 +6,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.0";
  * @param req A request object
  * @returns A Supabase client
  */
-export const createSupabaseClient = (req: Request) => {
+export const createEdgeSupabaseClient = (req: Request) => {
     const supabaseUrl = Deno.env.get("SUPABASE_URL");
     const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
     const authHeaders = req.headers;
