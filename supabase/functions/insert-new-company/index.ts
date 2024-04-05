6f9852024d0e99a51e519db20652e0fc6198858b
jolayemi <ajolayemi82@gmail.com>
2024-04-05 19:51:51 +0200

    wrote logic to insert companies

---
+++ supabase/functions/insert-new-company/index.ts
@@ -0,0 +1,29 @@
+import {createEdgeSupabaseClient} from "../_utils/supabaseClient.ts";
+
+console.log(`Function "insert-new-company" up and running!`);
+
+Deno.serve(async (req: Request): Promise<Response> => {
+  try {
+    const supabaseClient = createEdgeSupabaseClient(req);
+
+    // Get the company data provided in the request body
+    const {data} = await req.json();
+
+    // Insert the new company
+    const {error} = await supabaseClient.from("companies").insert(data);
+    if (error) throw error;
+    return new Response(
+      JSON.stringify({message: "Company inserted successfully!"}),
+      {
+        headers: {"Content-Type": "application/json"},
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(`An error occurred in insert-new-company: ${error.message}`);
+    return new Response(JSON.stringify({error: error.message}), {
+      status: 400,
+      headers: {"Content-Type": "application/json"},
+    });
+  }
+});
