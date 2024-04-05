7e4a2985bc4fd3fb086149390f58001e60360ccb
jolayemi <ajolayemi82@gmail.com>
2024-04-05 22:05:48 +0200

    completed edge function logic to update company data in database

--- supabase/functions/update-company/index.ts
+++ supabase/functions/update-company/index.ts
@@ -1,29 +1,31 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { createEdgeSupabaseClient } from "../_utils/supabaseClient.ts";
 
-console.log("Hello from Functions!")
+console.log(`Function "update-company" up and running!`);
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+Deno.serve(async (req: Request): Promise<Response> => {
+  try {
+    const supabaseClient = createEdgeSupabaseClient(req);
+
+    // Get the company data provided in the request body
+    const {toUpdate} = await req.json();
+
+    // Update the company
+    const {data, error} = await supabaseClient.from("companies").update(toUpdate).eq("id", toUpdate.id).select();
+    
+    if (error) throw error;
+
+    return new Response(
+      JSON.stringify(data),
+      {
+        headers: {"Content-Type": "application/json"},
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(`An error occurred in update-company: ${error.message}`);
+    return new Response(JSON.stringify({error: error.message}), {
+      status: 400,
+      headers: {"Content-Type": "application/json"},
+    });
   }
-
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/update-company' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+});
