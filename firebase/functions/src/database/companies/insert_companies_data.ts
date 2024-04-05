6d3a7432761d2f37df3c1d1fe63aa82d757fbaf2
jolayemi <ajolayemi82@gmail.com>
2024-04-05 20:19:46 +0200

    fixed bug

---
+++ firebase/functions/src/database/companies/insert_companies_data.ts
@@ -0,0 +1,16 @@
+import { TablesInsert } from "../../../schemas/supabase";
+import { createSupabaseClient } from "../../services/supabase_client";
+
+export const insertCompany = async (data: TablesInsert<'companies'>): Promise<void> => {
+    // Get the Supabase client
+    const supabase = await createSupabaseClient();
+    // Insert the company
+    const {error} = await supabase.functions.invoke("insert-new-company", {
+        body: {"data": data},
+    });
+
+    if (error) {
+        console.error(`An error occurred in insertCompany: ${error.message}`);
+        throw error;
+    }
+}
