4af03b70dab9a51a965f4d3783ea23cfb2ed30f8
jolayemi <ajolayemi82@gmail.com>
2024-04-06 20:15:05 +0200

    wrote edge function to get pump by mqtt msg name

---
+++ supabase/functions/get-pump-by-mqtt-msg-name/index.ts
@@ -0,0 +1,8 @@
+import {commonGetByMqttMsgName} from "../_utils/commonDatabaseOperations.ts";
+import {TableNames} from "../_utils/tableConstants.ts";
+
+console.log(`Function "get-pump-by-mqtt-msg-name" up and running!`);
+
+Deno.serve(async (req) => {
+  return await commonGetByMqttMsgName(req, TableNames.pumps, "id");
+});
