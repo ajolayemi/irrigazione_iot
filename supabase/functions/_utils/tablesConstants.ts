58d5c65fe7a7380585a54e249a8e4dd188ab53e6
jolayemi <ajolayemi82@gmail.com>
2024-04-06 16:55:11 +0200

    defined a class that holds tables names

---
+++ supabase/functions/_utils/tablesConstants.ts
@@ -0,0 +1,21 @@
+/**
+ * A class to store the names of the tables in the database
+ */
+export class TablesNames {
+  static readonly companies = "companies";
+  static readonly companies_user = "companies_user";
+  static readonly boards = "boards";
+  static readonly boards_statuses = "boards_statuses";
+  static readonly collectors = "collectors";
+  static readonly collector_sectors = "collector_sectors";
+  static readonly collector_pressures = "collector_pressures";
+  static readonly pumps = "pumps";
+  static readonly pump_pressures = "pump_pressures";
+  static readonly pump_statuses = "pump_statuses";
+  static readonly species = "species";
+  static readonly varieties = "varieties";
+  static readonly sectors = "sectors";
+  static readonly sector_pressures = "sector_pressures";
+  static readonly sector_statuses = "sector_statuses";
+  static readonly sector_pumps = "sector_pumps";
+}
