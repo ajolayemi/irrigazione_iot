d6108847f662f6fe6ee79b1be5a6ef998a0b5ec8
jolayemi <ajolayemi82@gmail.com>
2024-04-06 16:28:06 +0200

    refactored edge functions that updated db data

--- supabase/functions/update-company-user/index.ts
+++ supabase/functions/update-company-user/index.ts
@@ -1,29 +1,7 @@
-import {createEdgeSupabaseClient} from "../_utils/supabaseClient.ts";
+import {commonUpdate} from "../_utils/commonDatabaseOperations.ts";
 
 console.log(`Function "update-company-user" up and running!`);
 
 Deno.serve(async (req: Request): Promise<Response> => {
-  try {
-    const supabaseClient = createEdgeSupabaseClient(req);
-
-    // Get the company-user data provided in the request body
-    const {toUpdate} = await req.json();
-
-    // Insert the new company-user
-    const {data, error} = await supabaseClient.from("companies_user").update(toUpdate).eq("id", toUpdate.id).select();
-    if (error) throw error;
-    return new Response(
-      JSON.stringify({data}),
-      {
-        headers: {"Content-Type": "application/json"},
-        status: 200,
-      }
-    );
-  } catch (error) {
-    console.error(`An error occurred in update-company-user: ${error.message}`);
-    return new Response(JSON.stringify({error: error.message}), {
-      status: 400,
-      headers: {"Content-Type": "application/json"},
-    });
-  }
+  return await commonUpdate(req, "companies_user");
 });
--- supabase/functions/update-company/index.ts
+++ supabase/functions/update-company/index.ts
@@ -1,31 +1,7 @@
-import { createEdgeSupabaseClient } from "../_utils/supabaseClient.ts";
+import {commonUpdate} from "../_utils/commonDatabaseOperations.ts";
 
 console.log(`Function "update-company" up and running!`);
 
 Deno.serve(async (req: Request): Promise<Response> => {
-  try {
-    const supabaseClient = createEdgeSupabaseClient(req);
-
-    // Get the company data provided in the request body
-    const {toUpdate} = await req.json();
-
-    // Update the company
-    const {data, error} = await supabaseClient.from("companies").update(toUpdate).eq("id", toUpdate.id).select();
-    
-    if (error) throw error;
-
-    return new Response(
-      JSON.stringify(data),
-      {
-        headers: {"Content-Type": "application/json"},
-        status: 200,
-      }
-    );
-  } catch (error) {
-    console.error(`An error occurred in update-company: ${error.message}`);
-    return new Response(JSON.stringify({error: error.message}), {
-      status: 400,
-      headers: {"Content-Type": "application/json"},
-    });
-  }
+  return await commonUpdate(req, "companies");
 });
