50ae3a1877cac917e1fdb6d89e796958163a9282
jolayemi <ajolayemi82@gmail.com>
2024-04-06 16:48:18 +0200

    refactored getter logic

--- supabase/functions/get-company-by-id/index.ts
+++ supabase/functions/get-company-by-id/index.ts
@@ -1,30 +1,6 @@
-import {createEdgeSupabaseClient} from "../_utils/supabaseClient.ts";
-
+import {commonGetById} from "../_utils/commonDatabaseOperations.ts";
 console.log(`Function "get-companies-by-id" up and running!`);
 
 Deno.serve(async (req) => {
-  try {
-    const supabaseClient = createEdgeSupabaseClient(req);
-
-    // Get the id provided in the request body
-    const {id} = await req.json();
-
-
-    const {data: company, error} = await supabaseClient
-      .from("companies")
-      .select("*")
-      .eq("id", id as number)
-      .maybeSingle();
-    if (error) throw error;
-    return new Response(JSON.stringify(company), {
-      headers: {"Content-Type": "application/json"},
-      status: 200,
-    });
-  } catch (error) {
-    console.error(`An error occurred: ${error.message}`);
-    return new Response(JSON.stringify({error: error.message}), {
-      status: 400,
-      headers: {"Content-Type": "application/json"},
-    });
-  }
+  return await commonGetById(req, "companies");
 });
