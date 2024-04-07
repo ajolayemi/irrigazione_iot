83802111040069b706ebea5820d56167d2deed18
jolayemi <ajolayemi82@gmail.com>
2024-04-07 20:00:36 +0200

    updated columns returned by get-sector-by-mqtt-msg-name

--- supabase/functions/get-sector-by-mqtt-msg-name/index.ts
+++ supabase/functions/get-sector-by-mqtt-msg-name/index.ts
@@ -3,6 +3,10 @@ import {commonGetByMqttMsgName} from "../_utils/commonDatabaseOperations.ts";
 
 console.log(`Function "get-sector-by-mqtt-msg-name" up and running!`);
 
+
+/**
+ * Get a sector by its MQTT message name. The id of the sector is returned.
+ */
 Deno.serve(
-  async (req) => await commonGetByMqttMsgName(req, TableNames.sectors)
+  async (req) => await commonGetByMqttMsgName(req, TableNames.sectors, "id")
 );
