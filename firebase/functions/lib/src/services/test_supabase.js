c02f33c83cb88890485140ab1844c08ab84dca73
jolayemi <ajolayemi82@gmail.com>
2024-04-07 12:14:01 +0200

    updated test supabase

--- firebase/functions/lib/src/services/test_supabase.js
+++ firebase/functions/lib/src/services/test_supabase.js
@@ -1,12 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const read_company_data_1 = require("../database/companies/read_company_data");
+const insert_pump_data_1 = require("../database/pumps/insert_pump_data");
 const read_pump_data_1 = require("../database/pumps/read_pump_data");
 const secrets_1 = require("./secrets");
 (async () => {
-    // const testCompany = await getCompanyById("3");
-    // console.log(testCompany);
-    const pump = await (0, read_pump_data_1.getPumpByMqttMsgName)("p2");
+    const testCompany = await (0, read_company_data_1.getCompanyById)("1");
+    console.log(testCompany);
+    const p = await (0, read_pump_data_1.getPumpById)("1");
+    console.log(p);
+    const pump = await (0, read_pump_data_1.getPumpByMqttMsgName)("p1");
     console.log(pump);
+    const testPumpPressure = {
+        created_at: new Date().toISOString(),
+        pressure: 1.0,
+        pump_id: pump.id,
+    };
+    await (0, insert_pump_data_1.insertPumpPressure)(testPumpPressure);
 })();
 (async () => {
     const url = await (0, secrets_1.getSecretFromCloud)("SUPABASE_URL");
--- firebase/functions/src/services/test_supabase.ts
+++ firebase/functions/src/services/test_supabase.ts
@@ -1,11 +1,25 @@
-import { getPumpByMqttMsgName} from "../database/pumps/read_pump_data";
+import {TablesInsert} from "../../schemas/database.types";
+import {getCompanyById} from "../database/companies/read_company_data";
+import {insertPumpPressure} from "../database/pumps/insert_pump_data";
+import { getPumpById, getPumpByMqttMsgName} from "../database/pumps/read_pump_data";
 import {getSecretFromCloud} from "./secrets";
 
 (async () => {
-    // const testCompany = await getCompanyById("3");
-    // console.log(testCompany);
-    const pump = await getPumpByMqttMsgName("p2");
+    const testCompany = await getCompanyById("1");
+    console.log(testCompany);
+
+    const p = await getPumpById("1");
+    console.log(p);
+    const pump = await getPumpByMqttMsgName("p1");
     console.log(pump);
+
+    const testPumpPressure: TablesInsert<"pump_pressures"> = {
+      created_at: new Date().toISOString(),
+      pressure: 1.0,
+      pump_id: pump.id,
+    };
+
+    await insertPumpPressure(testPumpPressure)
 })();
 
 (async () => {
