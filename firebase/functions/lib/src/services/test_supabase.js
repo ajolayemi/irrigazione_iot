8ec34347cf46ca9289e925e132477470b0e9c8cd
jolayemi <ajolayemi82@gmail.com>
2024-04-07 12:19:42 +0200

    updated test_supabase module

--- firebase/functions/lib/src/services/test_supabase.js
+++ firebase/functions/lib/src/services/test_supabase.js
@@ -1,22 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const read_company_data_1 = require("../database/companies/read_company_data");
-const insert_pump_data_1 = require("../database/pumps/insert_pump_data");
 const read_pump_data_1 = require("../database/pumps/read_pump_data");
 const secrets_1 = require("./secrets");
 (async () => {
-    const testCompany = await (0, read_company_data_1.getCompanyById)("1");
-    console.log(testCompany);
+    // const testCompany = await getCompanyById("1");
+    // console.log(testCompany);
     const p = await (0, read_pump_data_1.getPumpById)("1");
     console.log(p);
-    const pump = await (0, read_pump_data_1.getPumpByMqttMsgName)("p1");
-    console.log(pump);
-    const testPumpPressure = {
-        created_at: new Date().toISOString(),
-        pressure: 1.0,
-        pump_id: pump.id,
-    };
-    await (0, insert_pump_data_1.insertPumpPressure)(testPumpPressure);
+    // const pump = await getPumpByMqttMsgName("p1");
+    // console.log(pump);
+    // const testPumpPressure: TablesInsert<"pump_pressures"> = {
+    //   created_at: new Date().toISOString(),
+    //   pressure: 1.0,
+    //   pump_id: pump.id,
+    // };
+    // await insertPumpPressure(testPumpPressure);
+    // const testPumpStatus: TablesInsert<"pump_statuses"> = {
+    //   created_at: new Date().toISOString(),
+    //   pump_id: p.id,
+    //   status: "2",
+    // };
+    // await insertPumpStatus(testPumpStatus);
 })();
 (async () => {
     const url = await (0, secrets_1.getSecretFromCloud)("SUPABASE_URL");
--- firebase/functions/src/services/test_supabase.ts
+++ firebase/functions/src/services/test_supabase.ts
@@ -1,25 +1,30 @@
-import {TablesInsert} from "../../schemas/database.types";
-import {getCompanyById} from "../database/companies/read_company_data";
-import {insertPumpPressure} from "../database/pumps/insert_pump_data";
-import { getPumpById, getPumpByMqttMsgName} from "../database/pumps/read_pump_data";
+import {getPumpById} from "../database/pumps/read_pump_data";
 import {getSecretFromCloud} from "./secrets";
 
 (async () => {
-    const testCompany = await getCompanyById("1");
-    console.log(testCompany);
+  // const testCompany = await getCompanyById("1");
+  // console.log(testCompany);
 
-    const p = await getPumpById("1");
-    console.log(p);
-    const pump = await getPumpByMqttMsgName("p1");
-    console.log(pump);
+  const p = await getPumpById("1");
+  console.log(p);
+  // const pump = await getPumpByMqttMsgName("p1");
+  // console.log(pump);
 
-    const testPumpPressure: TablesInsert<"pump_pressures"> = {
-      created_at: new Date().toISOString(),
-      pressure: 1.0,
-      pump_id: pump.id,
-    };
+  // const testPumpPressure: TablesInsert<"pump_pressures"> = {
+  //   created_at: new Date().toISOString(),
+  //   pressure: 1.0,
+  //   pump_id: pump.id,
+  // };
 
-    await insertPumpPressure(testPumpPressure)
+  // await insertPumpPressure(testPumpPressure);
+
+  // const testPumpStatus: TablesInsert<"pump_statuses"> = {
+  //   created_at: new Date().toISOString(),
+  //   pump_id: p.id,
+  //   status: "2",
+  // };
+
+  // await insertPumpStatus(testPumpStatus);
 })();
 
 (async () => {
