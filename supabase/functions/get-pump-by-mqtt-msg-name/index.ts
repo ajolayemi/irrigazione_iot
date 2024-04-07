31cccab3f889b6a6e0aec58cb207415b5614901a
jolayemi <ajolayemi82@gmail.com>
2024-04-07 09:53:01 +0200

    added a new column to be returned in get-pump-by-mqtt-msg-name

--- supabase/functions/get-pump-by-mqtt-msg-name/index.ts
+++ supabase/functions/get-pump-by-mqtt-msg-name/index.ts
@@ -4,5 +4,5 @@ import {TableNames} from "../_utils/tableConstants.ts";
 console.log(`Function "get-pump-by-mqtt-msg-name" up and running!`);
 
 Deno.serve(async (req) => {
-  return await commonGetByMqttMsgName(req, TableNames.pumps, "id");
+  return await commonGetByMqttMsgName(req, TableNames.pumps, "id, company_id");
 });
