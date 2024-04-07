7207ca5fb6965f6527568074a17dd2784f3a1b45
jolayemi <ajolayemi82@gmail.com>
2024-04-07 11:54:11 +0200

    renamed insertion edge functions

--- supabase/functions/insert-company-user/index.ts
+++ supabase/functions/insert-company-user/index.ts
@@ -1,7 +1,7 @@
 import {commonInsert} from "../_utils/commonDatabaseOperations.ts";
 import {TableNames} from "../_utils/tableConstants.ts";
 
-console.log(`Function "insert-new-company-user" up and running!`);
+console.log(`Function "insert-company-user" up and running!`);
 
 Deno.serve(async (req: Request): Promise<Response> => {
   return await commonInsert(req, TableNames.company_users);
