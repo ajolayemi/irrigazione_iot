79ac7476e2f10e7b5581282b4241e2f2bf48a230
jolayemi <ajolayemi82@gmail.com>
2024-04-08 19:50:32 +0200

    deleted some edge funcs

--- supabase/functions/get-company-by-id/index.ts
+++
@@ -1,8 +0,0 @@
-import {commonGetById} from "../_utils/commonDatabaseOperations.ts";
-import {TableNames} from "../_utils/tableConstants.ts";
-
-console.log(`Function "get-companies-by-id" up and running!`);
-
-Deno.serve(async (req) => {
-  return await commonGetById(req, TableNames.companies);
-});
