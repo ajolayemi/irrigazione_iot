96653f20ea5df755a6f40aed26c5284500111f0b
jolayemi <ajolayemi82@gmail.com>
2024-04-08 12:06:20 +0200

    wrote edge func to delete sectors

---
+++ supabase/functions/delete-sectors/index.ts
@@ -0,0 +1,18 @@
+import {TableNames} from "../_utils/tableConstants.ts";
+import {commonDelete} from "../_utils/commonDatabaseOperations.ts";
+
+console.log(`Function "delete-sector" up and running!`);
+
+Deno.serve(async (req) => await commonDelete(req, TableNames.sectors));
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/delete-sector' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
