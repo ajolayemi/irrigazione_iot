921e862f5e31aec749cbe3be7b831f45433267c8
jolayemi <ajolayemi82@gmail.com>
2024-04-08 16:52:08 +0200

    wrote funcs

---
+++ supabase/functions/insert-collector-sector/index.ts
@@ -0,0 +1,20 @@
+import {TableNames} from "../_utils/tableConstants.ts";
+import {commonInsert} from "../_utils/commonDatabaseOperations.ts";
+
+console.log(`Function 'insert-collector-sector' invoked`);
+
+Deno.serve(
+  async (req) => await commonInsert(req, TableNames.collector_sectors)
+);
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/insert-collector-sector' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
