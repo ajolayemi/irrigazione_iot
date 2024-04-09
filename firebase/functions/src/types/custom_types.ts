6305741d1fbe690ddd56c753f72cbff1e8f793d0
jolayemi <ajolayemi82@gmail.com>
2024-04-09 13:28:22 +0200

    delete types module

--- firebase/functions/src/types/custom_types.ts
+++
@@ -1,9 +0,0 @@
-/// A custom JSON type
-export type CustomJSON =
-  | string
-  | number
-  | boolean
-  | null
-  | {[key: string]: CustomJSON | undefined}
-  | {[key: string]: any}
-  | CustomJSON[];
