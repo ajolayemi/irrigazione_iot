1feb331206ab5acc168297f27df9d7562c052b81
jolayemi <ajolayemi82@gmail.com>
2024-04-09 19:29:09 +0200

    refactored cloud func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,15 @@
 import admin from "firebase-admin";
-import {logger} from "firebase-functions/v1";
 import {pubsub} from "firebase-functions/v2";
 import {processPressureMessageFromPubSub} from "./utils/process_pressure_message";
-// import {processPressureMessageFromPubSub} from "./utils/process_pressure_message";
 
 admin.initializeApp();
 
 exports.processPressureMessages = pubsub.onMessagePublished(
   "pressure",
   async (event) => {
-    const message = event.data.message;
-    const data = Buffer.from(message.data, "base64").toJSON();
-    logger.info(`Received pressure message: ${JSON.stringify(data)}`);
+    const message = event.data.message.json;
     const successInProcessingMessage = await processPressureMessageFromPubSub(
-      data
+      message
     );
     return Promise.resolve(successInProcessingMessage);
   }
