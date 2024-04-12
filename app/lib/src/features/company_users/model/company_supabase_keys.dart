c937d1cd48ecc80984105061c1de657668d3ea3f
jolayemi <ajolayemi82@gmail.com>
2024-04-12 16:11:08 +0200

    created model to gold keys to link supabase and flutter classes

---
+++ app/lib/src/features/company_users/model/company_supabase_keys.dart
@@ -0,0 +1,15 @@
+/// A representation of the keys used to interact with the company table in the database
+class CompanySupabaseKeys {
+  const CompanySupabaseKeys._();
+  
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String email = 'email';
+  static const String phoneNumber = 'phone_number';
+  static const String registeredOfficeAddress = 'registered_office_address';
+  static const String cf = 'cf';
+  static const String piva = 'piva';
+  static const String imageUrl = 'image_url';
+  static const String createdAt = 'created_at';
+  static const String updatedAt = 'updated_at';
+}
