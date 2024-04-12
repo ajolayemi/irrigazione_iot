29744f5ceceff722eb5e685cae275d435aa80e2d
jolayemi <ajolayemi82@gmail.com>
2024-04-12 17:13:34 +0200

    refactored pump class

--- app/lib/src/features/pumps/model/pump.dart
+++ app/lib/src/features/pumps/model/pump.dart
@@ -1,51 +1,35 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
-
-
+import 'package:irrigazione_iot/src/features/pumps/model/pump_supabase_keys.dart';
 
 // TODO: add pump company
 // TODO: prevalenza, portata,
-
 class Pump extends Equatable {
   const Pump({
     required this.id,
     required this.name,
     required this.capacityInVolume,
     required this.consumeRateInKw,
-    required this.commandForOn,
-    required this.commandForOff,
     required this.companyId,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.turnOnCommand,
+    required this.turnOffCommand,
+    required this.mqttMessageName,
+    required this.hasFilter,
   });
 
-  const Pump.empty()
-      : id = '',
-        name = '',
-        capacityInVolume = 0,
-        consumeRateInKw = 0,
-        commandForOn = '',
-        commandForOff = '',
-        companyId = '';
-
-  /// A unique identifier for the pump
   final String id;
-
-  /// The name of the pump
   final String name;
-
-  /// The capacity of the pump in volume
   final double capacityInVolume;
-
-  /// The consume rate of the pump in kilowatts
   final double consumeRateInKw;
-
-  /// The command to turn on the pump, i.e the one sent to and from MQTT
-  final String commandForOn;
-
-  /// The command to turn off the pump, i.e the one sent to and from MQTT
-  final String commandForOff;
-
-  /// An id to identify the company the pump belongs to
   final String companyId;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final String turnOnCommand;
+  final String turnOffCommand;
+  final String mqttMessageName;
+  final bool hasFilter;
 
   @override
   List<Object> get props {
@@ -54,9 +38,13 @@ class Pump extends Equatable {
       name,
       capacityInVolume,
       consumeRateInKw,
-      commandForOn,
-      commandForOff,
       companyId,
+      createdAt,
+      updatedAt,
+      turnOnCommand,
+      turnOffCommand,
+      mqttMessageName,
+      hasFilter,
     ];
   }
 
@@ -65,51 +53,64 @@ class Pump extends Equatable {
     String? name,
     double? capacityInVolume,
     double? consumeRateInKw,
-    String? commandForOn,
-    String? commandForOff,
     String? companyId,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    String? turnOnCommand,
+    String? turnOffCommand,
+    String? mqttMessageName,
+    bool? hasFilter,
   }) {
     return Pump(
       id: id ?? this.id,
       name: name ?? this.name,
       capacityInVolume: capacityInVolume ?? this.capacityInVolume,
       consumeRateInKw: consumeRateInKw ?? this.consumeRateInKw,
-      commandForOn: commandForOn ?? this.commandForOn,
-      commandForOff: commandForOff ?? this.commandForOff,
       companyId: companyId ?? this.companyId,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      turnOnCommand: turnOnCommand ?? this.turnOnCommand,
+      turnOffCommand: turnOffCommand ?? this.turnOffCommand,
+      mqttMessageName: mqttMessageName ?? this.mqttMessageName,
+      hasFilter: hasFilter ?? this.hasFilter,
     );
   }
 
-  @override
-  bool get stringify => true;
-
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJson() {
     return <String, dynamic>{
-      'id': id,
-      'name': name,
-      'capacityInVolume': capacityInVolume,
-      'consumeRateInKw': consumeRateInKw,
-      'commandForOn': commandForOn,
-      'commandForOff': commandForOff,
-      'companyId': companyId
+      PumpSupabaseKeys.id: id,
+      PumpSupabaseKeys.name: name,
+      PumpSupabaseKeys.capacityInVolume: capacityInVolume,
+      PumpSupabaseKeys.consumeRateInKw: consumeRateInKw,
+      PumpSupabaseKeys.companyId: companyId,
+      PumpSupabaseKeys.createdAt: createdAt,
+      PumpSupabaseKeys.updatedAt: updatedAt,
+      PumpSupabaseKeys.turnOnCommand: turnOnCommand,
+      PumpSupabaseKeys.turnOffCommand: turnOffCommand,
+      PumpSupabaseKeys.mqttMessageName: mqttMessageName,
+      PumpSupabaseKeys.hasFilter: hasFilter,
     };
   }
 
-  factory Pump.fromMap(Map<String, dynamic> map) {
+  static Pump fromJson(Map<String, dynamic> json) {
     return Pump(
-      id: map['id'] as String,
-      name: map['name'] as String,
-      capacityInVolume: map['capacityInVolume'] as double,
-      consumeRateInKw: map['consumeRateInKw'] as double,
-      commandForOn: map['commandForOn'] as String,
-      commandForOff: map['commandForOff'] as String,
-      companyId: map['companyId'] as String,
+      id: json[PumpSupabaseKeys.id] as String,
+      name: json[PumpSupabaseKeys.name] as String,
+      capacityInVolume: json[PumpSupabaseKeys.capacityInVolume] as double,
+      consumeRateInKw: json[PumpSupabaseKeys.consumeRateInKw] as double,
+      companyId: json[PumpSupabaseKeys.companyId] as String,
+      createdAt: json[PumpSupabaseKeys.createdAt] as DateTime,
+      updatedAt: json[PumpSupabaseKeys.updatedAt] as DateTime,
+      turnOnCommand: json[PumpSupabaseKeys.turnOnCommand] as String,
+      turnOffCommand: json[PumpSupabaseKeys.turnOffCommand] as String,
+      mqttMessageName: json[PumpSupabaseKeys.mqttMessageName] as String,
+      hasFilter: json[PumpSupabaseKeys.hasFilter] as bool,
     );
   }
 }
 
 extension PumpX on Pump {
   String getStatusCommand(bool status) {
-    return status ? commandForOn : commandForOff;
+    return status ? turnOnCommand : turnOffCommand;
   }
 }
