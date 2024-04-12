d3ebb8bed754112479404b6c1fc6fb49aa157b6d
jolayemi <ajolayemi82@gmail.com>
2024-04-12 17:05:53 +0200

    created class to hold keys for pump in database

---
+++ app/lib/src/features/pumps/model/pump_supabase_keys.dart
@@ -0,0 +1,15 @@
+class PumpSupabaseKeys {
+  const PumpSupabaseKeys._();
+
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String capacityInVolume = 'capacity_in_volume';
+  static const String consumeRateInKw = 'consume_rate_in_kw';
+  static const String companyId = 'company_id';
+  static const String createdAt = 'created_at';
+  static const String updatedAt = 'updated_at';
+  static const String turnOnCommand = 'turn_on_command';
+  static const String turnOffCommand = 'turn_off_command';
+  static const String mqttMessageName = 'mqtt_msg_name';
+  static const String hasFilter = 'has_filter';
+}
