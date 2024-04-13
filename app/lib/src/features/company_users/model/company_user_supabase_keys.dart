6fb63ebbabafbc6fcef98af871a8688622e1731a
jolayemi <ajolayemi82@gmail.com>
2024-04-13 13:41:49 +0200

    added module to gold supabase columns for company users

---
+++ app/lib/src/features/company_users/model/company_user_supabase_keys.dart
@@ -0,0 +1,9 @@
+class CompanyUserSupabaseKeys {
+  static const String id = 'id';
+  static const String email = 'email';
+  static const String fullName = 'full_name';
+  static const String role = 'role';
+  static const String companyId = 'company_id';
+  static const String createdAt = 'created_at';
+  static const String updatedAt = 'updated_at';
+}
