f1dce2ef4aa21e933f0b309ae7d15c47029307a2
jolayemi <ajolayemi82@gmail.com>
2024-04-13 12:20:04 +0200

    added module to hold supabase column names for collector

---
+++ app/lib/src/features/collectors/model/collector_supabase_keys.dart
@@ -0,0 +1,9 @@
+class CollectorSupabaseKeys {
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String connectedFilterName = 'connected_filter_name';
+  static const String createdAt = 'created_at';
+  static const String updatedAt = 'updated_at';
+  static const String companyId = 'company_id';
+  static const String mqttMsgName = 'mqtt_msg_name';
+}
