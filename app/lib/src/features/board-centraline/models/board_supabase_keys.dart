cdfe9cd73de73de310a1251ece05d682e4d25f63
jolayemi <ajolayemi82@gmail.com>
2024-04-13 13:33:12 +0200

    added module to hold supabase column names for boards

---
+++ app/lib/src/features/board-centraline/models/board_supabase_keys.dart
@@ -0,0 +1,11 @@
+class BoardSupabaseKeys {
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String model = 'model';
+  static const String serialNumber = 'serial_number';
+  static const String collectorId = 'collector_id';
+  static const String companyId = 'company_id';
+  static const String createdAt = 'created_at';
+  static const String updatedAt = 'updated_at';
+  static const String mqttMsgName = 'mqtt_msg_name';
+}
