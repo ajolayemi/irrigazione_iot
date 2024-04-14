0f047a152bfae41708e156ffe734b57c474b202c
jolayemi <ajolayemi82@gmail.com>
2024-04-14 19:16:18 +0200

    created module to hold database column names for pump flows

---
+++ app/lib/src/features/pumps/model/pump_flow_database_keys.dart
@@ -0,0 +1,8 @@
+class PumpFlowDatabaseKeys {
+  const PumpFlowDatabaseKeys._();
+
+  static const String id = 'id';
+  static const String flow = 'flow';
+  static const String pumpId = 'pump_id';
+  static const String createdAt = 'created_at';
+}
