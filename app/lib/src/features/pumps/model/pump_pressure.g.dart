ca0862e341ad8b52f60a7d5d235b130450014d4b
jolayemi <ajolayemi82@gmail.com>
2024-04-14 19:14:08 +0200

    created pump pressure model and it's database keys

---
+++ app/lib/src/features/pumps/model/pump_pressure.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pump_pressure.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PumpPressure _$PumpPressureFromJson(Map<String, dynamic> json) => PumpPressure(
+      id: json['id'] as String,
+      pumpId: json['pumpId'] as String,
+      filterInPressure: (json['filter_in_pressure'] as num).toDouble(),
+      filterOutPressure: (json['filter_out_pressure'] as num).toDouble(),
+      createdAt: DateTime.parse(json['created_at'] as String),
+    );
+
+Map<String, dynamic> _$PumpPressureToJson(PumpPressure instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'pumpId': instance.pumpId,
+      'filter_in_pressure': instance.filterInPressure,
+      'filter_out_pressure': instance.filterOutPressure,
+      'created_at': instance.createdAt.toIso8601String(),
+    };
