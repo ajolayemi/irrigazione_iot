17fbeaf3c06ad033817e8be5a8f19d75bfb0ef06
jolayemi <ajolayemi82@gmail.com>
2024-04-14 19:07:59 +0200

    formatted module

--- app/lib/src/features/pumps/model/pump_status.dart
+++ app/lib/src/features/pumps/model/pump_status.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-// A representation of the status of a pump.
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
-
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
 
 part 'pump_status.g.dart';
