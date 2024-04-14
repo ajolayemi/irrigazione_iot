a73e187cbe7d1ea275b9ec826bae5e4d8aa7845b
jolayemi <ajolayemi82@gmail.com>
2024-04-14 17:08:14 +0200

    added module to hold supabase column names for collector sector

---
+++ app/lib/src/features/collectors/model/collector_sector_database_keys.dart
@@ -0,0 +1,8 @@
+class CollectorSectorDatabaseKeys {
+  const CollectorSectorDatabaseKeys._();
+
+  static const String id = 'id';
+  static const String collectorId = 'collector_id';
+  static const String sectorId = 'sector_id';
+  static const String createdAt = 'created_at';
+}
