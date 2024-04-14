6a5ea6b380c16ba3c6570a934be694d98da99f21
jolayemi <ajolayemi82@gmail.com>
2024-04-14 16:12:48 +0200

    added module to hold supabase column names for collector pressure

---
+++ app/lib/src/features/collectors/model/collector_pressure_database_keys.dart
@@ -0,0 +1,10 @@
+class CollectorPressureDatabaseKeys {
+  const CollectorPressureDatabaseKeys._();
+
+  static const String id = 'id';
+  static const String filterInPressure = 'filter_in_pressure';
+  static const String filterOutPressure = 'filter_out_pressure';
+  static const String createdAt = 'created_at';
+  static const String collectorId = 'collector_id';
+  static const String pressureDifference = 'pressure_difference';
+}
