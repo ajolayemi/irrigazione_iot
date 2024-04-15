556b11779b68d36bf97e1b7c6ea67283be344d27
jolayemi <ajolayemi82@gmail.com>
2024-04-15 08:04:23 +0200

    added module to track database column names for sector pump

---
+++ app/lib/src/features/sectors/model/sector_pump_database_keys.dart
@@ -0,0 +1,8 @@
+class SectorPumpDatabaseKeys {
+  const SectorPumpDatabaseKeys._();
+
+  static const String id = 'id';
+  static const String sectorId = 'sector_id';
+  static const String pumpId = 'pump_id';
+  static const String createdAt = 'created_at';
+}
