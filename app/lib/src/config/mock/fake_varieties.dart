2f85fb776080d1c70f04ca62c1242da4d4e7f00f
jolayemi <ajolayemi82@gmail.com>
2024-04-15 15:04:53 +0200

    created a list of fake varieties

---
+++ app/lib/src/config/mock/fake_varieties.dart
@@ -0,0 +1,24 @@
+import 'package:irrigazione_iot/src/features/variety/model/variety.dart';
+
+const kFakeVarieties = <Variety>[
+  Variety(id: '1', name: 'navelina'),
+  Variety(id: '2', name: 'valencia'),
+  Variety(id: '3', name: 'clementine'),
+  Variety(id: '4', name: 'mandarin'),
+  Variety(id: '5', name: 'blood orange'),
+  Variety(id: '6', name: 'tangerine'),
+  Variety(id: '7', name: 'grapefruit'),
+  Variety(id: '8', name: 'lemon'),
+  Variety(id: '9', name: 'lime'),
+  Variety(id: '10', name: 'pomelo'),
+  Variety(id: '11', name: 'kumquat'),
+  Variety(id: '12', name: 'bergamot'),
+  Variety(id: '13', name: 'satsuma'),
+  Variety(id: '14', name: 'tangelo'),
+  Variety(id: '15', name: 'calamondin'),
+  Variety(id: '16', name: 'yuzu'),
+  Variety(id: '17', name: 'orangelo'),
+  Variety(id: '18', name: 'ugli fruit'),
+  Variety(id: '19', name: 'citron'),
+  Variety(id: '20', name: 'papeda'),
+];
