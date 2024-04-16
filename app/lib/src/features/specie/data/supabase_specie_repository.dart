70c94e41e8cd53ff58cb12e9562613c19f92ce76
jolayemi <ajolayemi82@gmail.com>
2024-04-16 21:38:49 +0200

    added a supabase implementation of specie repository

--- app/lib/src/features/specie/data/supabase_specie_repository.dart
+++ app/lib/src/features/specie/data/supabase_specie_repository.dart
@@ -1,14 +1,14 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:irrigazione_iot/src/features/specie/data/specie_repository.dart';
+import 'package:irrigazione_iot/src/features/specie/model/specie.dart';
 
 class SupabaseSpecieRepository implements SpecieRepository {
-
   const SupabaseSpecieRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
-  
+
   @override
-  Future<List<String>> getSpecieNames() {
+  Future<List<Specie>?> getSpecies() {
     // TODO: implement getSpecieNames
     throw UnimplementedError();
   }
