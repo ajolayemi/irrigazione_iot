0dc9b731adfb2dae5cbf5d99680ed580b0c320f8
jolayemi <ajolayemi82@gmail.com>
2024-04-16 19:50:49 +0200

    created a real implementation of collector sector repository

---
+++ app/lib/src/features/collectors/data/supabase_collector_sector_repository.dart
@@ -0,0 +1,38 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:irrigazione_iot/src/features/collectors/data/collector_sector_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/model/collector_sector.dart';
+
+class SupabaseCollectorSectorRepository implements CollectorSectorRepository {
+  const SupabaseCollectorSectorRepository(this._supabaseClient);
+
+  final SupabaseClient _supabaseClient;
+
+  @override
+  Future<CollectorSector?> addCollectorSector(
+      {required CollectorSector collectorSector}) {
+    // TODO: implement addCollectorSector
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> deleteCollectorSector(
+      {required CollectorSector collectorSector}) {
+    // TODO: implement deleteCollectorSector
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<CollectorSector?>> getCollectorSectorsById(
+      {required String collectorId}) {
+    // TODO: implement getCollectorSectorsById
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<CollectorSector?>> watchCollectorSectorsById(
+      {required String collectorId}) {
+    // TODO: implement watchCollectorSectorsById
+    throw UnimplementedError();
+  }
+}
