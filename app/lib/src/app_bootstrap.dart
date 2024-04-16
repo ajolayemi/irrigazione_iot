995b3e8c946c357c5ca9975569c4b0bf7d8573b0
jolayemi <ajolayemi82@gmail.com>
2024-04-16 11:59:21 +0200

    added app bootstrap modules

---
+++ app/lib/src/app_bootstrap.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/app.dart';
+import 'package:irrigazione_iot/src/exceptions/error_logger.dart';
+import 'package:irrigazione_iot/src/providers/shared_prefs_provider.dart';
+import 'package:irrigazione_iot/src/settings/settings_controller.dart';
+import 'package:irrigazione_iot/src/settings/settings_service.dart';
+
+/// Helper class to initialize services and configure the error handlers
+class AppBootstrap {
+  /// Create the root widget that should be passed to [runApp]
+  Future<Widget> createRootWidget(
+      {required ProviderContainer container}) async {
+    // * Register error handlers. For more info, see:
+    // * https://docs.flutter.dev/testing/errors
+    final errorLogger = container.read(errorLoggerProvider);
+    registerErrorHandlers(errorLogger);
+
+    // Load the SharedPreferences instance during initialization
+    await container.read(sharedPreferencesProvider.future);
+
+    return UncontrolledProviderScope(
+      container: container,
+      child: const IotIrrigationApp(),
+    );
+  }
+
+  Future<SettingsController> bootSettingsController() async {
+    // Set up the SettingsController, which will glue user settings to multiple
+    // Flutter Widgets.
+    final settingsController = SettingsController(SettingsService());
+
+    // Load the user's preferred theme while the splash screen is displayed.
+    // This prevents a sudden theme change when the app is first displayed.
+    await settingsController.loadSettings();
+
+    return settingsController;
+  }
+
+  // Register Flutter error handlers
+  void registerErrorHandlers(ErrorLogger errorLogger) {
+    // * Show some error UI if any uncaught exception happens
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+      errorLogger.logError(details.exception, details.stack);
+    };
+    // * Handle errors from the underlying platform/OS
+    PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+      errorLogger.logError(error, stack);
+      return true;
+    };
+    // * Show some error UI when any widget in the app fails to build
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text('An error occurred'),
+        ),
+        body: Center(child: Text(details.toString())),
+      );
+    };
+  }
+}
---
+++ app/lib/src/app_bootstrap_supabase.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/app_bootstrap.dart';
+import 'package:irrigazione_iot/src/exceptions/async_error_logger.dart';
+import 'package:irrigazione_iot/src/settings/settings_controller.dart';
+
+/// Extension methods specific for "fakes" project configuration
+extension AppBootstrapSupabase on AppBootstrap {
+  // TODO: update this documentation
+  /// Creates the top-level [ProviderContainer] by overriding providers with fake
+  /// repositories only. This is useful for testing purposes and for running the
+  /// app with a "fake" backend.
+  ///
+  /// Note: all repositories needed by the app can be accessed via providers.
+  /// Some of these providers throw an [UnimplementedError] by default.
+  ///
+  /// Example:
+  /// ```dart
+  /// @Riverpod(keepAlive: true)
+  /// LocalCartRepository localCartRepository(LocalCartRepositoryRef ref) {
+  ///   throw UnimplementedError();
+  /// }
+  /// ```
+  ///
+  /// As a result, this method does two things:
+  /// - create and configure the repositories as desired
+  /// - override the default implementations with a list of "overrides"
+  Future<ProviderContainer> createSupabaseProviderContainer(
+      {bool addDelay = true}) async {
+    final settingsController = await bootSettingsController();
+    return ProviderContainer(
+      overrides: [
+        // repositories
+
+        // services
+        settingsControllerProvider.overrideWithValue(settingsController),
+      ],
+      observers: [
+        AsyncErrorLogger(),
+      ],
+    );
+  }
+}
