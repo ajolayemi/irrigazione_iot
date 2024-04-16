26d473812a9459396e98d2368cf721058bd7d60f
jolayemi <ajolayemi82@gmail.com>
2024-04-16 19:46:25 +0200

    created a real implementation of collector pressure repository

---
+++ app/lib/src/features/collectors/data/supabase_collector_pressure_repository.dart
@@ -0,0 +1,21 @@
+import 'package:irrigazione_iot/src/features/collectors/data/collector_pressure_repository.dart';
+import 'package:irrigazione_iot/src/features/collectors/model/collector_pressure.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseCollectorPressureRepository extends CollectorPressureRepository {
+
+  SupabaseCollectorPressureRepository(this._supabaseClient);
+  final SupabaseClient _supabaseClient;
+
+  @override
+  Future<CollectorPressure?> getCollectorPressure({required String collectorId}) {
+    // TODO: implement getCollectorPressure
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<CollectorPressure?> watchCollectorPressure({required String collectorId}) {
+    // TODO: implement watchCollectorPressure
+    throw UnimplementedError();
+  }
+}
