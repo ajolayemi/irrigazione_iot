bb3a98b58e5774e60852d705f43f3ba30fccb670
jolayemi <ajolayemi82@gmail.com>
2024-04-18 13:38:07 +0200

    removed hard coded table names

--- app/lib/src/features/company_users/data/supabase_company_users_repository.dart
+++ app/lib/src/features/company_users/data/supabase_company_users_repository.dart
@@ -3,12 +3,16 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:irrigazione_iot/src/config/enums/roles.dart';
 import 'package:irrigazione_iot/src/features/company_users/data/company_users_repository.dart';
 import 'package:irrigazione_iot/src/features/company_users/model/company.dart';
+import 'package:irrigazione_iot/src/features/company_users/model/company_database_keys.dart';
 import 'package:irrigazione_iot/src/features/company_users/model/company_user.dart';
+import 'package:irrigazione_iot/src/features/company_users/model/company_user_database_keys.dart';
 
 class SupabaseCompanyUsersRepository implements CompanyUsersRepository {
   SupabaseCompanyUsersRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
 
+  final _companyTableName = CompanyDatabaseKeys.table;
+  final _companyUserTable = CompanyUserDatabaseKeys.table;
 
   @override
   Future<CompanyUser?> addCompanyUser({required CompanyUser companyUser}) {
@@ -59,8 +63,10 @@ class SupabaseCompanyUsersRepository implements CompanyUsersRepository {
   Stream<List<Company>> watchCompaniesAssociatedWithUser(
       {required String email}) {
     // The email parameter is not used in the query because the companies table
-    // has a RLS policy that filters the rows based on the user's email already in the backendZ
-    final stc = _supabaseClient.from('companies').stream(primaryKey: ['id']);
+    // has a RLS policy that filters the rows based on the user's email already in the backend
+    final stc = _supabaseClient
+        .from(_companyTableName)
+        .stream(primaryKey: [CompanyDatabaseKeys.id]);
     return stc.map((companies) =>
         companies.map((company) => Company.fromJson(company)).toList());
   }
