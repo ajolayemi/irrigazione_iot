5a30ce178d1080dcd576eaef723f92ed5dac7342
jolayemi <ajolayemi82@gmail.com>
2024-04-18 14:53:07 +0200

    added an extension on SupabaseClient

---
+++ app/lib/src/utils/supabase_client_extension.dart
@@ -0,0 +1,17 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+extension SupabaseClientExtensions on SupabaseClient {
+  /// Getter for the current access token
+  String? get accessToken => auth.currentSession?.accessToken;
+
+  /// Invokes a function with the provided [functionName] and [body]
+  Future<FunctionResponse> invokeFunction({
+    required String functionName,
+    required Map<String, dynamic> body,
+  }) =>
+      functions.invoke(
+        functionName,
+        body: body,
+        headers: {'Authorization': 'Bearer $this.accessToken'},
+      );
+}
