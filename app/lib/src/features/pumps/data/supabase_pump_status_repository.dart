bf4e4309bb2ce11fb2f8926f47856e1ba980ae0c
jolayemi <ajolayemi82@gmail.com>
2024-04-18 20:32:51 +0200

    implemented repository method

--- app/lib/src/features/pumps/data/supabase_pump_status_repository.dart
+++ app/lib/src/features/pumps/data/supabase_pump_status_repository.dart
@@ -2,9 +2,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:irrigazione_iot/src/features/pumps/data/pump_status_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump_status.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump_status_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
 
 class SupabasePumpStatusRepository implements PumpStatusRepository {
-
   const SupabasePumpStatusRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
 
@@ -16,7 +18,17 @@ class SupabasePumpStatusRepository implements PumpStatusRepository {
 
   @override
   Stream<bool?> watchPumpStatus(Pump pump) {
-    // TODO: implement watchPumpStatus
-    throw UnimplementedError();
+    final stream = _supabaseClient.pumpStatus
+        .stream(primaryKey: [PumpStatusDatabaseKeys.id])
+        .eq(PumpStatusDatabaseKeys.pumpId, pump.id)
+        .order(PumpStatusDatabaseKeys.createdAt, ascending: false)
+        .limit(1);
+
+    return stream.map((status) {
+      if (status.isEmpty) return false;
+
+      final statusObject = PumpStatus.fromJson(status.first);
+      return statusObject.translatePumpStatusToBoolean(pump);
+    });
   }
 }
