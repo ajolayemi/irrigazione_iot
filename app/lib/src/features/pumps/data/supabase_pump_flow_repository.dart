a6cf09dca6656584fda9de5ea320fa98c2147595
jolayemi <ajolayemi82@gmail.com>
2024-04-18 19:30:33 +0200

    implemented repository method

--- app/lib/src/features/pumps/data/supabase_pump_flow_repository.dart
+++ app/lib/src/features/pumps/data/supabase_pump_flow_repository.dart
@@ -1,15 +1,25 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:irrigazione_iot/src/features/pumps/data/pump_flow_repository.dart';
-import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump_flow.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump_flow_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
 
 class SupabasePumpFlowRepository implements PumpFlowRepository {
   const SupabasePumpFlowRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
 
   @override
-  Stream<int> watchTotalLitresDispensed(Pump pump) {
-    // TODO: implement watchTotalLitresDispensed
-    throw UnimplementedError();
+  Stream<int> watchTotalLitresDispensed(String pumpId) {
+    final stream = _supabaseClient.pumpFlow
+        .stream(primaryKey: [PumpFlowDatabaseKeys.id]).eq(
+            PumpFlowDatabaseKeys.pumpId, pumpId);
+
+    return stream.map((flows) {
+      return flows.fold<int>(0, (total, flow) {
+        final pumpFlow = PumpFlow.fromJson(flow);
+        return total + pumpFlow.flow.toInt();
+      });
+    });
   }
 }
