c52f6788a2429e36d6c1af69216839abd35734a7
jolayemi <ajolayemi82@gmail.com>
2024-04-18 16:39:25 +0200

    added getters for tables on supabase client extension

--- app/lib/src/utils/supabase_client_extension.dart
+++ app/lib/src/utils/supabase_client_extension.dart
@@ -1,6 +1,12 @@
+import 'package:irrigazione_iot/src/features/company_users/model/company_database_keys.dart';
+import 'package:irrigazione_iot/src/features/company_users/model/company_user_database_keys.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 extension SupabaseClientExtensions on SupabaseClient {
+  SupabaseQueryBuilder get companies => from(CompanyDatabaseKeys.table);
+
+  SupabaseQueryBuilder get companyUsers => from(CompanyUserDatabaseKeys.table);
+
   /// Getter for the current access token
   String? get accessToken => auth.currentSession?.accessToken;
 
