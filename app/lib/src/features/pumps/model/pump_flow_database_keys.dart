0ae0785ab43ecf2336db38fb9f40aff71dad7337
jolayemi <ajolayemi82@gmail.com>
2024-04-18 19:29:36 +0200

    updated pump flow database keys

--- app/lib/src/features/pumps/model/pump_flow_database_keys.dart
+++ app/lib/src/features/pumps/model/pump_flow_database_keys.dart
@@ -1,6 +1,8 @@
 class PumpFlowDatabaseKeys {
   const PumpFlowDatabaseKeys._();
 
+  static const String table = 'pump_flows';
+
   static const String id = 'id';
   static const String flow = 'flow';
   static const String pumpId = 'pump_id';
