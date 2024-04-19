7b7636fa3206e06a2958d99bec2fa711939f03c8
jolayemi <ajolayemi82@gmail.com>
2024-04-19 09:52:16 +0200

    added table name to sector status db keys

--- app/lib/src/features/sectors/model/sector_status_database_keys.dart
+++ app/lib/src/features/sectors/model/sector_status_database_keys.dart
@@ -1,6 +1,7 @@
 class SectorStatusDatabaseKeys {
   const SectorStatusDatabaseKeys._();
 
+  static const String table = 'sector_statuses';
   static const String id = 'id';
   static const String sectorId = 'sector_id';
   static const String status = 'status';
