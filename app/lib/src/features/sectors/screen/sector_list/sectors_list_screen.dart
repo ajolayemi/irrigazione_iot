0a9410ac522bd131e0253494ee12e55e662d1666
jolayemi <ajolayemi82@gmail.com>
2024-04-19 17:06:59 +0200

    refactored sector list screen

--- app/lib/src/features/sectors/screen/sector_list/sectors_list_screen.dart
+++ app/lib/src/features/sectors/screen/sector_list/sectors_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:irrigazione_iot/src/config/routes/routes_enums.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_pump_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
@@ -9,17 +10,17 @@ import 'package:irrigazione_iot/src/features/sectors/screen/sector_list/dismiss_
 import 'package:irrigazione_iot/src/features/sectors/screen/sector_list/sector_list_tile.dart';
 import 'package:irrigazione_iot/src/features/sectors/screen/sector_list/sectors_list_tile_skeleton.dart';
 import 'package:irrigazione_iot/src/features/sectors/screen/sector_switch_controller.dart';
-import 'package:irrigazione_iot/src/utils/async_value_ui.dart';
-import 'package:irrigazione_iot/src/utils/extensions.dart';
 import 'package:irrigazione_iot/src/shared/widgets/app_sliver_bar.dart';
 import 'package:irrigazione_iot/src/shared/widgets/async_value_widget.dart';
 import 'package:irrigazione_iot/src/shared/widgets/common_add_icon_button.dart';
+import 'package:irrigazione_iot/src/utils/async_value_ui.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class SectorsListScreen extends ConsumerWidget {
   const SectorsListScreen({super.key});
 
   void _onAddSectorPressed(WidgetRef ref, BuildContext context) {
-    ref.read(selectedPumpsIdProvider.notifier).state = [];
+    ref.read(selectPumpRadioButtonProvider.notifier).state = null;
     context.pushNamed(AppRoute.addSector.name);
   }
 
