12b331774781add65acab3d4bff7a1470156aaef
jolayemi <ajolayemi82@gmail.com>
2024-04-19 09:52:04 +0200

    implemented repository method

--- app/lib/src/features/sectors/data/supabase_sector_status_repository.dart
+++ app/lib/src/features/sectors/data/supabase_sector_status_repository.dart
@@ -2,9 +2,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:irrigazione_iot/src/features/sectors/data/sector_status_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector.dart';
+import 'package:irrigazione_iot/src/features/sectors/model/sector_status.dart';
+import 'package:irrigazione_iot/src/features/sectors/model/sector_status_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
 
 class SupabaseSectorStatusRepository implements SectorStatusRepository {
-
   const SupabaseSectorStatusRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
 
@@ -14,10 +16,18 @@ class SupabaseSectorStatusRepository implements SectorStatusRepository {
     throw UnimplementedError();
   }
 
-
   @override
   Stream<bool?> watchSectorStatus(Sector sector) {
-    // TODO: implement watchSectorStatus
-    throw UnimplementedError();
+    final stream = _supabaseClient.sectorStatus
+        .stream(primaryKey: [SectorStatusDatabaseKeys.id])
+        .eq(SectorStatusDatabaseKeys.sectorId, sector.id)
+        .order(SectorStatusDatabaseKeys.createdAt, ascending: false)
+        .limit(1);
+
+    return stream.map((statuses) {
+      if (statuses.isEmpty) return false;
+      final status = SectorStatus.fromJson(statuses.first);
+      return status.translateSectorStatusToBoolean(sector);
+    });
   }
 }
