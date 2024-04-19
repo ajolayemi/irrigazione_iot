92e42ecb15dbd48debd2e04b31bc94a8211bbdab
jolayemi <ajolayemi82@gmail.com>
2024-04-19 10:47:56 +0200

    implemented supabase sector repository methods

--- app/lib/src/features/sectors/data/supabase_sector_repository.dart
+++ app/lib/src/features/sectors/data/supabase_sector_repository.dart
@@ -1,68 +1,112 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:irrigazione_iot/src/features/sectors/model/sector_database_keys.dart';
+import 'package:irrigazione_iot/src/shared/models/db_cud_bodies.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
 
 class SupabaseSectorRepository implements SectorRepository {
   const SupabaseSectorRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
 
-  @override
-  Future<Sector?> addSector(Sector sector, String companyId) {
-    // TODO: implement addSector
-    throw UnimplementedError();
+  List<Sector?> _sectorsFromJsonList(List<Map<String, dynamic>> data) {
+    return data.map((sector) => Sector.fromJson(sector)).toList();
+  }
+
+  Sector? _sectorFromJsonSingle(List<Map<String, dynamic>> data) {
+    return data.isEmpty ? null : Sector.fromJson(data.first);
   }
 
   @override
-  Future<bool> deleteSector(String sectorID) {
-    // TODO: implement deleteSector
-    throw UnimplementedError();
+  Future<Sector?> createSector(Sector sector) async {
+    // set created_at and updated_at fields
+    final data = sector
+        .copyWith(
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+        )
+        .toJson();
+    final res = await _supabaseClient.invokeFunction(
+      functionName: 'insert-sector',
+      body: InsertBody(data: data).toJson(),
+    );
+    return res.toObject<Sector>(Sector.fromJson);
   }
 
   @override
-  Future<Sector?> getSector(String sectorID) {
-    // TODO: implement getSector
-    throw UnimplementedError();
+  Future<Sector?> updateSector(Sector sector) async {
+    // add the updated_at field
+    final data = sector.copyWith(updatedAt: DateTime.now()).toJson();
+    final res = await _supabaseClient.invokeFunction(
+      functionName: 'update-sector',
+      body: UpdateBody(
+        id: sector.id,
+        data: data,
+      ).toJson(),
+    );
+    return res.toObject<Sector>(Sector.fromJson);
   }
 
   @override
-  Future<List<Sector?>> getSectors(String companyId) {
-    // TODO: implement getSectors
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Sector?> updateSector(Sector sector, String companyId) {
-    // TODO: implement updateSector
-    throw UnimplementedError();
+  Future<bool> deleteSector(String sectorID) async {
+    final res = await _supabaseClient.invokeFunction(
+      functionName: 'delete-sector',
+      body: DeleteBody(ids: [sectorID]).toJson(),
+    );
+    return res.onDelete;
   }
 
   @override
   Stream<List<String?>> watchCompanyUsedSectorNames(String companyId) {
-    // TODO: implement watchCompanyUsedSectorNames
-    throw UnimplementedError();
+    return watchSectors(companyId).map(
+      (sectors) => sectors.map((sector) => sector?.name).toList(),
+    );
   }
 
   @override
   Stream<List<String?>> watchCompanyUsedSectorOffCommands(String companyId) {
-    // TODO: implement watchCompanyUsedSectorOffCommands
-    throw UnimplementedError();
+    return watchSectors(companyId).map(
+      (sectors) => sectors.map((sector) => sector?.turnOffCommand).toList(),
+    );
   }
 
   @override
   Stream<List<String?>> watchCompanyUsedSectorOnCommands(String companyId) {
-    // TODO: implement watchCompanyUsedSectorOnCommands
-    throw UnimplementedError();
+    return watchSectors(companyId).map(
+      (sectors) => sectors.map((sector) => sector?.turnOnCommand).toList(),
+    );
   }
 
   @override
   Stream<Sector?> watchSector(String sectorID) {
-    // TODO: implement watchSector
-    throw UnimplementedError();
+    final stream = _supabaseClient.sectors.stream(
+      primaryKey: [SectorDatabaseKeys.id],
+    ).eq(
+      SectorDatabaseKeys.id,
+      sectorID,
+    );
+
+    return stream.map(_sectorFromJsonSingle);
   }
 
   @override
   Stream<List<Sector?>> watchSectors(String companyId) {
-    // TODO: implement watchSectors
-    throw UnimplementedError();
+    final stream = _supabaseClient.sectors.stream(
+      primaryKey: [SectorDatabaseKeys.id],
+    ).eq(
+      SectorDatabaseKeys.companyId,
+      companyId,
+    );
+
+    return stream.map(_sectorsFromJsonList);
+  }
+
+  @override
+  Stream<List<String?>> watchSectorUsedMqttMsgNames() {
+    return _supabaseClient.sectors
+        .stream(primaryKey: [SectorDatabaseKeys.id]).map((sectors) => sectors
+            .map((sector) => Sector.fromJson(sector).mqttMsgName)
+            .toList());
   }
 }
