36a8dff4988cd2f7ee951b352a7dd9ae208bfcd7
jolayemi <ajolayemi82@gmail.com>
2024-04-20 14:17:47 +0200

    implemented repository methods

--- app/lib/src/features/variety/data/supabase_variety_repository.dart
+++ app/lib/src/features/variety/data/supabase_variety_repository.dart
@@ -2,14 +2,18 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:irrigazione_iot/src/features/variety/data/variety_repository.dart';
 import 'package:irrigazione_iot/src/features/variety/model/variety.dart';
+import 'package:irrigazione_iot/src/features/variety/model/variety_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
 
 class SupabaseVarietyRepository implements VarietyRepository {
   const SupabaseVarietyRepository(this._supabaseClient);
   final SupabaseClient _supabaseClient;
 
-  @override
-  Future<List<Variety>?> getVarieties() {
-    // TODO: implement getVarieties
-    throw UnimplementedError();
+  List<Variety> _varietiesFromJson(List<Map<String, dynamic>> json) {
+    return json.map((e) => Variety.fromJson(e)).toList();
   }
+
+  @override
+  Stream<List<Variety>?> watchVarieties() => _supabaseClient.varieties
+      .stream(primaryKey: [VarietyDatabaseKeys.id]).map(_varietiesFromJson);
 }
