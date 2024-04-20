7865baecc9f4bd416a4950d2735c521d6fd1c4c4
jolayemi <ajolayemi82@gmail.com>
2024-04-20 15:33:25 +0200

    refactored sector details screen

--- app/lib/src/features/sectors/screen/sector_details/sector_details_screen_content.dart
+++ app/lib/src/features/sectors/screen/sector_details/sector_details_screen_content.dart
@@ -1,82 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:irrigazione_iot/src/config/routes/routes_enums.dart';
+
 import 'package:irrigazione_iot/src/features/pumps/data/pump_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_pressure_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_pump_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector_status.dart';
-import 'package:irrigazione_iot/src/utils/date_formatter.dart';
-import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/features/specie/data/specie_repository.dart';
+import 'package:irrigazione_iot/src/features/variety/data/variety_repository.dart';
 import 'package:irrigazione_iot/src/shared/widgets/details_tile_widget.dart';
 import 'package:irrigazione_iot/src/shared/widgets/responsive_details_card.dart';
+import 'package:irrigazione_iot/src/utils/date_formatter.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class SectorDetailsScreenContents extends ConsumerWidget {
-  const SectorDetailsScreenContents(
-      {super.key, required this.sector, this.sectorStatus});
+  const SectorDetailsScreenContents({
+    super.key,
+    required this.sector,
+    this.sectorStatus,
+  });
 
   final Sector sector;
   final SectorStatus? sectorStatus;
 
-  void _onTapConnectedPumpsTile(BuildContext context, String sectorId) {
-    context.pushNamed(
-      AppRoute.sectorConnectedPumps.name,
-      pathParameters: {'sectorId': sectorId},
-    );
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final loc = context.loc;
     return SliverList(
       delegate: SliverChildListDelegate.fixed(
         [
-          ResponsiveDetailsCard(child: Consumer(builder: (context, ref, child) {
-            final lastIrrigated = ref
-                .watch(sectorLastPressureStreamProvider(sector.id))
-                .valueOrNull;
-            final dateFormatter = ref.watch(dateFormatWithTimeProvider);
-            return DetailTileWidget(
-              title: context.loc.sectorLastIrrigationForTile,
-              subtitle: lastIrrigated == null
-                  ? context.loc.sectorLastIrrigationEmpty
-                  : dateFormatter.format(lastIrrigated),
-            );
-          })),
           ResponsiveDetailsCard(
             child: Consumer(
               builder: (context, ref, child) {
-                final connectedPumps =
-                    ref.watch(sectorPumpStreamProvider(sector.id)).valueOrNull;
-                if (connectedPumps == null) return const DetailTileWidget();
-                final pump = ref
-                    .watch(pumpStreamProvider(connectedPumps.pumpId))
+                final lastIrrigated = ref
+                    .watch(sectorLastPressureStreamProvider(sector.id))
                     .valueOrNull;
-
+                final dateFormatter = ref.watch(dateFormatWithTimeProvider);
                 return DetailTileWidget(
-                  onTap: () => _onTapConnectedPumpsTile(
-                    context,
-                    sector.id,
-                  ),
-                  title: context.loc.sectorConnectedPumps,
-                  subtitle: pump?.name,
+                  title: context.loc.sectorLastIrrigationForTile,
+                  subtitle: lastIrrigated == null
+                      ? context.loc.sectorLastIrrigationEmpty
+                      : dateFormatter.format(lastIrrigated),
                 );
               },
             ),
           ),
-
-          // TODO variety and specie name should be displayed instead of id
-          ResponsiveDetailsCard(
-            child: DetailTileWidget(
-              title: context.loc.sectorSpecie,
-              subtitle: sector.specieId,
-            ),
+          Consumer(
+            builder: (context, ref, child) {
+              final sectorPump =
+                  ref.watch(sectorPumpStreamProvider(sector.id)).valueOrNull;
+              final pump = ref
+                  .watch(pumpStreamProvider(sectorPump?.pumpId ?? '0'))
+                  .valueOrNull;
+              return ResponsiveDetailsCard(
+                child: DetailTileWidget(
+                  title: context.loc.sectorConnectedPumps,
+                  subtitle: pump?.name ?? loc.notAvailable,
+                ),
+              );
+            },
           ),
-          ResponsiveDetailsCard(
-            child: DetailTileWidget(
-              title: context.loc.sectorVariety,
-              subtitle: sector.varietyId,
-            ),
+          Consumer(
+            builder: (context, ref, child) {
+              final specieName = ref
+                  .watch(specieStreamProvider(sector.specieId))
+                  .valueOrNull
+                  ?.name;
+              return ResponsiveDetailsCard(
+                child: DetailTileWidget(
+                  title: context.loc.sectorSpecie,
+                  subtitle: specieName ?? loc.notAvailable,
+                ),
+              );
+            },
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              final varietyName = ref
+                  .watch(varietyStreamProvider(sector.varietyId))
+                  .valueOrNull
+                  ?.name;
+              return ResponsiveDetailsCard(
+                child: DetailTileWidget(
+                  title: context.loc.sectorVariety,
+                  subtitle: varietyName ?? loc.notAvailable,
+                ),
+              );
+            },
           ),
           ResponsiveDetailsCard(
             child: DetailTileWidget(
