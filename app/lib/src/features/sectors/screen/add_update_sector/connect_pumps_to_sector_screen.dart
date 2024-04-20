53208e19cb8ab8cd3c23eeedff0b225259dd7061
jolayemi <ajolayemi82@gmail.com>
2024-04-20 13:13:06 +0200

    refactored full widget

--- app/lib/src/features/sectors/screen/add_update_sector/connect_pumps_to_sector_screen.dart
+++ app/lib/src/features/sectors/screen/add_update_sector/connect_pumps_to_sector_screen.dart
@@ -2,20 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:irrigazione_iot/src/config/enums/button_types.dart';
-import 'package:irrigazione_iot/src/config/enums/roles.dart';
 import 'package:irrigazione_iot/src/config/routes/routes_enums.dart';
-import 'package:irrigazione_iot/src/constants/app_sizes.dart';
-import 'package:irrigazione_iot/src/features/company_users/data/company_users_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/screen/empty_pump_widget.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_pump_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/screen/add_update_sector/connect_pumps_to_sector_controller.dart';
 import 'package:irrigazione_iot/src/shared/models/radio_button_item.dart';
-import 'package:irrigazione_iot/src/shared/widgets/app_bar_icon_buttons.dart';
-import 'package:irrigazione_iot/src/shared/widgets/app_cta_button.dart';
-import 'package:irrigazione_iot/src/shared/widgets/app_sliver_bar.dart';
 import 'package:irrigazione_iot/src/shared/widgets/async_value_widget.dart';
-import 'package:irrigazione_iot/src/shared/widgets/padded_safe_area.dart';
+import 'package:irrigazione_iot/src/shared/widgets/common_add_icon_button.dart';
+import 'package:irrigazione_iot/src/shared/widgets/custom_sliver_connect_something_to.dart';
 import 'package:irrigazione_iot/src/shared/widgets/responsive_radio_list_tile.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 
@@ -28,83 +22,59 @@ class ConnectPumpToSector extends ConsumerWidget {
   final String? pumpIdAlreadyConnected;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final canEdit = ref.watch(companyUserRoleProvider).valueOrNull?.canEdit;
     final availablePumps = ref.watch(availablePumpsFutureProvider(
       alreadyConnectedPumpId: pumpIdAlreadyConnected,
     ));
     final selectedPumpId = ref.watch(selectPumpRadioButtonProvider);
     final loc = context.loc;
-    return Scaffold(
-      body: PaddedSafeArea(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.p12),
-        child: Column(
-          children: [
-            Expanded(
-              child: CustomScrollView(
-                slivers: [
-                  AppSliverBar(
-                    title: loc.connectPumpsToSectorPageTile,
-                    actions: [
-                      AppBarIconButton(
-                        onPressed: () =>
-                            context.pushNamed(AppRoute.addPump.name),
-                        icon: Icons.add,
-                        isVisibile: canEdit,
-                      ),
-                    ],
-                  ),
-                  AsyncValueSliverWidget(
-                    value: availablePumps,
-                    data: (pumps) {
-                      if (pumps == null || pumps.isEmpty) {
-                        return const EmptyPumpWidget();
-                      }
 
-                      // it should be save to assume that the pumps are not null here
-                      return SliverList(
-                        delegate: SliverChildBuilderDelegate(
-                          (context, index) {
-                            final pump = pumps[index];
-                            return ResponsiveRadioListTile(
-                              title: Text(pump.name),
-                              value: RadioButtonItem(
-                                value: pump.id,
-                                label: pump.name,
-                              ),
-                              groupValue: RadioButtonItem(
-                                value: selectedPumpId?.value ?? '',
-                                label: selectedPumpId?.label ?? '',
-                              ),
-                              onChanged: (val) => ref
-                                  .read(connectPumpsToSectorControllerProvider
-                                      .notifier)
-                                  .handleSelection(val),
-                            );
-                          },
-                          childCount: pumps.length,
-                        ),
-                      );
-                    },
-                    loading: () => const SliverFillRemaining(
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+    return CustomSliverConnectSomethingTo(
+      title: loc.connectPumpsToSectorPageTile,
+      actions: [
+        CommonAddIconButton(
+          onPressed: () => context.pushNamed(
+            AppRoute.addPump.name,
+          ),
+        ),
+      ],
+      child: AsyncValueSliverWidget(
+        value: availablePumps,
+        data: (pumps) {
+          if (pumps == null || pumps.isEmpty) {
+            return const EmptyPumpWidget();
+          }
 
-            // Confirm button
-            SliverCTAButton(
-              text: loc.genericConfirmButtonLabel,
-              buttonType: ButtonType.primary,
-              onPressed: () => Navigator.of(context).pop(selectedPumpId),
+          // it should be save to assume that the pumps are not null here
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final pump = pumps[index];
+                return ResponsiveRadioListTile(
+                  title: Text(pump.name),
+                  value: RadioButtonItem(
+                    value: pump.id,
+                    label: pump.name,
+                  ),
+                  groupValue: RadioButtonItem(
+                    value: selectedPumpId?.value ?? '',
+                    label: selectedPumpId?.label ?? '',
+                  ),
+                  onChanged: (val) => ref
+                      .read(connectPumpsToSectorControllerProvider.notifier)
+                      .handleSelection(val),
+                );
+              },
+              childCount: pumps.length,
             ),
-            gapH32,
-          ],
+          );
+        },
+        loading: () => const SliverFillRemaining(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
       ),
+      onCTAPressed: () => context.popNavigator(selectedPumpId),
     );
   }
 }
