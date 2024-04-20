c6ae2ebf96d99d5491f3a9186770e926fb8aca7f
jolayemi <ajolayemi82@gmail.com>
2024-04-20 14:16:44 +0200

    added table names in database keys

--- app/lib/src/features/specie/model/specie_database_keys.dart
+++ app/lib/src/features/specie/model/specie_database_keys.dart
@@ -1,6 +1,7 @@
 class SpecieDatabaseKeys {
   const SpecieDatabaseKeys._();
 
+  static const String table = 'species';
   static const String id = 'id';
   static const String name = 'name';
   static const String createdAt = 'created_at';
