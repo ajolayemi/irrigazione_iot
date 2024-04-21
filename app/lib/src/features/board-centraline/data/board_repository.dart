3ea3fda4d78a70baec9a51424d96004a1cb2c964
jolayemi <ajolayemi82@gmail.com>
2024-04-21 13:24:04 +0200

    removed unused import and sorted imports as well

--- app/lib/src/features/board-centraline/data/board_repository.dart
+++ app/lib/src/features/board-centraline/data/board_repository.dart
@@ -1,15 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:irrigazione_iot/src/features/board-centraline/data/supabase_board_repository.dart';
-import 'package:irrigazione_iot/src/shared/models/radio_button_item.dart';
-import 'package:irrigazione_iot/src/shared/providers/supabase_client_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'package:irrigazione_iot/src/features/board-centraline/data/supabase_board_repository.dart';
 import 'package:irrigazione_iot/src/features/board-centraline/models/board.dart';
-import 'package:irrigazione_iot/src/features/collectors/data/collector_repository.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector.dart';
 import 'package:irrigazione_iot/src/features/company_users/data/selected_company_repository.dart';
+import 'package:irrigazione_iot/src/shared/models/radio_button_item.dart';
+import 'package:irrigazione_iot/src/shared/providers/supabase_client_provider.dart';
 
 part 'board_repository.g.dart';
 
