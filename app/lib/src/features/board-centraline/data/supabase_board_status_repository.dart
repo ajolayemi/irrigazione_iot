8b49d7d974f0267b7755150f5e5533cfec9778da
jolayemi <ajolayemi82@gmail.com>
2024-04-21 12:40:31 +0200

    implemented base repository method

--- app/lib/src/features/board-centraline/data/supabase_board_status_repository.dart
+++ app/lib/src/features/board-centraline/data/supabase_board_status_repository.dart
@@ -1,5 +1,7 @@
 import 'package:irrigazione_iot/src/features/board-centraline/data/board_status_repository.dart';
 import 'package:irrigazione_iot/src/features/board-centraline/models/board_status.dart';
+import 'package:irrigazione_iot/src/features/board-centraline/models/board_status_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseBoardStatusRepository implements BoardStatusRepository {
@@ -7,15 +9,17 @@ class SupabaseBoardStatusRepository implements BoardStatusRepository {
   final SupabaseClient _supabaseClient;
 
   @override
-  Future<BoardStatus?> getBoardStatus(String boardID) {
-    // TODO: implement getBoardStatus
-    throw UnimplementedError();
-  }
-
-  @override
   Stream<BoardStatus?> watchBoardStatus(String boardID) {
-    // TODO: implement watchBoardStatus
-    throw UnimplementedError();
+    final stream = _supabaseClient.boardStatus
+        .stream(primaryKey: [BoardStatusDatabaseKeys.id])
+        .eq(BoardStatusDatabaseKeys.boardId, boardID)
+        .order(BoardStatusDatabaseKeys.createdAt, ascending: false)
+        .limit(1);
+
+    return stream.map((statuses) {
+      if (statuses.isEmpty) return null;
+      return BoardStatus.fromJson(statuses.first);
+    });
   }
 
   
