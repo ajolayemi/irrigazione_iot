308320683061b0482f784fcff6ca2d6b80092765
jolayemi <ajolayemi82@gmail.com>
2024-04-21 10:37:19 +0200

    did a complete refactor of screen to select irrigation source for a sector

--- app/lib/src/features/sectors/screen/add_update_sector/select_an_irrigation_source.dart
+++ app/lib/src/features/sectors/screen/add_update_sector/select_an_irrigation_source.dart
@@ -1,37 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:irrigazione_iot/src/config/enums/irrigation_enums.dart';
-import 'package:irrigazione_iot/src/features/sectors/widgets/responsive_select_screens_tile.dart';
+import 'package:irrigazione_iot/src/shared/models/radio_button_item.dart';
+import 'package:irrigazione_iot/src/shared/widgets/custom_sliver_connect_something_to.dart';
+import 'package:irrigazione_iot/src/shared/widgets/responsive_radio_list_tile.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
-import 'package:irrigazione_iot/src/shared/widgets/app_sliver_bar.dart';
-import 'package:irrigazione_iot/src/shared/widgets/padded_safe_area.dart';
 
-class SelectAnIrrigationSource extends StatelessWidget {
-  const SelectAnIrrigationSource({super.key});
+class SelectAnIrrigationSource extends StatefulWidget {
+  const SelectAnIrrigationSource({
+    super.key,
+    this.selectedIrrigationSource,
+  });
+
+  final String? selectedIrrigationSource;
+
+  @override
+  State<SelectAnIrrigationSource> createState() =>
+      _SelectAnIrrigationSourceState();
+}
+
+class _SelectAnIrrigationSourceState extends State<SelectAnIrrigationSource> {
+  final irrigationSources = IrrigationSource.values;
+  late RadioButtonItem _selectedIrrigationSource;
+
+  @override
+  void initState() {
+    _selectedIrrigationSource = RadioButtonItem(
+      value: widget.selectedIrrigationSource ?? '',
+      label: widget.selectedIrrigationSource ?? '',
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PaddedSafeArea(
-        child: CustomScrollView(
-          slivers: [
-            AppSliverBar(
-              title: context.loc.selectAnOption,
-              expandedHeight: 120.0,
-            ),
-            SliverList(
-                delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final irrigationSource = IrrigationSource.values[index];
-                return ResponsiveSelectScreensTile(
-                  title: irrigationSource.uiName,
-                  onTap: () {
-                    Navigator.of(context).pop(irrigationSource.uiName);
-                  },
-                );
-              },
-              childCount: IrrigationSource.values.length,
-            ))
-          ],
+    final loc = context.loc;
+    return CustomSliverConnectSomethingTo(
+      title: loc.selectAnOption,
+      onCTAPressed: () =>
+          context.popNavigator<RadioButtonItem>(_selectedIrrigationSource),
+      child: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final source = irrigationSources[index];
+            return ResponsiveRadioListTile(
+              value:
+                  RadioButtonItem(value: source.uiName, label: source.uiName),
+              groupValue: _selectedIrrigationSource,
+              title: source.uiName,
+              onChanged: (val) => setState(
+                () {
+                  _selectedIrrigationSource =
+                      _selectedIrrigationSource.copyWith(
+                    value: val?.value,
+                    label: val?.label,
+                  );
+                },
+              ),
+            );
+          },
+          childCount: irrigationSources.length,
         ),
       ),
     );
