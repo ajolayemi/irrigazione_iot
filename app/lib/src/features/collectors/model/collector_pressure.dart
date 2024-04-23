669704f0d9e3ddfda29fb0ab9bdae037fd1f3b1f
jolayemi <ajolayemi82@gmail.com>
2024-04-23 10:15:37 +0200

    added custom converter

--- app/lib/src/features/collectors/model/collector_pressure.dart
+++ app/lib/src/features/collectors/model/collector_pressure.dart
@@ -1,19 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector_pressure_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/int_converter.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'collector_pressure.g.dart';
 
 @JsonSerializable()
 class CollectorPressure extends Equatable {
-  const CollectorPressure({
-    required this.id,
-    required this.filterInPressure,
-    required this.filterOutPressure,
-    required this.collectorId,
-    this.createdAt
-  }) : pressureDifference = filterInPressure - filterOutPressure;
+  const CollectorPressure(
+      {required this.id,
+      required this.filterInPressure,
+      required this.filterOutPressure,
+      required this.collectorId,
+      this.createdAt})
+      : pressureDifference = filterInPressure - filterOutPressure;
 
   const CollectorPressure.empty()
       : id = '',
@@ -23,16 +24,23 @@ class CollectorPressure extends Equatable {
         collectorId = '',
         pressureDifference = 0;
 
-  @JsonKey(name: CollectorPressureDatabaseKeys.id)
+  @JsonKey(name: CollectorPressureDatabaseKeys.id, includeToJson: false)
+  @IntConverter()
   final String id;
+
   @JsonKey(name: CollectorPressureDatabaseKeys.filterInPressure)
   final double filterInPressure;
+
   @JsonKey(name: CollectorPressureDatabaseKeys.filterOutPressure)
   final double filterOutPressure;
+
   @JsonKey(name: CollectorPressureDatabaseKeys.createdAt)
   final DateTime? createdAt;
+
   @JsonKey(name: CollectorPressureDatabaseKeys.collectorId)
+  @IntConverter()
   final String collectorId;
+
   @JsonKey(name: CollectorPressureDatabaseKeys.pressureDifference)
   final double pressureDifference;
 
