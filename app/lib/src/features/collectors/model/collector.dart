8f3abdaba9c91639f65e1993a1307c6cef249656
jolayemi <ajolayemi82@gmail.com>
2024-04-23 09:31:34 +0200

    added custom converter

--- app/lib/src/features/collectors/model/collector.dart
+++ app/lib/src/features/collectors/model/collector.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:irrigazione_iot/src/features/collectors/model/collector_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/int_converter.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'collector.g.dart';
@@ -17,7 +18,7 @@ class Collector extends Equatable {
     this.updatedAt,
   });
 
- const Collector.empty()
+  const Collector.empty()
       : id = '',
         name = '',
         connectedFilterName = '',
@@ -26,18 +27,26 @@ class Collector extends Equatable {
         companyId = '',
         mqttMsgName = '';
 
-  @JsonKey(name: CollectorDatabaseKeys.id)
+  @JsonKey(name: CollectorDatabaseKeys.id, includeToJson: false)
+  @IntConverter()
   final String id;
+
   @JsonKey(name: CollectorDatabaseKeys.name)
   final String name;
+
   @JsonKey(name: CollectorDatabaseKeys.connectedFilterName)
   final String connectedFilterName;
+
   @JsonKey(name: CollectorDatabaseKeys.createdAt)
   final DateTime? createdAt;
+
   @JsonKey(name: CollectorDatabaseKeys.updatedAt)
   final DateTime? updatedAt;
+
   @JsonKey(name: CollectorDatabaseKeys.companyId)
+  @IntConverter()
   final String companyId;
+
   @JsonKey(name: CollectorDatabaseKeys.mqttMsgName)
   final String mqttMsgName;
 
