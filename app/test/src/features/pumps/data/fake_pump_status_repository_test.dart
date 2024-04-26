ba0cd679d5f861f824a967dceb5faa38e96b8f7d
jolayemi <ajolayemi82@gmail.com>
2024-04-26 08:19:15 +0200

    updated test

--- app/test/src/features/pumps/data/fake_pump_status_repository_test.dart
+++ app/test/src/features/pumps/data/fake_pump_status_repository_test.dart
@@ -3,18 +3,14 @@ import 'package:irrigazione_iot/src/config/mock/fake_pump_status.dart';
 import 'package:irrigazione_iot/src/config/mock/fake_pumps.dart';
 import 'package:irrigazione_iot/src/features/pumps/data/fake_pump_status_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
-import 'package:irrigazione_iot/src/features/pumps/model/pump_status.dart';
 
 void main() {
   final testPump = kFakePumps[0];
-  final expectedPumpStatus = kFakePumpStatus[0].translatePumpStatusToBoolean(
-    testPump,
-  );
+  final expectedPumpStatus = kFakePumpStatus[0].statusBoolean;
 
   FakePumpStatusRepository makePumpStatusRepository() =>
       FakePumpStatusRepository(addDelay: false);
   group('FakePumpStatusRepository', () {
-
     test('watchPumpStatus(1) returns a valid pumpStatus', () {
       final pumpStatusRepository = makePumpStatusRepository();
       addTearDown(pumpStatusRepository.dispose);
