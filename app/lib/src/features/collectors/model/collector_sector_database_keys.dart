e995d79a381f500f45f70214f1a828b2ced98313
jolayemi <ajolayemi82@gmail.com>
2024-04-26 08:22:56 +0200

    added new collector sector db key

--- app/lib/src/features/collectors/model/collector_sector_database_keys.dart
+++ app/lib/src/features/collectors/model/collector_sector_database_keys.dart
@@ -1,6 +1,7 @@
 class CollectorSectorDatabaseKeys {
   const CollectorSectorDatabaseKeys._();
 
+  static const String table = 'collector_sectors';
   static const String id = 'id';
   static const String collectorId = 'collector_id';
   static const String sectorId = 'sector_id';
