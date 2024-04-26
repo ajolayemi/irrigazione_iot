dbd8ffca69d032cb9e39a3726a7b541b0501d3d7
jolayemi <ajolayemi82@gmail.com>
2024-04-26 07:35:23 +0200

    updated the returned cols by edge funcs

--- supabase/functions/get-pump-by-mqtt-msg-name/index.ts
+++ supabase/functions/get-pump-by-mqtt-msg-name/index.ts
@@ -1,8 +1,9 @@
 import {commonGetByMqttMsgName} from "../_utils/commonDatabaseOperations.ts";
+import { ColumnNames } from "../_utils/tableConstants.ts";
 import {TableNames} from "../_utils/tableConstants.ts";
 
 console.log(`Function "get-pump-by-mqtt-msg-name" up and running!`);
 
 Deno.serve(async (req) => {
-  return await commonGetByMqttMsgName(req, TableNames.pumps, "id, company_id");
+  return await commonGetByMqttMsgName(req, TableNames.pumps, ColumnNames.columns_to_get_for_mqtt);
 });
