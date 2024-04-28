c343d502da001306ecee6cf57557fbee086346bd
jolayemi <ajolayemi82@gmail.com>
2024-04-28 13:44:19 +0200

    updated collector database keys

--- app/lib/src/features/collectors/model/collector_database_keys.dart
+++ app/lib/src/features/collectors/model/collector_database_keys.dart
@@ -4,7 +4,7 @@ class CollectorDatabaseKeys {
   static const String table = 'collectors';
   static const String id = 'id';
   static const String name = 'name';
-  static const String connectedFilterName = 'connected_filter_name';
+  static const String hasFilter = 'has_filter';
   static const String createdAt = 'created_at';
   static const String updatedAt = 'updated_at';
   static const String companyId = 'company_id';
