4cd6762088a8e84184cb7906101f506955d692c5
jolayemi <ajolayemi82@gmail.com>
2024-04-30 16:10:42 +0200

    created service class for pump status

---
+++ app/lib/src/features/pumps/service/pump_status_service.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/shared/models/firebase_callable_function_body.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:irrigazione_iot/src/config/data/mqtt_topics_suffix.dart';
+import 'package:irrigazione_iot/src/features/authentication/data/auth_repository.dart';
+import 'package:irrigazione_iot/src/features/company_users/data/company_repository.dart';
+import 'package:irrigazione_iot/src/features/company_users/data/selected_company_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/pump_status_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
+
+part 'pump_status_service.g.dart';
+
+class PumpStatusService {
+  const PumpStatusService(
+    this._ref,
+  );
+  final Ref _ref;
+
+  Future<void> toggleStatus({
+    required Pump pump,
+    required bool status,
+  }) async {
+    final authRepo = _ref.read(authRepositoryProvider);
+    final uid = authRepo.currentUser?.uid;
+
+    if (uid == null) return;
+    final selectedCompanyRepo = _ref.read(selectedCompanyRepositoryProvider);
+
+    final companyId = selectedCompanyRepo.loadSelectedCompanyId(uid);
+
+    if (companyId == null) return;
+
+    final companyRepo = _ref.read(companyRepositoryProvider);
+    final company = await companyRepo.fetchCompany(companyId);
+
+    if (company == null) return;
+
+    final companyMqttTopicName = company.mqttTopicName;
+    final statusCommand = pump.getStatusCommand(status);
+
+    final pumpStatusRepo = _ref.read(pumpStatusRepositoryProvider);
+
+    final mqttSuffix = _ref.read(mqttTopicsSuffixProvider);
+
+    final body = FirebaseCallableFunctionBody(
+      topic: '$companyMqttTopicName/${mqttSuffix.pumpStatusToggle}',
+      message: statusCommand,
+    );
+    await pumpStatusRepo.togglePumpStatus(
+        pumpId: pump.id,
+        statusBoolean: status,
+        statusBody: body
+      );
+  }
+}
+
+@Riverpod(keepAlive: true)
+PumpStatusService pumpStatusService(PumpStatusServiceRef ref) {
+  return PumpStatusService(ref);
+}
