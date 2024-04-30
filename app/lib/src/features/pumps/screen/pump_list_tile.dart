a6400c50cedcee0c92998c2a95d88c651da92608
jolayemi <ajolayemi82@gmail.com>
2024-04-30 15:22:59 +0200

    refactored pump list tile

--- app/lib/src/features/pumps/screen/pump_list_tile.dart
+++ app/lib/src/features/pumps/screen/pump_list_tile.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:irrigazione_iot/src/config/routes/routes_enums.dart';
 import 'package:irrigazione_iot/src/constants/breakpoints.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
 import 'package:irrigazione_iot/src/features/pumps/screen/dismiss_pump_controller.dart';
 import 'package:irrigazione_iot/src/features/pumps/screen/pump_list_tile_subtitle.dart';
-import 'package:irrigazione_iot/src/features/pumps/screen/pump_status_switch_controller.dart';
-import 'package:irrigazione_iot/src/utils/custom_controller_state.dart';
-import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/features/pumps/screen/pump_status_controller.dart';
+import 'package:irrigazione_iot/src/features/pumps/widgets/pump_tile_title.dart';
+import 'package:irrigazione_iot/src/features/pumps/widgets/pump_tile_trailing_button.dart';
 import 'package:irrigazione_iot/src/shared/widgets/alert_dialogs.dart';
 import 'package:irrigazione_iot/src/shared/widgets/custom_dismissible.dart';
 import 'package:irrigazione_iot/src/shared/widgets/responsive_center.dart';
+import 'package:irrigazione_iot/src/utils/custom_controller_state.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class PumpListTile extends ConsumerWidget {
   const PumpListTile({
@@ -43,38 +46,35 @@ class PumpListTile extends ConsumerWidget {
         .confirmDismiss(pump.id);
   }
 
+  void _onTap(BuildContext context) {
+    context.goNamed(
+      AppRoute.pumpDetails.name,
+      pathParameters: {
+        'pumpId': pump.id,
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final globalLoadingState =
-        ref.watch(pumpStatusSwitchControllerProvider).isGlobalLoading;
-
+        ref.watch(pumpStatusControllerProvider).isGlobalLoading;
     final isDeleting = ref.watch(dismissPumpControllerProvider).isLoading;
 
-    // TODO: Remove switch
-    // TODO: replace with normal Outlined Button
-    // TODO: an indicator after pump name to indicate if on or off
-    // TODO: info icon button to view details
     return ResponsiveCenter(
       maxContentWidth: Breakpoint.tablet,
       child: InkWell(
-        onTap: globalLoadingState
-            ? null
-            : () => context.goNamed(
-                  AppRoute.pumpDetails.name,
-                  pathParameters: {
-                    'pumpId': pump.id,
-                  },
-                ),
+        onTap: globalLoadingState ? null : () => _onTap(context),
         child: CustomDismissibleWidget(
           dismissibleKey: pumpListTileKey(pump),
           confirmDismiss: (_) async => await _dismissPump(context, ref),
           onDismissed: (_) {},
           isDeleting: isDeleting,
           child: ListTile(
-              title: Text(pump.name),
-              subtitle: PumpListTileSubtitle(pump: pump),
-              trailing:
-                  OutlinedButton(onPressed: () {}, child: Text('Spegni'))),
+            title: PumpTileTitle(pump: pump),
+            subtitle: PumpListTileSubtitle(pump: pump),
+            trailing: PumpTileTrailingButton(pump: pump),
+          ),
         ),
       ),
     );
