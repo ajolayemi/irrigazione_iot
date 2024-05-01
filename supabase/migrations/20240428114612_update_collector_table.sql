f19836f838db0b0a0e9464a3e5ffd9784941693a
jolayemi <ajolayemi82@gmail.com>
2024-05-01 13:05:04 +0200

    updated migration files

--- supabase/migrations/20240428114612_update_collector_table.sql
+++ supabase/migrations/20240428114612_update_collector_table.sql
@@ -2,4 +2,4 @@ alter table "public"."collectors"
 drop column "connected_filter_name";
 
 alter table "public"."collectors"
-add column "has_filter" boolean not null;
+add column "has_filter" boolean not null default false;
