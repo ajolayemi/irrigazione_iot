7641939827a791d422be8754331a0456eba666b7
jolayemi <ajolayemi82@gmail.com>
2024-05-01 17:56:45 +0200

    replaced pump and sector subtitles

--- app/lib/src/features/pumps/widgets/pump_list_tile_subtitle.dart
+++ app/lib/src/features/pumps/widgets/pump_list_tile_subtitle.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 import 'package:irrigazione_iot/src/features/pumps/data/pump_flow_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
-import 'package:irrigazione_iot/src/utils/date_formatter.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class PumpListTileSubtitle extends ConsumerWidget {
@@ -15,7 +16,6 @@ class PumpListTileSubtitle extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final dateFormatter = ref.watch(dateFormatWithTimeProvider);
     final loc = context.loc;
     final lastDispensationDate =
         ref.watch(lastDispensationStreamProvider(pump.id)).valueOrNull;
@@ -23,7 +23,10 @@ class PumpListTileSubtitle extends ConsumerWidget {
       loc.pumpStatusLastSwitchedOn(
         lastDispensationDate == null
             ? loc.notAvailable
-            : dateFormatter.format(lastDispensationDate),
+            : timeago.format(
+                lastDispensationDate,
+                locale: context.locale,
+              ),
       ),
       style: context.textTheme.titleSmall?.copyWith(
         color: Colors.grey,
--- app/lib/src/features/sectors/widgets/sector_list_tile_subtitle.dart
+++ app/lib/src/features/sectors/widgets/sector_list_tile_subtitle.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 import 'package:irrigazione_iot/src/features/sectors/data/sector_pressure_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/model/sector.dart';
 import 'package:irrigazione_iot/src/features/specie/data/specie_repository.dart';
 import 'package:irrigazione_iot/src/features/variety/data/variety_repository.dart';
-import 'package:irrigazione_iot/src/utils/date_formatter.dart';
 import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class SectorListTileSubtitle extends ConsumerWidget {
@@ -21,26 +22,17 @@ class SectorListTileSubtitle extends ConsumerWidget {
     final varietySpecie = sectorVariety != null && sectorSpecie != null
         ? '${sectorSpecie.name} ${sectorVariety.name}'
         : context.loc.notAvailable;
-    // TODO: add after sector name an indicator to show whether a sector is on
-    // TODO: sector list tile should become ExpansionTile
-    // TODO: when expanded, user should see
-    // TODO: - switch to turn on/off the sector
-    // TODO: - if on/off, last irrigation date and duration (how long was the last irrigation and litres consumed per plants)
-    // TODO: - if on, since when is this sector being irrigated
-    // TODO: - if on and automatic, how much longer till the end
-    // TODO: - average litres/plant in the last 7 days
-    // TODO: last irrigation date should follow this format
-    // TODO: esempio dom. 31 marzo 12:00 (giorno e ore, 2 gg e 3 ore fa or 1 ora fa )
-    // TODO: second case for when it's <= a single day difference
 
-    // TODO: Expansion tile is not needed anymore
     // TODO: the details screen should follow the same structure as that of pumps
     // TODO: add Connected Sensors to the sector details screen
-    final dateFormatter = ref.watch(dateFormatWithTimeProvider);
+
     final lastIrrigatedDate =
         ref.watch(sectorLastPressureStreamProvider(sector.id)).valueOrNull;
     final lastIrrigatedString = lastIrrigatedDate != null
-        ? dateFormatter.format(lastIrrigatedDate)
+        ? timeago.format(
+            lastIrrigatedDate,
+            locale: context.locale,
+          )
         : context.loc.notAvailable;
     return Text(
       '$varietySpecie\n${context.loc.sectorLastIrrigation(
