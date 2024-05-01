f3aba7aed5250718fd24d25c66992d2f1e8387ba
jolayemi <ajolayemi82@gmail.com>
2024-05-01 18:24:09 +0200

    moved card to display pump last dispensation in its own widget

--- app/lib/src/features/pumps/screen/pump_details/pump_details_list.dart
+++ app/lib/src/features/pumps/screen/pump_details/pump_details_list.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:irrigazione_iot/src/features/pumps/data/pump_flow_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
-import 'package:irrigazione_iot/src/utils/date_formatter.dart';
-import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/features/pumps/widgets/pump_details_last_dispensation_card.dart';
 import 'package:irrigazione_iot/src/shared/widgets/details_tile_widget.dart';
 import 'package:irrigazione_iot/src/shared/widgets/responsive_details_card.dart';
+import 'package:irrigazione_iot/src/utils/extensions.dart';
 
 class PumpDetailsList extends ConsumerWidget {
   const PumpDetailsList({super.key, required this.pump});
@@ -17,17 +18,7 @@ class PumpDetailsList extends ConsumerWidget {
     final loc = context.loc;
     return SliverList(
       delegate: SliverChildListDelegate.fixed([
-        ResponsiveDetailsCard(child: Consumer(builder: (context, ref, child) {
-          final lastDispensation =
-              ref.watch(lastDispensationStreamProvider(pump.id)).valueOrNull;
-          final dateFormatter = ref.watch(dateFormatWithTimeProvider);
-          return DetailTileWidget(
-            title: loc.pumpLastDispensationForTile,
-            subtitle: lastDispensation == null
-                ? loc.pumpLastDispensationEmpty
-                : dateFormatter.format(lastDispensation),
-          );
-        })),
+        PumpDetailsLastDispensationCard(pumpId: pump.id),
         ResponsiveDetailsCard(
           child: DetailTileWidget(
             title: loc.pumpVolumeCapacityFormFieldTitle,
