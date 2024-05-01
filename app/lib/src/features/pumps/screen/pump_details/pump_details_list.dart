17d910350031e4818ef186799e0cff6aeb2f4e38
jolayemi <ajolayemi82@gmail.com>
2024-05-01 19:00:47 +0200

    refactored pump details list

--- app/lib/src/features/pumps/screen/pump_details/pump_details_list.dart
+++ app/lib/src/features/pumps/screen/pump_details/pump_details_list.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/constants/app_sizes.dart';
 
-import 'package:irrigazione_iot/src/features/pumps/data/pump_flow_repository.dart';
 import 'package:irrigazione_iot/src/features/pumps/model/pump.dart';
+import 'package:irrigazione_iot/src/features/pumps/widgets/pump_details_characteristics.dart';
 import 'package:irrigazione_iot/src/features/pumps/widgets/pump_details_last_dispensation_card.dart';
-import 'package:irrigazione_iot/src/shared/widgets/details_tile_widget.dart';
-import 'package:irrigazione_iot/src/shared/widgets/responsive_details_card.dart';
-import 'package:irrigazione_iot/src/utils/extensions.dart';
+import 'package:irrigazione_iot/src/features/pumps/widgets/pump_details_statistics.dart';
 
 class PumpDetailsList extends ConsumerWidget {
   const PumpDetailsList({super.key, required this.pump});
@@ -15,34 +14,13 @@ class PumpDetailsList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final loc = context.loc;
     return SliverList(
       delegate: SliverChildListDelegate.fixed([
         PumpDetailsLastDispensationCard(pumpId: pump.id),
-        ResponsiveDetailsCard(
-          child: DetailTileWidget(
-            title: loc.pumpVolumeCapacityFormFieldTitle,
-            subtitle: '${pump.capacityInVolume.toString()} m³',
-          ),
-        ),
-        ResponsiveDetailsCard(
-          child: DetailTileWidget(
-            title: loc.pumpKwFormFieldTitle,
-            subtitle: '${pump.consumeRateInKw.toString()} kW',
-          ),
-        ),
-        Consumer(
-          builder: (context, ref, child) {
-            final litresDispensed =
-                ref.watch(pumpTotalDispensedLitresProvider(pump.id)).value;
-            return ResponsiveDetailsCard(
-              child: DetailTileWidget(
-                title: loc.pumpTotalDispensedLitres,
-                subtitle: '${litresDispensed.toString()} L',
-              ),
-            );
-          },
-        )
+        gapH8,
+        PumpDetailsCharacteristics(pump: pump),
+        gapH4,
+        PumpDetailsStatistics(pumpId: pump.id),
       ]),
     );
   }
