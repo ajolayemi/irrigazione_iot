bea2b2433b7b4464c828b835a0fab57113bba46d
jolayemi <ajolayemi82@gmail.com>
2024-05-02 15:18:44 +0200

    defined pump statistic repository and implemented its method

---
+++ app/lib/src/features/pumps/data/supabase_pump_statistic_repository.dart
@@ -0,0 +1,26 @@
+import 'package:irrigazione_iot/src/features/pumps/data/pump_statistic_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump_pressure.dart';
+import 'package:irrigazione_iot/src/features/pumps/model/pump_pressure_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabasePumpStatisticRepository implements PumpStatisticRepository {
+  const SupabasePumpStatisticRepository(this._supabaseClient);
+  final SupabaseClient _supabaseClient;
+  @override
+  Stream<PumpPressure?> watchLastPumpPressure(String pumpId) {
+    final stream = _supabaseClient.pumpPressuresStream
+        .eq(PumpPressureDatabaseKeys.pumpId, pumpId)
+        .order(PumpPressureDatabaseKeys.createdAt, ascending: false)
+        .limit(1);
+
+    return stream.map((pressures) {
+      print(pressures);
+      if (pressures.isEmpty) return null;
+      final pumpPressure = PumpPressure.fromJson(
+        pressures.first,
+      );
+      return pumpPressure;
+    });
+  }
+}
