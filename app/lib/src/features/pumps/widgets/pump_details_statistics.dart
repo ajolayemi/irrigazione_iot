1d993e5a54894a7f71a167b34c8b7a9815524462
jolayemi <ajolayemi82@gmail.com>
2024-05-02 15:19:32 +0200

    built pump details expansion tile

--- app/lib/src/features/pumps/widgets/pump_details_statistics.dart
+++ app/lib/src/features/pumps/widgets/pump_details_statistics.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/constants/app_sizes.dart';
 import 'package:irrigazione_iot/src/features/pumps/data/pump_flow_repository.dart';
+import 'package:irrigazione_iot/src/features/pumps/data/pump_statistic_repository.dart';
 import 'package:irrigazione_iot/src/shared/widgets/common_expansion_tile.dart';
 import 'package:irrigazione_iot/src/shared/widgets/details_tile_widget.dart';
 import 'package:irrigazione_iot/src/shared/widgets/responsive_details_card.dart';
@@ -19,9 +20,13 @@ class PumpDetailsStatistics extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final loc = context.loc;
+    final lastPressure =
+        ref.watch(pumpLastPressureStreamProvider(pumpId)).valueOrNull;
+    final lastPressureDate = context.timeAgo(lastPressure?.createdAt);
     return CommonExpansionTile(
       title: loc.entityStatistics,
       children: [
+        // Pump total dispensed litres
         Consumer(
           builder: (context, ref, child) {
             final litresDispensed = ref
@@ -37,6 +42,34 @@ class PumpDetailsStatistics extends ConsumerWidget {
             );
           },
         ),
+
+        // Last filter in pressure read
+        ResponsiveDetailsCard(
+          child: DetailTileWidget(
+            title: loc.lastFilterInPressure,
+            subtitle:
+                '${lastPressure?.filterInPressure ?? '-'} bar ($lastPressureDate)',
+          ),
+        ),
+
+        // Last filter out pressure read
+        ResponsiveDetailsCard(
+          child: DetailTileWidget(
+            title: loc.lastFilterOutPressure,
+            subtitle:
+                '${lastPressure?.filterOutPressure ?? '-'} bar ($lastPressureDate)',
+          ),
+        ),
+
+        // last pressure difference
+        ResponsiveDetailsCard(
+          child: DetailTileWidget(
+            title: loc.lastFilterPressureDifference,
+            subtitle:
+                '${lastPressure?.pressureDifference ?? '-'} bar ($lastPressureDate)',
+          ),
+        ),
+
         gapH16,
       ],
     );
