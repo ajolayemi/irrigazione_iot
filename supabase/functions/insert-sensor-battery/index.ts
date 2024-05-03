bfae801b953f8ea188acbb7ba257321670e5d8b6
jolayemi <ajolayemi82@gmail.com>
2024-05-03 12:40:23 +0200

    defined new edge functions

---
+++ supabase/functions/insert-sensor-battery/index.ts
@@ -0,0 +1,20 @@
+import {commonInsert} from "../_utils/commonDatabaseOperations.ts";
+import {TableNames} from "../_utils/tableConstants.ts";
+
+console.log(`Function "insert-sensor-battery" up and running!`);
+
+Deno.serve(
+  async (req) => await commonInsert(req, TableNames.sensor_battery_data)
+);
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/insert-sensor-battery' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
