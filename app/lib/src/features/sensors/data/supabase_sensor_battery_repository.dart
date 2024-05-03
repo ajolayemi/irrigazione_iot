90e82c0282f526314f002623b4591e089ada7563
jolayemi <ajolayemi82@gmail.com>
2024-05-03 17:19:12 +0200

    defined sensor battery repository, its provider and a supabase implementation of it

---
+++ app/lib/src/features/sensors/data/supabase_sensor_battery_repository.dart
@@ -0,0 +1,26 @@
+import 'package:irrigazione_iot/src/features/sensors/data/sensor_battery_repository.dart';
+import 'package:irrigazione_iot/src/features/sensors/model/sensor_battery.dart';
+import 'package:irrigazione_iot/src/features/sensors/model/sensor_battery_database_keys.dart';
+import 'package:irrigazione_iot/src/utils/supabase_extensions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseSensorBatteryRepository implements SensorBatteryRepository {
+  const SupabaseSensorBatteryRepository(this._supabaseClient);
+  final SupabaseClient _supabaseClient;
+
+  @override
+  Stream<SensorBattery?> lastSensorBatteryStream(String sensorId) {
+    return _supabaseClient.sensorBatteryData
+        .stream(primaryKey: [SensorBatteryDatabaseKeys.id])
+        .eq(SensorBatteryDatabaseKeys.sensorId, sensorId)
+        .order(SensorBatteryDatabaseKeys.createdAt, ascending: false)
+        .limit(1)
+        .map(
+          (data) => data.isEmpty
+              ? null
+              : SensorBattery.fromJson(
+                  data.first,
+                ),
+        );
+  }
+}
