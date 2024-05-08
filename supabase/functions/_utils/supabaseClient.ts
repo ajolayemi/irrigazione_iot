a0b8419edc4fab16436742c55fd00693ab4aaa49
jolayemi <ajolayemi82@gmail.com>
2024-05-08 09:46:24 +0200

    bug: fixed bug for when no auth bearer was provided

--- supabase/functions/_utils/supabaseClient.ts
+++ supabase/functions/_utils/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.0";
+import {createClient} from "https://esm.sh/@supabase/supabase-js@2.42.0";
 
 /**
  * Returns a Supabase client with the Auth context of the logged in user
@@ -7,18 +7,17 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.0";
  * @returns A Supabase client
  */
 export const createEdgeSupabaseClient = (req: Request) => {
-    const supabaseUrl = Deno.env.get("SUPABASE_URL");
-    const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
-    const authHeaders = req.headers;
-    return createClient(
-        supabaseUrl ?? "",
-        supabaseAnonKey ?? "",
-        {
-            global: {
-                headers: {
-                    Authorization: authHeaders.get("Authorization")!,
-                },
-            },
-        }
-    );
-}
+  const supabaseUrl = Deno.env.get("SUPABASE_URL");
+  const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
+  const authHeaders = req.headers;
+  const bearerToken = authHeaders.get("Authorization")?.endsWith("null")
+    ? ""
+    : authHeaders.get("Authorization") ?? "";
+  return createClient(supabaseUrl ?? "", supabaseAnonKey ?? "", {
+    global: {
+      headers: {
+        Authorization: bearerToken,
+      },
+    },
+  });
+};
