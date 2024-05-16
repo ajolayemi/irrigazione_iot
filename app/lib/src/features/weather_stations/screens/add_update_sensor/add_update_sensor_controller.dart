03d5710a1a7269819711c88f77542c8f63f708c8
jolayemi <ajolayemi82@gmail.com>
2024-05-16 10:14:09 +0200

    refactor: refactored ex AddUpdateSensorService, it becomes AddUpdateWeatherStationService

--- app/lib/src/features/weather_stations/screens/add_update_sensor/add_update_sensor_controller.dart
+++ app/lib/src/features/weather_stations/screens/add_update_sensor/add_update_sensor_controller.dart
@@ -1,5 +1,5 @@
 import 'package:irrigazione_iot/src/features/weather_stations/models/weather_station.dart';
-import 'package:irrigazione_iot/src/features/weather_stations/services/add_update_sensor_service.dart';
+import 'package:irrigazione_iot/src/features/weather_stations/services/add_update_weather_station_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'add_update_sensor_controller.g.dart';
@@ -17,7 +17,7 @@ class AddUpdateSensorController extends _$AddUpdateSensorController {
       return false;
     }
 
-    state = await AsyncValue.guard(() => service.createSensor(sensor));
+    state = await AsyncValue.guard(() => service.createWeatherStation(sensor));
     return !state.hasError;
   }
 
@@ -29,7 +29,7 @@ class AddUpdateSensorController extends _$AddUpdateSensorController {
       return false;
     }
 
-    state = await AsyncValue.guard(() => service.updateSensor(sensor));
+    state = await AsyncValue.guard(() => service.updateWeatherStation(sensor));
     return !state.hasError;
   }
 }
