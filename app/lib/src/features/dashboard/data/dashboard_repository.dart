04a1ea343dc5a7dd327c7387be455258922ff7d0
jolayemi <ajolayemi82@gmail.com>
2024-05-20 18:45:36 +0200

    feat: added new dashboard repository method and implemented it

--- app/lib/src/features/dashboard/data/dashboard_repository.dart
+++ app/lib/src/features/dashboard/data/dashboard_repository.dart
@@ -1,6 +1,7 @@
 import 'package:irrigazione_iot/src/features/company_users/data/selected_company_repository.dart';
 import 'package:irrigazione_iot/src/features/dashboard/data/supabase_dashboard_repository.dart';
 import 'package:irrigazione_iot/src/features/dashboard/models/pump_switched_on.dart';
+import 'package:irrigazione_iot/src/features/dashboard/models/sector_switched_on.dart';
 import 'package:irrigazione_iot/src/shared/providers/supabase_client_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -9,6 +10,9 @@ part 'dashboard_repository.g.dart';
 abstract class DashboardRepository {
   /// Emits a list of [PumpSwitchedOn]s for the provided [companyId].
   Stream<List<PumpSwitchedOn>?> watchPumpsSwitchedOn(String companyId);
+
+  /// Emits a list of [SectorSwitchedOn]s for the provided [companyId].
+  Stream<List<SectorSwitchedOn>?> watchSectorsSwitchedOn(String companyId);
 }
 
 @Riverpod(keepAlive: true)
@@ -25,4 +29,14 @@ Stream<List<PumpSwitchedOn>?> pumpsSwitchedOnStream(
   if (currentSelectedCompanyByUser == null) return Stream.value([]);
   final dashboardRepo = ref.watch(dashboardRepositoryProvider);
   return dashboardRepo.watchPumpsSwitchedOn(currentSelectedCompanyByUser.id);
+}
+
+@riverpod
+Stream<List<SectorSwitchedOn>?> sectorsSwitchedOnStream(
+    SectorsSwitchedOnStreamRef ref) {
+  final currentSelectedCompanyByUser =
+      ref.watch(currentTappedCompanyProvider).value;
+  if (currentSelectedCompanyByUser == null) return Stream.value([]);
+  final dashboardRepo = ref.watch(dashboardRepositoryProvider);
+  return dashboardRepo.watchSectorsSwitchedOn(currentSelectedCompanyByUser.id);
 }
--- app/lib/src/features/dashboard/data/supabase_dashboard_repository.dart
+++ app/lib/src/features/dashboard/data/supabase_dashboard_repository.dart
@@ -1,3 +1,5 @@
+import 'package:irrigazione_iot/src/features/dashboard/models/sector_switched_on.dart';
+import 'package:irrigazione_iot/src/features/dashboard/models/sector_switched_on_database_keys.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:irrigazione_iot/src/features/dashboard/data/dashboard_repository.dart';
@@ -13,13 +15,21 @@ class SupabaseDashboardRepository implements DashboardRepository {
         primaryKey: [PumpSwitchedOnDatabaseKeys.id],
       );
 
+  SupabaseStreamFilterBuilder get _sectorsSwitchedOnStream =>
+      _supabaseClient.from(SectorSwitchedOnDatabaseKeys.table).stream(
+        primaryKey: [SectorSwitchedOnDatabaseKeys.id],
+      );
+
   List<PumpSwitchedOn>? convertToPumpSwitchedOnList(
       List<Map<String, dynamic>>? data) {
     if (data == null) return null;
-    return data
-        .map((e) => PumpSwitchedOn.fromJson(e))
-        .where((el) => el.statusBoolean)
-        .toList();
+    return data.map((e) => PumpSwitchedOn.fromJson(e)).toList();
+  }
+
+  List<SectorSwitchedOn>? convertToSectorSwitchedOnList(
+      List<Map<String, dynamic>>? data) {
+    if (data == null) return null;
+    return data.map((e) => SectorSwitchedOn.fromJson(e)).toList();
   }
 
   @override
@@ -28,5 +38,13 @@ class SupabaseDashboardRepository implements DashboardRepository {
         PumpSwitchedOnDatabaseKeys.companyId, companyId);
 
     return res.map(convertToPumpSwitchedOnList);
+  }
+
+  @override
+  Stream<List<SectorSwitchedOn>?> watchSectorsSwitchedOn(String companyId) {
+    final res = _sectorsSwitchedOnStream.eq(
+        SectorSwitchedOnDatabaseKeys.companyId, companyId);
+
+    return res.map(convertToSectorSwitchedOnList);
   }
 }
