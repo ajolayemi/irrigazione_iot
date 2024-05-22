519c32e204128eb4d922093dbbb18b0e813c6308
jolayemi <ajolayemi82@gmail.com>
2024-05-22 07:42:09 +0200

    refactor: added button to switch on/off a sector in its dashboard item

--- app/lib/src/features/dashboard/widgets/sectors/sector_dashboard_tile_title.dart
+++ app/lib/src/features/dashboard/widgets/sectors/sector_dashboard_tile_title.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:irrigazione_iot/src/constants/app_sizes.dart';
-import 'package:irrigazione_iot/src/features/dashboard/widgets/shared/common_pressure_rate_trailing_text.dart';
 import 'package:irrigazione_iot/src/features/dashboard/widgets/shared/dashboard_child_item_details_row.dart';
-import 'package:irrigazione_iot/src/features/sectors/data/sector_pressure_repository.dart';
 import 'package:irrigazione_iot/src/features/sectors/models/sector.dart';
+import 'package:irrigazione_iot/src/features/sectors/screens/sector_switch.dart';
 import 'package:irrigazione_iot/src/shared/widgets/custom_status_indicator.dart';
 import 'package:irrigazione_iot/src/utils/extensions/build_ctx_extensions.dart';
 
@@ -20,31 +19,22 @@ class SectorDashboardTileTitle extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = context.textTheme;
     return DashboardChildItemDetailsRow(
-        leading: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Flexible(
-              child: Text(
-                sector.name,
-                style: textTheme.titleMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
+      leading: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            child: Text(
+              sector.name,
+              style: textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.bold,
               ),
             ),
-            gapW8,
-            const CommonStatusIndicator(status: true),
-          ],
-        ),
-        trailing: Consumer(
-          builder: (context, ref, child) {
-            final sectorPressure = ref.watch(
-              sectorLastPressureStreamProvider(sector.id),
-            );
-            final value = sectorPressure.valueOrNull?.pressure ?? 0.0;
-            return CommonPressureRateTrailingText(
-              pressure: value,
-            );
-          },
-        ));
+          ),
+          gapW8,
+          const CommonStatusIndicator(status: true),
+        ],
+      ),
+      trailing: SectorSwitch(sector: sector),
+    );
   }
 }
