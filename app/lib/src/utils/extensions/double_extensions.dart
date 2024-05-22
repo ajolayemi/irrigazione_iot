498e608d09fe919600b0b116cef70b49621fe267
jolayemi <ajolayemi82@gmail.com>
2024-05-22 08:25:04 +0200

    refactor: replaced normal yellow color for pressures with a shade of yellow

--- app/lib/src/utils/extensions/double_extensions.dart
+++ app/lib/src/utils/extensions/double_extensions.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 extension PressureColorX on double {
   /// Returns the color that represents a pressure level.
-  Color getPressureColor() {
+  Color? getPressureColor() {
     if (this < 3.5) {
       return Colors.green;
     } else if (this < 4.0) {
-      return Colors.yellow;
+      return Colors.yellow[600];
     }
     return Colors.red;
   }
