38920e67a9998d359305f07d994dc6ab04dada63
jolayemi <ajolayemi82@gmail.com>
2024-05-22 07:48:39 +0200

    feat: added a separate widget to display sector pressure info

--- app/lib/src/features/dashboard/widgets/sectors/sector_switched_on_list_tile.dart
+++ app/lib/src/features/dashboard/widgets/sectors/sector_switched_on_list_tile.dart
@@ -6,6 +6,7 @@ import 'package:irrigazione_iot/src/features/dashboard/widgets/sectors/sector_co
 import 'package:irrigazione_iot/src/features/dashboard/widgets/sectors/sector_collector_pressure_for_dashboard.dart';
 import 'package:irrigazione_iot/src/features/dashboard/widgets/sectors/sector_dashboard_tile_title.dart';
 import 'package:irrigazione_iot/src/features/dashboard/widgets/sectors/sector_irrigation_duration.dart';
+import 'package:irrigazione_iot/src/features/dashboard/widgets/sectors/sector_pressure_for_dashboard.dart';
 import 'package:irrigazione_iot/src/features/dashboard/widgets/shared/dashboard_child_item_list_tile.dart';
 import 'package:irrigazione_iot/src/features/sectors/data/sector_repository.dart';
 import 'package:irrigazione_iot/src/shared/widgets/common_responsive_divider.dart';
@@ -30,6 +31,8 @@ class SectorSwitchedOnListTile extends ConsumerWidget {
           SectorCollectorForDashboard(sectorId: sector.id),
           const CommonResponsiveDivider(),
           SectorCollectorPressureForDashboard(sectorId: sector.id),
+          gapH8,
+          SectorPressureForDashboard(sectorId: sector.id),
           const CommonResponsiveDivider(),
           SectorIrrigationDuration(sectorId: sector.id),
         ],
