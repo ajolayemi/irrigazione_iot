98114b6e85477b9def15780b23613e90c1aa2b57
Kehinde Jolayemi <ajolayemi82@gmail.com>
2024-10-12 10:42:33 +0200

    feat: added google map package

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,19 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Register Google Maps
+    registerGoogleMaps()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+
+// Google Maps API Key
+extension AppDelegate {
+
+    private func registerGoogleMaps() {
+        GMSServices.provideAPIKey("AIzaSyCtXOM6sUPtvRZHigIwykHa5UZLbQo2gHA")
+    }
+  
 }
