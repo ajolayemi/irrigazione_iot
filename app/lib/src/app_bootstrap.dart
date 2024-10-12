0869ef3d8e7a65ff42b55e898a08d9b2fbf38f65
Kehinde Jolayemi <ajolayemi82@gmail.com>
2024-10-12 08:43:04 +0200

    feat: initialized service locator

--- app/lib/src/app_bootstrap.dart
+++ app/lib/src/app_bootstrap.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:irrigazione_iot/src/application/di/service_locator.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:irrigazione_iot/src/app.dart';
 import 'package:irrigazione_iot/src/exceptions/error_logger.dart';
@@ -23,6 +24,8 @@ class AppBootstrap {
 
     // Load the SharedPreferences instance during initialization
     await container.read(sharedPreferencesProvider.future);
+
+      ServiceLocator.init();
 
     return UncontrolledProviderScope(
       container: container,
